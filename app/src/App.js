40bcd542285522d0804a84808cd30be963cc7405
Dave Cole <170641+dhcole@users.noreply.github.com>
2020-12-11 12:26:30 -0500

    check in

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,69 @@
+import React from 'react';
 import Form from '@rjsf/core';
 import schema from './form.json';
 
 const log = (type) => console.log.bind(console, type);
 
-function App() {
-  return (
-  <Form schema={schema}
-        onChange={log("changed")}
-        onSubmit={log("submitted")}
-        onError={log("errors")} />
-  );
+const sections = Object.keys(schema.properties);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { page: 0 };
+  }
+
+  componentDidMount() {
+    document.querySelectorAll('#root > .field-object > fieldset')[0].className = 'active';
+  }
+
+  componentDidUpdate() {
+    document.querySelectorAll('#root > .field-object > fieldset').forEach(function(el) {
+      el.className = '';
+    });
+    document.querySelector('#root_' + sections[this.state.page]).className = 'active';
+  }
+
+  onSubmit(submit, e) {
+    console.log('submit', this.state.page+1 < sections.length)
+
+    localStorage.setItem('formData', JSON.stringify(submit.formData));
+    if (this.state.page+1 < sections.length) {
+      this.setState({
+        page: this.state.page + 1
+      });
+    }
+  }
+
+  onBack(submit, e) {
+    console.log('back', this.state.page+1 > 0)
+    localStorage.setItem('formData', JSON.stringify(submit.formData));
+    if (this.state.page+1 > 0) {
+      this.setState({
+        page: this.state.page - 1
+      });
+    }
+    return false;
+  }
+
+
+  render() {
+    let data = localStorage.getItem('formData');
+    data = (data !== 'undefined') ? JSON.parse(data) : {};
+    return (
+      <Form schema={schema}
+            className={`page-${this.state.page}`}
+            onChange={log("changed")}
+            onSubmit={this.onSubmit.bind(this)}
+            formData={data}
+            onError={log("errors")}
+      >
+        {this.state.page > 0 &&
+          <a className="btn btn-default" onClick={this.onBack.bind(this)}>Back</a>
+        }
+        <button type="submit" className="btn btn-primary">Next</button>
+      </Form>
+    );
+  }
 }
 
 export default App;
