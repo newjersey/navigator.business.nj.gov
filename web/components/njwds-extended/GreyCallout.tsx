215992ae8f28eb44f6f1cd540ed6bc5e1b66a322
Anne LoVerso <anne.loverso@gmail.com>
2021-03-25 16:40:55 -0400

    Increase font size for 'Edit' text in grey box [#43]

--- web/components/njwds-extended/GreyCallout.tsx
+++ web/components/njwds-extended/GreyCallout.tsx
@@ -15,7 +15,7 @@ export const GreyCallout = (props: Props): ReactElement => {
     <div className="margin-top-3 font-body-2xs fdr padding-3 bg-base-lightest border-base-lighter border-1px">
       <div>{props.children}</div>
       {props.link && (
-        <div className="mla">
+        <div className="mla font-body-sm">
           <a href={props.link.href}>{props.link.text}</a>
         </div>
       )}
