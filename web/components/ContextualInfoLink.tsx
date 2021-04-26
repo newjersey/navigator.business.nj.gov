a4408e16d0683f190be3d8371b6684f83a9cfc31
Anne LoVerso <anne.loverso@gmail.com>
2021-04-26 11:43:11 -0400

    [#84] change dotted line to dashed

--- web/components/ContextualInfoLink.tsx
+++ web/components/ContextualInfoLink.tsx
@@ -23,7 +23,7 @@ export const ContextualInfoLink = ({ children }: { children: string[] }): ReactE
   return (
     <>
       <button className="usa-button--unstyled cursor-pointer" onClick={setContext}>
-        <span className="dotted-underline">{displayText}</span>
+        <span className="dashed-underline border-primary">{displayText}</span>
       </button>
     </>
   );
