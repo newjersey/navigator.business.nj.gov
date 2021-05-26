d2fd84e513f5b4bac1051130ccdfcbaa28a8a548
E J Kalafarski <e.j.kalafarski@gmail.com>
2021-05-26 13:36:44 -0400

    Reverted unavailableText, updated badInputAlertText

--- web/display-content/tasks/search-business-names/SearchBusinessNamesDefaults.ts
+++ web/display-content/tasks/search-business-names/SearchBusinessNamesDefaults.ts
@@ -1,7 +1,7 @@
 export const SearchBusinessNamesDefaults: Record<string, string> = {
   placeholderText: "Type potential business name",
   searchButtonText: "Check Availability",
-  unavailableText: 'Sorry, "${name}" isn’t available. Try begin more specific.',
+  unavailableText: 'Sorry, "${name}" isn’t available. Try another name.',
   similarUnavailableNamesText: "Here are similar business names that are already taken:",
   availableText: 'Looks like "${name}" might be available!',
   updateButtonText: "Update the name on your roadmap with this one",
@@ -10,5 +10,5 @@ export const SearchBusinessNamesDefaults: Record<string, string> = {
   officialCheckButtonLink: "https://www.njportal.com/DOR/BusinessNameSearch/Search/BusinessName",
   officialCheckText:
     "Do an official check with the New Jersey Division of Revenue and Enterprise Services to confirm your name is legally clear to use. Once confirmed, you can reserve the name and form your business.",
-  badInputAlertText: "Sorry, that isn't a valid search input",
+  badInputAlertText: "Sorry, that isn't a valid company name. Try being more specific.",
 };
