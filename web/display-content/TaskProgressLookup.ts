597fc50ee1f2d35011a2a507e6bfc7743ea9daac
E J Kalafarski <e.j.kalafarski@gmail.com>
2021-06-24 11:40:24 -0400

    Remove hyphen

--- web/display-content/TaskProgressLookup.ts
+++ web/display-content/TaskProgressLookup.ts
@@ -2,6 +2,6 @@ import { TaskProgress } from "../lib/types/types";
 
 export const TaskProgressLookup: Record<TaskProgress, string> = {
   NOT_STARTED: "Not started",
-  IN_PROGRESS: "In-progress",
+  IN_PROGRESS: "Inprogress",
   COMPLETED: "Completed",
 };
