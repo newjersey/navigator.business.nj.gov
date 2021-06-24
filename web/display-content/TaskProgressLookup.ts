dc227e345449d594c9f4f60d363705f6217a49e1
E J Kalafarski <e.j.kalafarski@gmail.com>
2021-06-24 11:40:43 -0400

    New and improved with spacebar

--- web/display-content/TaskProgressLookup.ts
+++ web/display-content/TaskProgressLookup.ts
@@ -2,6 +2,6 @@ import { TaskProgress } from "../lib/types/types";
 
 export const TaskProgressLookup: Record<TaskProgress, string> = {
   NOT_STARTED: "Not started",
-  IN_PROGRESS: "Inprogress",
+  IN_PROGRESS: "In progress",
   COMPLETED: "Completed",
 };
