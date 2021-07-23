f823d0bfc154d78ed0355077553862c9acaf9065
Anne LoVerso <anne.loverso@gmail.com>
2021-07-23 16:06:00 -0400

    Fix console warnings in Signup tests

--- web/components/Signup.test.tsx
+++ web/components/Signup.test.tsx
@@ -21,12 +21,17 @@ describe("<Signup />", () => {
     subject = render(<Signup isOpen={true} onClose={jest.fn()} />);
   };
 
-  it("collects name, email, and confirm email fields and submits to api", () => {
+  it("collects name, email, and confirm email fields and submits to api", async () => {
     renderPage();
+
+    const returnedPromise = Promise.resolve({ authRedirectURL: "www.example.com" });
+    mockApi.postSelfReg.mockReturnValue(returnedPromise);
+
     fillText("Some Name", "name");
     fillText("some-email@example.com", "email");
     fillText("some-email@example.com", "confirm-email");
     clickSubmit();
+    await act(() => returnedPromise);
     expect(mockApi.postSelfReg).toHaveBeenCalledWith({
       email: "some-email@example.com",
       confirmEmail: "some-email@example.com",
@@ -50,22 +55,20 @@ describe("<Signup />", () => {
     expect(subject.queryByTestId("loading-spinner")).not.toBeInTheDocument();
   });
 
-  it("redirects the user to the url returned by the api", (done) => {
+  it("redirects the user to the url returned by the api", async () => {
     renderPage();
     fillText("Some Name", "name");
     fillText("some-email@example.com", "email");
     fillText("some-email@example.com", "confirm-email");
-    mockApi.postSelfReg.mockResolvedValue({ authRedirectURL: "www.example.com" });
-
-    mockPush.mockImplementation((newUrl: string): void => {
-      expect(newUrl).toEqual("www.example.com");
-      done();
-    });
+    const resolvedPromise = Promise.resolve({ authRedirectURL: "www.example.com" });
+    mockApi.postSelfReg.mockReturnValue(resolvedPromise);
 
     clickSubmit();
+    await act(() => resolvedPromise);
+    expect(mockPush).toHaveBeenCalledWith("www.example.com");
   });
 
-  it("shows the user an error message if the emails don't match", () => {
+  it("shows the user an error message if the emails don't match", async () => {
     renderPage();
     expect(subject.queryByTestId("error-alert-EMAILS_DO_NOT_MATCH")).not.toBeInTheDocument();
 
@@ -76,13 +79,18 @@ describe("<Signup />", () => {
     expect(subject.queryByTestId("error-alert-EMAILS_DO_NOT_MATCH")).toBeInTheDocument();
     expect(mockApi.postSelfReg).not.toHaveBeenCalled();
 
+    const resolvedPromise = Promise.resolve({ authRedirectURL: "www.example.com" });
+    mockApi.postSelfReg.mockReturnValue(resolvedPromise);
     fillText("some-email@example.com", "confirm-email");
     clickSubmit();
+
+    await act(() => resolvedPromise);
+
     expect(subject.queryByTestId("error-alert-EMAILS_DO_NOT_MATCH")).not.toBeInTheDocument();
     expect(mockApi.postSelfReg).toHaveBeenCalled();
   });
 
-  it("shows the user an error message if any fields empty", () => {
+  it("shows the user an error message if any fields empty", async () => {
     renderPage();
     expect(subject.queryByTestId("error-alert-REQUIRED_FIELDS")).not.toBeInTheDocument();
 
@@ -100,8 +108,12 @@ describe("<Signup />", () => {
     clickSubmit();
     expect(subject.queryByTestId("error-alert-REQUIRED_FIELDS")).toBeInTheDocument();
 
+    const resolvedPromise = Promise.resolve({ authRedirectURL: "www.example.com" });
+    mockApi.postSelfReg.mockReturnValue(resolvedPromise);
     fillText("Some Name", "name");
     clickSubmit();
+
+    await act(() => resolvedPromise);
     expect(subject.queryByTestId("error-alert-REQUIRED_FIELDS")).not.toBeInTheDocument();
     expect(mockApi.postSelfReg).toHaveBeenCalled();
   });
