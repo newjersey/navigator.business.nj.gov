7bf6779ebdf20aa4bc69b2e3c3934aaac33dacad
Maita Navarro <mnavarr3@wellesley.edu>
2021-08-11 14:57:11 -0400

    [#218] make mini roadmap burger bar sticky on mobile

--- web/components/njwds-extended/SidebarPageLayout.tsx
+++ web/components/njwds-extended/SidebarPageLayout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import { MediaQueries } from "@/lib/PageSizes";
 import { useMediaQuery } from "@material-ui/core";
 import { Icon } from "@/components/njwds/Icon";
@@ -15,8 +15,22 @@ interface Props {
 export const SidebarPageLayout = ({ children, sidebar, backButton, pageTitle }: Props): ReactElement => {
   const isLargeScreen = useMediaQuery(MediaQueries.desktopAndUp);
   const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
 
   const isVisible = () => (sidebarIsOpen ? "is-visible" : "");
+
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 108) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  });
 
   const nav = () => (
     <nav aria-label="Secondary navigation" className={`left-nav ${isVisible()}`}>
@@ -48,8 +62,8 @@ export const SidebarPageLayout = ({ children, sidebar, backButton, pageTitle }: 
         }}
       />
       {!isLargeScreen && (
-        <div className="usa-nav-container">
-          <div className="usa-navbar">
+        <div className={`usa-nav-container ${scrolled ? "scrolled scrolled-transition" : ""}`}>
+          <div className={`usa-navbar ${scrolled ? "bg-white" : ""}`}>
             <button
               className="left-nav-menu-button radius-0"
               onClick={() => {
@@ -59,13 +73,13 @@ export const SidebarPageLayout = ({ children, sidebar, backButton, pageTitle }: 
             >
               <Icon className="font-sans-xl">menu</Icon>
             </button>
-            <div className="usa-logo">
+            <div className={`usa-logo ${!isLargeScreen && scrolled ? "bg-white" : ""}`}>
               <em className="usa-logo__text">{pageTitle}</em>
             </div>
           </div>
         </div>
       )}
-      <div className="usa-section">
+      <div className={`usa-section ${!isLargeScreen && scrolled ? "scrolled-padding" : ""}`}>
         <div className="grid-container">
           <div className="grid-row grid-gap">
             <div className="usa-layout-docs__sidenav desktop:grid-col-4">
