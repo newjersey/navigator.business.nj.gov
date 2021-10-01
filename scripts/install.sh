4dd6c304464e45ce75d60bb923e8882b577ef492
Dave Sueltenfuss <dsueltenfuss@fearless.tech>
2021-10-01 07:22:56 -0400

    feat: migrate all install requirements to package.json

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-npm --prefix=api install
-npm --prefix=web install
-npm install -g @aws-amplify/cli
-npm install -g serverless
-npm --prefix=api run install-dynamo-local
+npm install
