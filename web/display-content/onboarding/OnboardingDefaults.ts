01b963ba82b713530c2ffb67f634ff6ed93e3215
Jess Lax <78366346+Jesslax@users.noreply.github.com>
2021-10-19 16:41:24 -0400

    Updated line 2 and 5

--- web/display-content/onboarding/OnboardingDefaults.ts
+++ web/display-content/onboarding/OnboardingDefaults.ts
@@ -1,8 +1,8 @@
 export const OnboardingDefaults: Record<string, string> = {
-  pageTitle: "Tell Us Your",
+  pageTitle: "Tell Us About Your Business",
   nextButtonText: "Next",
   backButtonText: "Back",
-  stepXofYTemplate: "(step ${currentPage} of ${totalPages})",
+  stepXofYTemplate: "(Step ${currentPage} of ${totalPages})",
   errorTextRequiredLegal: "Please select a legal structure.",
   errorTextRequiredMunicipality: "Please select a location.",
 };
