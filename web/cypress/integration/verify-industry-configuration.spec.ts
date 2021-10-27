83db5688f815448eacb18e7c7bd381d9a6cc3d45
Dave Sueltenfuss <dsueltenfuss@fearless.tech>
2021-10-27 16:11:27 -0400

    test: add new test to validate industry configuration

---
+++ web/cypress/integration/verify-industry-configuration.spec.ts
@@ -0,0 +1,26 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
+
+import { completeOnboarding } from "../support/helpers";
+import { Industries } from "@businessnjgovnavigator/shared";
+
+describe("Roadmap [all]", () => {
+  beforeEach(() => {
+    cy.resetUserData();
+    cy.loginByCognitoApi();
+  });
+
+  Industries.forEach((industry) => {
+    it(` ${industry.name} completes onboarding and shows the roadmap`, () => {
+      // onboarding
+      completeOnboarding("Beesapple's", industry.id, "general-partnership", !industry.canBeHomeBased);
+
+      cy.url().should("contain", "/roadmap");
+
+      // check roadmap
+      cy.get('[data-business-name="Beesapple\'s"]').should("exist");
+      cy.get(`[data-industry=${industry.id}]`).should("exist");
+      cy.get('[data-legal-structure="general-partnership"]').should("exist");
+      cy.get('[data-municipality="Absecon"]').should("exist");
+    });
+  });
+});
