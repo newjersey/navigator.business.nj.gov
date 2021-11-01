ab88f01b0708f2f1dc2c0cddcdf53aa194d57c78
Faraz Akhtar <fa8338@gmail.com>
2021-11-01 15:21:25 -0400

    chore: add gateway response fault to serverless.ts

--- api/serverless.ts
+++ api/serverless.ts
@@ -151,6 +151,19 @@ if (!env.CI || stage === "local") {
           TableName: usersTable,
         },
       },
+      GatewayResponseDefault4XX: {
+        Type: "AWS::ApiGateway::GatewayResponse",
+        Properties: {
+          ResponseParameters: {
+            "gatewayresponse.header.Access-Control-Allow-Origin": "'*'",
+            "gatewayresponse.header.Access-Control-Allow-Headers": "'*'",
+          },
+          ResponseType: "DEFAULT_5XX",
+          RestApiId: {
+            Ref: "ApiGatewayRestApi",
+          },
+        },
+      },
     },
   };
 }
