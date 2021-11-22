f01af598d49d8d6d6450bb63a336076f231885fb
Faraz Akhtar <fa8338@gmail.com>
2021-11-22 14:46:47 -0500

    feat: [#886] reposition task status on roadmap for mobile

--- web/src/components/Task.tsx
+++ web/src/components/Task.tsx
@@ -4,6 +4,8 @@ import * as types from "@/lib/types/types";
 import { useUserData } from "@/lib/data-hooks/useUserData";
 import { TaskProgressTagLookup } from "@/components/TaskProgressTagLookup";
 import analytics from "@/lib/utils/analytics";
+import { useMediaQuery } from "@mui/material";
+import { MediaQueries } from "@/lib/PageSizes";
 
 interface Props {
   task: types.Task;
@@ -11,22 +13,26 @@ interface Props {
 
 export const Task = (props: Props): ReactElement => {
   const { userData } = useUserData();
+  const isTabletScreen = useMediaQuery(MediaQueries.tabletAndUp);
 
   const taskProgress = (userData?.taskProgress && userData.taskProgress[props.task.id]) || "NOT_STARTED";
 
   return (
-    <li>
-      <span className="margin-right-205">{TaskProgressTagLookup[taskProgress]}</span>
-      <Link href={`/tasks/${props.task.urlSlug}`} passHref>
-        <a
-          onClick={() => analytics.event.roadmap_task_title.click.go_to_task()}
-          href={`/tasks/${props.task.urlSlug}`}
-          className="usa-link"
-          data-task={props.task.id}
-        >
-          {props.task.name}
-        </a>
-      </Link>
+    <li className="margin-0">
+      <div className={`line-height-sans-2 flex ${isTabletScreen ? "margin-bottom-2" : "margin-bottom-1"}`}>
+        {isTabletScreen && <span className="margin-right-205">{TaskProgressTagLookup[taskProgress]}</span>}
+        <Link href={`/tasks/${props.task.urlSlug}`} passHref>
+          <a
+            onClick={() => analytics.event.roadmap_task_title.click.go_to_task()}
+            href={`/tasks/${props.task.urlSlug}`}
+            className="usa-link"
+            data-task={props.task.id}
+          >
+            {props.task.name}
+          </a>
+        </Link>
+      </div>
+      {!isTabletScreen && <div className="margin-bottom-2">{TaskProgressTagLookup[taskProgress]}</div>}
     </li>
   );
 };
