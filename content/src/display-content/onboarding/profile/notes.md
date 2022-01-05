f5cbc56be50102160966c0e96cebda496d10b3cb
Faraz Akhtar <fa8338@gmail.com>
2022-01-05 10:26:43 -0500

    feat: [#1065] updated font-weight to steps in onboarding flow

--- content/src/display-content/onboarding/profile/notes.md
+++ content/src/display-content/onboarding/profile/notes.md
@@ -1,4 +1,5 @@
 ---
+placeholder: "Enter notes"
 ---
 
 ## Notes
--- web/src/components/onboarding/OnboardingEmployerId.tsx
+++ web/src/components/onboarding/OnboardingEmployerId.tsx
@@ -2,7 +2,8 @@ import { OnboardingDefaults } from "@/display-defaults/onboarding/OnboardingDefa
 import { ProfileFieldErrorMap, ProfileFields } from "@/lib/types/types";
 import { displayAsEin } from "@/lib/utils/displayAsEin";
 import { templateEval } from "@/lib/utils/helpers";
-import React, { FocusEvent, ReactElement } from "react";
+import { ProfileDataContext } from "@/pages/onboarding";
+import React, { FocusEvent, ReactElement, useContext } from "react";
 import { OnboardingField } from "./OnboardingField";
 
 interface Props {
@@ -11,6 +12,8 @@ interface Props {
 }
 
 export const OnboardingEmployerId = (props: Props): ReactElement => {
+  const { state } = useContext(ProfileDataContext);
+
   const onValidation = (event: FocusEvent<HTMLInputElement>): void => {
     const valid = event.target.value.length === 10 || event.target.value.length === 0;
     props.onValidation(fieldName, !valid);
@@ -23,19 +26,25 @@ export const OnboardingEmployerId = (props: Props): ReactElement => {
   const dataFormat = (value: string): string => value.replace(/[^0-9]/g, "");
 
   return (
-    <OnboardingField
-      fieldName={fieldName}
-      onValidation={onValidation}
-      handleChange={handleChange}
-      error={props.fieldStates[fieldName].invalid}
-      validationText={templateEval(OnboardingDefaults.errorTextMinimumNumericField, {
-        length: "10",
-      })}
-      valueFilter={dataFormat}
-      visualFilter={displayAsEin}
-      fieldOptions={{
-        inputProps: { inputMode: "numeric", maxLength: "10" },
-      }}
-    />
+    <>
+      <div role="heading" aria-level={2} className="h3-styling margin-bottom-2">
+        {state.displayContent.employerId.headingBolded}{" "}
+        <span className="text-light">{state.displayContent.employerId.headingNotBolded}</span>
+      </div>
+      <OnboardingField
+        fieldName={fieldName}
+        onValidation={onValidation}
+        handleChange={handleChange}
+        error={props.fieldStates[fieldName].invalid}
+        validationText={templateEval(OnboardingDefaults.errorTextMinimumNumericField, {
+          length: "10",
+        })}
+        valueFilter={dataFormat}
+        visualFilter={displayAsEin}
+        fieldOptions={{
+          inputProps: { inputMode: "numeric", maxLength: "10" },
+        }}
+      />
+    </>
   );
 };
