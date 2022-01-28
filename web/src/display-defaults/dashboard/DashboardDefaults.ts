a50f1add38e03d360f800ecf76ee94f0f127e218
Cecilia R <86479586+CeciliaRam@users.noreply.github.com>
2022-01-28 16:10:11 -0500

    Update DashboardDefaults.ts (#1478)

--- web/src/display-defaults/dashboard/DashboardDefaults.ts
+++ web/src/display-defaults/dashboard/DashboardDefaults.ts
@@ -5,7 +5,7 @@ export const DashboardDefaults: Record<string, string> = {
   opportunitiesHeader: "Opportunities",
   fundingTagText: "Funding",
   certificationTagText: "Certification",
-  calendarLegalText: "This calendar does not comprise the entirety of your obligations as a business.",
+  calendarLegalText: "This calendar is not a complete list of your obligations as a business.",
   calendarHeader: "My Calendar",
   calendarTooltip:
     "This is your personalized calendar of upcoming of taxes and filings that the New Jersey Department of Treasury has identified. Click on any item to learn about the filing and how you can comply.",
