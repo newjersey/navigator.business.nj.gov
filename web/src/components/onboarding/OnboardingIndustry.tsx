86ccc8efdaa94f93dc100f4896002b057a6510ec
Faraz Akhtar <fa8338@gmail.com>
2022-02-09 10:08:56 -0500

    refactor: remove form control api from onboarding industry component

--- web/src/components/onboarding/OnboardingIndustry.tsx
+++ web/src/components/onboarding/OnboardingIndustry.tsx
@@ -4,7 +4,6 @@ import { OnboardingLiquorLicense } from "@/components/onboarding/OnboardingLiquo
 import { isLiquorLicenseApplicable } from "@/lib/domain-logic/isLiquorLicenseApplicable";
 import { setHeaderRole } from "@/lib/utils/helpers";
 import { ProfileDataContext } from "@/pages/onboarding";
-import { FormControl, InputLabel } from "@mui/material";
 import React, { ReactElement, useContext } from "react";
 
 interface Props {
@@ -20,12 +19,7 @@ export const OnboardingIndustry = ({ headerAriaLevel = 2 }: Props): ReactElement
     <>
       <Content overrides={{ h2: headerLevelTwo }}>{state.displayContent.industry.contentMd}</Content>
       <div className="form-input margin-top-2">
-        <FormControl fullWidth>
-          <InputLabel id="industry-label" className="visibility-hidden">
-            Industry
-          </InputLabel>
-          <IndustryDropdown />
-        </FormControl>
+        <IndustryDropdown />
 
         {state.profileData.industryId === "home-contractor" && (
           <div className="margin-top-2">
