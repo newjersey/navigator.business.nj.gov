464f74db4d6d5b3772f4e0529e20cb78da4fee82
Naman Agrawal <naman@innovation.nj.gov>
2022-02-15 09:49:33 -0500

    fix: align members modal buttons in mobile

--- web/src/components/tasks/business-formation/MembersModal.tsx
+++ web/src/components/tasks/business-formation/MembersModal.tsx
@@ -127,7 +127,6 @@ export const MembersModal = (props: Props): ReactElement => {
       data-testid={"members-modal"}
     >
       <DialogTitle sx={{ marginBottom: 2, padding: 3, paddingBottom: 0 }}>
-        {" "}
         <Content>{state.displayContent.membersModal.contentMd}</Content>
         <IconButton
           aria-label="close"
@@ -144,147 +143,147 @@ export const MembersModal = (props: Props): ReactElement => {
       </DialogTitle>
 
       <DialogContent dividers={true}>
-        <div>
-          <div className="form-input margin-bottom-1 margin-top-1">
-            {state.formationFormData.agentNumberOrManual === "MANUAL_ENTRY" ? (
-              <FormGroup className="padding-left-105">
-                <FormControlLabel
-                  label={state.displayContent.membersModal.sameNameCheckboxText}
-                  control={
-                    <Checkbox
-                      checked={useAgentAddress}
-                      onChange={(event) => checkBoxCheck(event.target.checked)}
-                    />
-                  }
-                />
-              </FormGroup>
-            ) : (
-              <></>
-            )}
+        <div className="form-input margin-bottom-1 margin-top-1">
+          {state.formationFormData.agentNumberOrManual === "MANUAL_ENTRY" ? (
+            <FormGroup className="padding-left-105">
+              <FormControlLabel
+                label={state.displayContent.membersModal.sameNameCheckboxText}
+                control={
+                  <Checkbox
+                    checked={useAgentAddress}
+                    onChange={(event) => checkBoxCheck(event.target.checked)}
+                  />
+                }
+              />
+            </FormGroup>
+          ) : (
+            <></>
+          )}
 
-            <form ref={formRef} data-testid="member" className="padding-top-1" onSubmit={onSubmit}>
-              <Content>{state.displayContent.memberName.contentMd}</Content>
-              <GenericTextField
-                value={memberData.name}
-                placeholder={state.displayContent.memberName.placeholder}
-                handleChange={(value: string) => setMemberData({ ...memberData, name: value })}
-                error={memberErrorMap["memberName"].invalid}
-                onValidation={onValidation}
-                validationText={BusinessFormationDefaults.nameErrorText}
-                fieldName="memberName"
-                required={true}
-                autoComplete="name"
-                disabled={useAgentAddress}
-              />
-              <Content>{state.displayContent.memberAddressLine1.contentMd}</Content>
-              <GenericTextField
-                fieldName="memberAddressLine1"
-                value={memberData.addressLine1}
-                placeholder={state.displayContent.memberAddressLine1.placeholder}
-                handleChange={(value: string) => setMemberData({ ...memberData, addressLine1: value })}
-                error={memberErrorMap["memberAddressLine1"].invalid}
-                onValidation={onValidation}
-                autoComplete="address-line1"
-                validationText={BusinessFormationDefaults.addressErrorText}
-                disabled={useAgentAddress}
-                required={true}
-              />
-              <Content
-                style={{ display: "inline" }}
-                overrides={{
-                  p: ({ children }: { children: string[] }): ReactElement => (
-                    <p style={{ display: "inline" }}>{children}</p>
-                  ),
-                }}
+          <form ref={formRef} data-testid="member" className="padding-top-1" onSubmit={onSubmit}>
+            <Content>{state.displayContent.memberName.contentMd}</Content>
+            <GenericTextField
+              value={memberData.name}
+              placeholder={state.displayContent.memberName.placeholder}
+              handleChange={(value: string) => setMemberData({ ...memberData, name: value })}
+              error={memberErrorMap["memberName"].invalid}
+              onValidation={onValidation}
+              validationText={BusinessFormationDefaults.nameErrorText}
+              fieldName="memberName"
+              required={true}
+              autoComplete="name"
+              disabled={useAgentAddress}
+            />
+            <Content>{state.displayContent.memberAddressLine1.contentMd}</Content>
+            <GenericTextField
+              fieldName="memberAddressLine1"
+              value={memberData.addressLine1}
+              placeholder={state.displayContent.memberAddressLine1.placeholder}
+              handleChange={(value: string) => setMemberData({ ...memberData, addressLine1: value })}
+              error={memberErrorMap["memberAddressLine1"].invalid}
+              onValidation={onValidation}
+              autoComplete="address-line1"
+              validationText={BusinessFormationDefaults.addressErrorText}
+              disabled={useAgentAddress}
+              required={true}
+            />
+            <Content
+              style={{ display: "inline" }}
+              overrides={{
+                p: ({ children }: { children: string[] }): ReactElement => (
+                  <p style={{ display: "inline" }}>{children}</p>
+                ),
+              }}
+            >
+              {state.displayContent.memberAddressLine2.contentMd}
+            </Content>
+            <h6 style={{ display: "inline", textTransform: "initial" }}>(Optional)</h6>
+            <GenericTextField
+              fieldName="memberAddressLine2"
+              value={memberData.addressLine2}
+              placeholder={state.displayContent.memberAddressLine2.placeholder}
+              disabled={useAgentAddress}
+              autoComplete="address-line2"
+              handleChange={(value: string) => setMemberData({ ...memberData, addressLine2: value })}
+            />
+            <div className="grid-row grid-gap-2">
+              <div className="grid-col-12 tablet:grid-col-6">
+                <Content>{state.displayContent.memberAddressCity.contentMd}</Content>
+                <GenericTextField
+                  fieldName="memberAddressCity"
+                  autoComplete="address-level2"
+                  value={memberData.addressCity}
+                  disabled={useAgentAddress}
+                  required={true}
+                  placeholder={state.displayContent.memberAddressCity.placeholder}
+                  handleChange={(value: string) => setMemberData({ ...memberData, addressCity: value })}
+                  error={memberErrorMap["memberAddressCity"].invalid}
+                  onValidation={onValidation}
+                  validationText={BusinessFormationDefaults.addressCityErrorText}
+                />
+              </div>
+              <div className="grid-col-6 tablet:grid-col-3">
+                <Content>{state.displayContent.memberAddressState.contentMd}</Content>
+                <StateDropdown
+                  fieldName="memberAddressState"
+                  value={memberData.addressState}
+                  placeholder={state.displayContent.memberAddressState.placeholder}
+                  validationText={BusinessFormationDefaults.addressStateErrorText}
+                  onSelect={(value: string | undefined) =>
+                    setMemberData({ ...memberData, addressState: value ?? "" })
+                  }
+                  error={memberErrorMap["memberAddressState"].invalid}
+                  autoComplete="address-level1"
+                  disabled={useAgentAddress}
+                  onValidation={onValidation}
+                  required={true}
+                />
+              </div>
+              <div className="grid-col-6 tablet:grid-col-3">
+                <Content>{state.displayContent.memberAddressZipCode.contentMd}</Content>
+                <GenericTextField
+                  numericProps={{
+                    maxLength: 5,
+                  }}
+                  disabled={useAgentAddress}
+                  fieldName={"memberAddressZipCode"}
+                  autoComplete="postal-code"
+                  error={memberErrorMap["memberAddressZipCode"].invalid}
+                  handleChange={(value: string) => setMemberData({ ...memberData, addressZipCode: value })}
+                  value={memberData.addressZipCode}
+                  validationText={BusinessFormationDefaults.addressZipCodeErrorText}
+                  onValidation={onValidation}
+                  required={true}
+                  placeholder={state.displayContent.memberAddressZipCode.placeholder}
+                />
+              </div>
+            </div>
+            <div className="margin-top-2">
+              <div
+                className="padding-3 bg-base-lightest flex flex-justify-end task-submit-button-background flex-wrap"
+                style={{ gap: "1rem" }}
               >
-                {state.displayContent.memberAddressLine2.contentMd}
-              </Content>{" "}
-              <h6 style={{ display: "inline", textTransform: "initial" }}>(Optional)</h6>
-              <GenericTextField
-                fieldName="memberAddressLine2"
-                value={memberData.addressLine2}
-                placeholder={state.displayContent.memberAddressLine2.placeholder}
-                disabled={useAgentAddress}
-                autoComplete="address-line2"
-                handleChange={(value: string) => setMemberData({ ...memberData, addressLine2: value })}
-              />
-              <div className="grid-row grid-gap-2">
-                <div className="grid-col-12 tablet:grid-col-6">
-                  <Content>{state.displayContent.memberAddressCity.contentMd}</Content>
-                  <GenericTextField
-                    fieldName="memberAddressCity"
-                    autoComplete="address-level2"
-                    value={memberData.addressCity}
-                    disabled={useAgentAddress}
-                    required={true}
-                    placeholder={state.displayContent.memberAddressCity.placeholder}
-                    handleChange={(value: string) => setMemberData({ ...memberData, addressCity: value })}
-                    error={memberErrorMap["memberAddressCity"].invalid}
-                    onValidation={onValidation}
-                    validationText={BusinessFormationDefaults.addressCityErrorText}
-                  />
-                </div>
-                <div className="grid-col-5 tablet:grid-col-3">
-                  <Content>{state.displayContent.memberAddressState.contentMd}</Content>
-                  <StateDropdown
-                    fieldName="memberAddressState"
-                    value={memberData.addressState}
-                    placeholder={state.displayContent.memberAddressState.placeholder}
-                    validationText={BusinessFormationDefaults.addressStateErrorText}
-                    onSelect={(value: string | undefined) =>
-                      setMemberData({ ...memberData, addressState: value ?? "" })
-                    }
-                    error={memberErrorMap["memberAddressState"].invalid}
-                    autoComplete="address-level1"
-                    disabled={useAgentAddress}
-                    onValidation={onValidation}
-                    required={true}
-                  />
-                </div>
-                <div className="grid-col-7 tablet:grid-col-3">
-                  <Content>{state.displayContent.memberAddressZipCode.contentMd}</Content>
-                  <GenericTextField
-                    numericProps={{
-                      maxLength: 5,
-                    }}
-                    disabled={useAgentAddress}
-                    fieldName={"memberAddressZipCode"}
-                    autoComplete="postal-code"
-                    error={memberErrorMap["memberAddressZipCode"].invalid}
-                    handleChange={(value: string) => setMemberData({ ...memberData, addressZipCode: value })}
-                    value={memberData.addressZipCode}
-                    validationText={BusinessFormationDefaults.addressZipCodeErrorText}
-                    onValidation={onValidation}
-                    required={true}
-                    placeholder={state.displayContent.memberAddressZipCode.placeholder}
-                  />
-                </div>
+                <Button
+                  style="secondary"
+                  onClick={props.handleClose}
+                  dataTestid="memberCancel"
+                  widthAutoOnMobile
+                  noRightMargin
+                >
+                  {BusinessFormationDefaults.membersModalBackButtonText}
+                </Button>
+                <Button
+                  style="primary"
+                  typeSubmit={true}
+                  dataTestid="memberSubmit"
+                  noRightMargin
+                  widthAutoOnMobile
+                >
+                  <span className="text-no-wrap">{BusinessFormationDefaults.membersModalNextButtonText}</span>
+                </Button>
               </div>
-              <div className="margin-top-2">
-                <div className="padding-3 bg-base-lightest flex flex-justify-end task-submit-button-background flex-wrap">
-                  <Button
-                    style="secondary"
-                    onClick={props.handleClose}
-                    dataTestid={"memberCancel"}
-                    widthAutoOnMobile
-                  >
-                    {BusinessFormationDefaults.membersModalBackButtonText}
-                  </Button>
-                  <Button
-                    style="primary"
-                    typeSubmit={true}
-                    dataTestid={"memberSubmit"}
-                    noRightMargin
-                    widthAutoOnMobile
-                  >
-                    <span className="text-no-wrap">
-                      {BusinessFormationDefaults.membersModalNextButtonText}
-                    </span>
-                  </Button>
-                </div>
-              </div>
-            </form>
-          </div>
+            </div>
+          </form>
         </div>
       </DialogContent>
     </Dialog>
