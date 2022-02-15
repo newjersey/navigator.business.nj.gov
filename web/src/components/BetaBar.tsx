661cb3731fb62da4fd326a71520503a4c1ea8cb4
Dave Sueltenfuss <dsueltenfuss@fearless.tech>
2022-02-15 10:00:48 -0500

    fix: [#1439] update beta bar color and remove tag

--- web/src/components/BetaBar.tsx
+++ web/src/components/BetaBar.tsx
@@ -1,14 +1,10 @@
-import { Tag } from "@/components/njwds-extended/Tag";
 import { Beta } from "@/display-defaults/Beta";
 import React, { ReactElement } from "react";
 
 export const BetaBar = (): ReactElement => {
   return (
-    <div className="display-flex flex-justify-center flex-align-center bg-base-lightest font-sans-xs minh-3 margin-auto width-full padding-y-2">
-      <Tag tagVariant="accent" bold={true}>
-        {Beta.betaIconText}
-      </Tag>
-      <span className="margin-left-05">
+    <div className="display-flex flex-justify-center flex-align-center bg-beta font-sans-xs minh-3 margin-auto width-full padding-y-2">
+      <span>
         {Beta.betaMainText}
         <a className="usa-link" href={Beta.betaFormLink} target="_blank" rel="noreferrer">
           {Beta.betaFormText}
--- web/src/display-defaults/Beta.ts
+++ web/src/display-defaults/Beta.ts
@@ -1,5 +1,4 @@
 export const Beta: Record<string, string> = {
-  betaIconText: "Beta",
   betaMainText: "This site is in beta. ",
   betaFormText: "Let us know what you think!",
   betaFormLink: "https://forms.business.nj.gov/feedback/",
