30d085751eb05f06e46c1b420889ad96c25fdd44
Dave Sueltenfuss <dsueltenfuss@fearless.tech>
2022-02-16 10:19:18 -0500

    chore: [#1439] add margin border to beta text

--- web/src/components/BetaBar.tsx
+++ web/src/components/BetaBar.tsx
@@ -4,7 +4,7 @@ import React, { ReactElement } from "react";
 export const BetaBar = (): ReactElement => {
   return (
     <div className="display-flex flex-justify-center flex-align-center bg-beta font-sans-xs minh-3 margin-auto width-full padding-y-2">
-      <span>
+      <span className="margin-left-2 margin-right-2">
         {Beta.betaMainText}
         <a className="usa-link" href={Beta.betaFormLink} target="_blank" rel="noreferrer">
           {Beta.betaFormText}
