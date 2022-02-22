198ada6c06b542c76eb5974d702661f48f31e421
Naman Agrawal <naman@innovation.nj.gov>
2022-02-22 13:49:55 -0500

    feat: [#1672] update formatting for task blockquote and cta

--- web/src/components/TaskCTA.tsx
+++ web/src/components/TaskCTA.tsx
@@ -14,16 +14,18 @@ export const TaskCTA = (props: Props): ReactElement => {
   }
 
   return (
-    <a href={props.link} target="_blank" rel="noreferrer noopener" className="margin-left-auto">
-      <div className="margin-top-4 margin-bottom-1">
-        <Button
-          style="primary"
-          noRightMargin
-          onClick={analytics.event.task_primary_call_to_action.click.open_external_website}
-        >
-          {props.text || TaskDefaults.defaultCallToActionText}
-        </Button>
+    <div className="flex flex-justify-end margin-top-3 padding-3 margin-x-neg-3 margin-bottom-neg-3 bg-base-lightest">
+      <div>
+        <a href={props.link} target="_blank" rel="noreferrer noopener">
+          <Button
+            style="primary"
+            noRightMargin
+            onClick={analytics.event.task_primary_call_to_action.click.open_external_website}
+          >
+            {props.text || TaskDefaults.defaultCallToActionText}
+          </Button>
+        </a>
       </div>
-    </a>
+    </div>
   );
 };
