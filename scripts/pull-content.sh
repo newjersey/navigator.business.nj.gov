fdf7d7283268e3719d67fce171405f2434c20630
Michael Deeb <michael.f.deeb@gmail.com>
2022-02-22 13:33:12 -0500

    chore: moving over git subtree scripts

---
+++ scripts/pull-content.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+cd $(git rev-parse --show-toplevel)
+
+TAG="$@";
+
+if [ -z $TAG ]; then
+    TAG="main";
+fi
+
+git subtree pull --prefix content git@github.com:newjersey/navigator-content.git $TAG --squash
