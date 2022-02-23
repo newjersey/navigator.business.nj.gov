d40b192aaa76e98f0c07f77147e8d75db48f9f03
Michael Deeb <michael.f.deeb@gmail.com>
2022-02-23 13:24:59 -0500

    chore: [#1648] adds sorting to funding oppertunity display order

--- web/src/lib/domain-logic/filterFundings.ts
+++ web/src/lib/domain-logic/filterFundings.ts
@@ -1,39 +1,42 @@
-import { County, Funding } from "@/lib/types/types";
+import { County, Funding, FundingStatusOrder } from "@/lib/types/types";
 import { UserData } from "@businessnjgovnavigator/shared";
 
 export const filterFundings = (fundings: Funding[], userData: UserData): Funding[] => {
-  return fundings.filter((it) => {
-    let allowedFunding = true;
-    if (userData.profileData.homeBasedBusiness && it.homeBased !== "yes" && it.homeBased !== "unknown") {
-      allowedFunding = false;
-    }
+  const filtered = fundings.filter((it) => {
+    if (it.publishStageArchive === "Do Not Publish") return false;
+
+    if (userData.profileData.homeBasedBusiness && it.homeBased !== "yes" && it.homeBased !== "unknown")
+      return false;
 
     if (userData.profileData.municipality && it.county.length > 0) {
       if (
         !it.county.includes("All") &&
         !it.county.includes(userData.profileData.municipality.county as County)
-      ) {
-        allowedFunding = false;
-      }
+      )
+        return false;
     }
-
     if (userData.profileData.sectorId && it.sector.length > 0) {
       const sectorRegex = new RegExp(it.sector.join("|"), "i");
-      if (!sectorRegex.test(userData.profileData.sectorId)) {
-        allowedFunding = false;
-      }
+      if (!sectorRegex.test(userData.profileData.sectorId)) return false;
     }
 
     if (userData.profileData.existingEmployees && it.employeesRequired) {
-      if (parseInt(userData.profileData.existingEmployees) === 0 && it.employeesRequired !== "n/a") {
-        allowedFunding = false;
-      }
+      if (parseInt(userData.profileData.existingEmployees) === 0 && it.employeesRequired !== "n/a")
+        return false;
     }
 
-    if (it.publishStageArchive === "Do Not Publish") {
-      allowedFunding = false;
-    }
+    if (it.status === "closed") return false;
 
-    return allowedFunding;
+    return true;
+  });
+
+  return filtered.sort((a, b) => {
+    const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+    const nameB = b.name.toUpperCase();
+    if (FundingStatusOrder[a.status] < FundingStatusOrder[b.status]) return -1;
+    else if (FundingStatusOrder[a.status] > FundingStatusOrder[b.status]) return 1;
+    else if (nameA < nameB) return -1;
+    else if (nameA > nameB) return 1;
+    return 0;
   });
 };
