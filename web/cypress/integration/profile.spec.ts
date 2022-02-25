2af613a2cd53878823268eab1d7c935eb25d2380
Faraz Akhtar <fa8338@gmail.com>
2022-02-25 14:26:46 -0500

    test: cypress view profile page to ensure fields exist

--- web/cypress/integration/profile.spec.ts
+++ web/cypress/integration/profile.spec.ts
@@ -1,73 +1,113 @@
 /* eslint-disable cypress/no-unnecessary-waiting */
 
-import { completeNewBusinessOnboarding, randomInt } from "../support/helpers";
-import { onProfilePage } from "../support/page_objects/profilePage";
-import { onRoadmapPage } from "../support/page_objects/roadmapPage";
+import { Industries, Industry, LegalStructure } from "@businessnjgovnavigator/shared/";
+import {
+  checkProfilePage,
+  completeNewBusinessOnboarding,
+  homeBasedIndustries,
+  industriesNotHomeBasedOrLiquorLicense,
+  legalStructureWithTradeName,
+  liquorLicenseIndustries,
+  randomElementFromArray,
+  randomInt,
+} from "../support/helpers";
 
 describe("Profile [feature] [all] [group1]", () => {
   beforeEach(() => {
     cy.resetUserData();
     cy.loginByCognitoApi();
   });
-  it("navigates to profile page and updates all available fields", () => {
-    const businessName = `Generic Business Name ${randomInt()}`;
-    const industryId = "generic";
-    const companyType = "general-partnership";
-    const location = "Absecon";
-    const homeBasedQuestion = false;
-    const liquorLicenseQuestion = undefined;
 
-    const newBusinessName = `Generic Business Name ${randomInt()}`;
-    const newIndustryId = "restaurant";
-    const newLiquorLicenseQuestion = true;
-    const newCompanyType = "limited-liability-partnership";
-    const newLocation = "Atlantic";
-    const newEmployerId = randomInt(9).toString();
-    const newNotes = `Notes ${randomInt()}`;
-    const newEntityId = randomInt(10).toString();
-    const newTaxId = randomInt(9).toString();
+  describe("navigates to profile page and checks all fields", () => {
+    it("random industry", () => {
+      const industry = randomElementFromArray(industriesNotHomeBasedOrLiquorLicense as Industry[]);
+      const businessName = `Generic Business Name ${randomInt()}`;
+      const industryId = industry.id;
+      const companyType = "general-partnership";
+      const location = "Absecon";
+      const homeBasedQuestion = industry.canBeHomeBased === false ? undefined : Boolean(randomInt() % 2);
+      const liquorLicenseQuestion =
+        industry.isLiquorLicenseApplicable === false ? undefined : Boolean(randomInt() % 2);
 
-    completeNewBusinessOnboarding(
-      businessName,
-      industryId,
-      companyType,
-      location,
-      homeBasedQuestion,
-      liquorLicenseQuestion
-    );
+      completeNewBusinessOnboarding(
+        businessName,
+        industryId,
+        companyType,
+        location,
+        homeBasedQuestion,
+        liquorLicenseQuestion
+      );
 
-    cy.url().should("contain", "/roadmap");
-    onRoadmapPage.clickEditProfileLink();
+      checkProfilePage(companyType, homeBasedQuestion, liquorLicenseQuestion);
+    });
 
-    onProfilePage.getBusinessName().should("exist");
-    onProfilePage.getIndustryDropdown().should("exist");
-    onProfilePage.getLiquorLicense().should("not.exist");
-    onProfilePage.getLegalStructure().should("exist");
-    onProfilePage.getLocationDropdown().should("exist");
-    onProfilePage.getHomeBased(false).should("be.checked");
-    onProfilePage.getEmployerId().should("exist");
-    onProfilePage.getTaxId().should("exist");
-    onProfilePage.getNotes().should("exist");
-    onProfilePage.getEntityId().should("not.exist");
+    it("random homebased industry", () => {
+      const industry = randomElementFromArray(homeBasedIndustries as Industry[]);
+      const businessName = `Generic Business Name ${randomInt()}`;
+      const industryId = industry.id;
+      const companyType = "general-partnership";
+      const location = "Absecon";
+      const homeBasedQuestion = industry.canBeHomeBased === false ? undefined : Boolean(randomInt() % 2);
+      const liquorLicenseQuestion =
+        industry.isLiquorLicenseApplicable === false ? undefined : Boolean(randomInt() % 2);
 
-    // update profile page
-    onProfilePage.selectHomeBased(true);
-    onProfilePage.getBusinessName().clear();
-    onProfilePage.typeBusinessName(newBusinessName);
-    onProfilePage.selectIndustry(newIndustryId);
-    onProfilePage.getHomeBased().should("not.exist");
-    onProfilePage.getLiquorLicense(false).should("be.checked");
-    onProfilePage.selectLiquorLicense(newLiquorLicenseQuestion);
-    onProfilePage.getLiquorLicense(false).should("not.be.checked");
-    onProfilePage.getLiquorLicense(true).should("be.checked");
-    onProfilePage.selectLegalStructure(newCompanyType);
-    onProfilePage.selectLocation(newLocation);
-    onProfilePage.typeEntityId(newEntityId);
-    onProfilePage.typeEmployerId(newEmployerId);
-    onProfilePage.typeNotes(newNotes);
-    onProfilePage.typeTaxId(newTaxId);
+      completeNewBusinessOnboarding(
+        businessName,
+        industryId,
+        companyType,
+        location,
+        homeBasedQuestion,
+        liquorLicenseQuestion
+      );
 
-    onProfilePage.clickSaveButton();
-    cy.url().should("contain", "/roadmap");
+      checkProfilePage(companyType, homeBasedQuestion, liquorLicenseQuestion);
+    });
+
+    it("random liquor license industry", () => {
+      const industry = randomElementFromArray(liquorLicenseIndustries as Industry[]);
+      const businessName = `Generic Business Name ${randomInt()}`;
+      const industryId = industry.id;
+      const companyType = "general-partnership";
+      const location = "Absecon";
+      const homeBasedQuestion = industry.canBeHomeBased === false ? undefined : Boolean(randomInt() % 2);
+      const liquorLicenseQuestion =
+        industry.isLiquorLicenseApplicable === false ? undefined : Boolean(randomInt() % 2);
+
+      completeNewBusinessOnboarding(
+        businessName,
+        industryId,
+        companyType,
+        location,
+        homeBasedQuestion,
+        liquorLicenseQuestion
+      );
+
+      checkProfilePage(companyType, homeBasedQuestion, liquorLicenseQuestion);
+    });
+
+    it("random industry with company type that has trade name (entity id)", () => {
+      const industry = randomElementFromArray(Industries as Industry[]);
+      const companyTypeWithTradeName = randomElementFromArray(
+        legalStructureWithTradeName as LegalStructure[]
+      );
+      const businessName = `Generic Business Name ${randomInt()}`;
+      const industryId = industry.id;
+      const companyType = companyTypeWithTradeName.id;
+      const location = "Absecon";
+      const homeBasedQuestion = industry.canBeHomeBased === false ? undefined : Boolean(randomInt() % 2);
+      const liquorLicenseQuestion =
+        industry.isLiquorLicenseApplicable === false ? undefined : Boolean(randomInt() % 2);
+
+      completeNewBusinessOnboarding(
+        businessName,
+        industryId,
+        companyType,
+        location,
+        homeBasedQuestion,
+        liquorLicenseQuestion
+      );
+
+      checkProfilePage(companyType, homeBasedQuestion, liquorLicenseQuestion);
+    });
   });
 });
--- web/cypress/support/helpers.ts
+++ web/cypress/support/helpers.ts
@@ -1,5 +1,8 @@
-import { LookupIndustryById } from "@businessnjgovnavigator/shared";
+import { isEntityIdApplicable } from "@/lib/domain-logic/isEntityIdApplicable";
+import { Industries, LegalStructures, LookupIndustryById } from "@businessnjgovnavigator/shared";
 import { onOnboardingPage } from "./page_objects/onboardingPage";
+import { onProfilePage } from "./page_objects/profilePage";
+import { onRoadmapPage } from "./page_objects/roadmapPage";
 
 /* eslint-disable cypress/no-unnecessary-waiting */
 export const clickNext = (): void => {
@@ -26,56 +29,6 @@ export const clickTask = (taskId: string): void => {
   const taskValue = `[data-task="${taskId}"]`;
   cy.get(taskValue).click({ force: true });
   cy.wait(1000);
-};
-
-export const randomInt = (length = 8): number =>
-  Math.floor(
-    Math.pow(10, length - 1) + Math.random() * (Math.pow(10, length) - Math.pow(10, length - 1) - 1)
-  );
-
-export const completeNewBusinessOnboarding = (
-  businessName: string,
-  industry: string,
-  companyType: string,
-  city: string,
-  homeBased: boolean | undefined,
-  liquor: boolean | undefined
-): void => {
-  cy.url().should("include", `onboarding?page=${1}`);
-  onOnboardingPage.selectNewBusiness(false);
-  onOnboardingPage.getHasExistingBusiness(true).should("not.be.checked");
-  onOnboardingPage.clickNext();
-
-  cy.url().should("include", `onboarding?page=${2}`);
-  onOnboardingPage.typeBusinessName(businessName);
-  onOnboardingPage.clickNext();
-
-  cy.url().should("include", `onboarding?page=${3}`);
-  onOnboardingPage.selectIndustry(industry);
-
-  if (liquor === undefined) {
-    onOnboardingPage.getLiquorLicense().should("not.exist");
-  } else {
-    onOnboardingPage.selectLiquorLicense(liquor);
-  }
-
-  onOnboardingPage.clickNext();
-
-  cy.url().should("include", `onboarding?page=${4}`);
-  onOnboardingPage.selectLegalStructure(companyType);
-  onOnboardingPage.clickNext();
-
-  cy.url().should("include", `onboarding?page=${5}`);
-  onOnboardingPage.selectLocation(city);
-
-  if (homeBased === undefined) {
-    onOnboardingPage.getHomeBased().should("not.exist");
-  } else {
-    onOnboardingPage.selectHomeBased(homeBased);
-  }
-
-  onOnboardingPage.clickNext();
-  cy.url().should("include", `roadmap`);
 };
 
 export const completeOnboarding = (
@@ -156,3 +109,119 @@ export interface Pa11yThresholds {
   ignore?: string[];
   runners?: string[];
 }
+
+export const randomInt = (length = 8): number =>
+  Math.floor(
+    Math.pow(10, length - 1) + Math.random() * (Math.pow(10, length) - Math.pow(10, length - 1) - 1)
+  );
+
+export const randomElementFromArray = (array: any[]) => {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+export const homeBasedIndustries = Industries.filter((industry) => {
+  return industry.canBeHomeBased;
+});
+
+export const liquorLicenseIndustries = Industries.filter((industry) => {
+  return industry.isLiquorLicenseApplicable;
+});
+
+export const industriesNotHomeBasedOrLiquorLicense = Industries.filter((industry) => {
+  return !industry.canBeHomeBased && !industry.isLiquorLicenseApplicable;
+});
+
+export const legalStructureWithTradeName = LegalStructures.filter((legalStructure) => {
+  return legalStructure.hasTradeName;
+});
+
+export const checkProfilePage = (
+  companyType: string,
+  homeBasedQuestion: boolean | undefined,
+  liquorLicenseQuestion: boolean | undefined
+) => {
+  cy.url().should("contain", "/roadmap");
+  onRoadmapPage.clickEditProfileLink();
+
+  onProfilePage.getBusinessName().should("exist");
+  onProfilePage.getIndustryDropdown().should("exist");
+  onProfilePage.getLegalStructure().should("exist");
+  onProfilePage.getLocationDropdown().should("exist");
+  onProfilePage.getEmployerId().should("exist");
+  onProfilePage.getTaxId().should("exist");
+  onProfilePage.getNotes().should("exist");
+
+  if (isEntityIdApplicable(companyType)) {
+    onProfilePage.getEntityId().should("exist");
+  } else {
+    onProfilePage.getEntityId().should("not.exist");
+  }
+
+  if (homeBasedQuestion === undefined) {
+    onProfilePage.getHomeBased().should("not.exist");
+  } else if (homeBasedQuestion === true) {
+    onProfilePage.getHomeBased(true).should("be.checked");
+    onProfilePage.getHomeBased(false).should("not.be.checked");
+  } else {
+    onProfilePage.getHomeBased(true).should("not.be.checked");
+    onProfilePage.getHomeBased(false).should("be.checked");
+  }
+
+  if (liquorLicenseQuestion === undefined) {
+    onProfilePage.getLiquorLicense().should("not.exist");
+  } else if (liquorLicenseQuestion === true) {
+    onProfilePage.getLiquorLicense(true).should("be.checked");
+    onProfilePage.getLiquorLicense(false).should("not.be.checked");
+  } else {
+    onProfilePage.getLiquorLicense(true).should("not.be.checked");
+    onProfilePage.getLiquorLicense(false).should("be.checked");
+  }
+
+  onProfilePage.clickSaveButton();
+  cy.url().should("contain", "/roadmap");
+};
+
+export const completeNewBusinessOnboarding = (
+  businessName: string,
+  industry: string,
+  companyType: string,
+  city: string,
+  homeBased: boolean | undefined,
+  liquor: boolean | undefined
+): void => {
+  cy.url().should("include", `onboarding?page=${1}`);
+  onOnboardingPage.selectNewBusiness(false);
+  onOnboardingPage.getHasExistingBusiness(true).should("not.be.checked");
+  onOnboardingPage.clickNext();
+
+  cy.url().should("include", `onboarding?page=${2}`);
+  onOnboardingPage.typeBusinessName(businessName);
+  onOnboardingPage.clickNext();
+
+  cy.url().should("include", `onboarding?page=${3}`);
+  onOnboardingPage.selectIndustry(industry);
+
+  if (liquor === undefined) {
+    onOnboardingPage.getLiquorLicense().should("not.exist");
+  } else {
+    onOnboardingPage.selectLiquorLicense(liquor);
+  }
+
+  onOnboardingPage.clickNext();
+
+  cy.url().should("include", `onboarding?page=${4}`);
+  onOnboardingPage.selectLegalStructure(companyType);
+  onOnboardingPage.clickNext();
+
+  cy.url().should("include", `onboarding?page=${5}`);
+  onOnboardingPage.selectLocation(city);
+
+  if (homeBased === undefined) {
+    onOnboardingPage.getHomeBased().should("not.exist");
+  } else {
+    onOnboardingPage.selectHomeBased(homeBased);
+  }
+
+  onOnboardingPage.clickNext();
+  cy.url().should("include", `roadmap`);
+};
