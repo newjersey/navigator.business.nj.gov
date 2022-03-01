3507c87f09d5715e7f142b244a9a5126575f12b0
Faraz Akhtar <fa8338@gmail.com>
2022-03-01 13:01:37 -0500

    chore: added structure to files folder

--- web/.storybook/preview.js
+++ web/.storybook/preview.js
@@ -1,6 +1,6 @@
 require("njwds/dist/css/styles.css");
-require("../src/styles/global.scss");
-require("../src/styles/members.module.scss");
+require("../src/styles/main.scss");
+require("../src/styles/sections/members.module.scss");
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
