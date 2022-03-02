62e672f055d5508a98e03b9ee6dbb33e5b753b96
Dave Sueltenfuss <dsueltenfuss@fearless.tech>
2022-03-02 07:11:11 -0500

    build: update packages to the latest version

--- web/test/pages/onboarding/onboarding-owning.test.tsx
+++ web/test/pages/onboarding/onboarding-owning.test.tsx
@@ -1,11 +1,6 @@
 import * as api from "@/lib/api-client/apiClient";
 import { templateEval } from "@/lib/utils/helpers";
-import {
-  generateMunicipality,
-  generateProfileData as generateProfileData,
-  generateUser,
-  generateUserData,
-} from "@/test/factories";
+import { generateMunicipality, generateProfileData, generateUser, generateUserData } from "@/test/factories";
 import * as mockRouter from "@/test/mock/mockRouter";
 import { useMockRouter } from "@/test/mock/mockRouter";
 import { currentUserData, setupStatefulUserDataContext } from "@/test/mock/withStatefulUserData";
