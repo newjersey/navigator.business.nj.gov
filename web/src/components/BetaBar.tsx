16397e8a88b147dc981a862901dddaa8be74d0d9
Anne LoVerso <anne.loverso@gmail.com>
2022-03-02 10:12:14 -0500

    fix: add space for beta bar

--- web/src/components/BetaBar.tsx
+++ web/src/components/BetaBar.tsx
@@ -5,7 +5,7 @@ export const BetaBar = (): ReactElement => {
   return (
     <div className="display-flex flex-justify-center flex-align-center bg-beta font-sans-xs minh-3 margin-auto width-full padding-y-2">
       <span className="margin-left-2 margin-right-2">
-        {Config.betaBar.betaMainText}
+        {Config.betaBar.betaMainText}{" "}
         <a className="usa-link" href={Config.betaBar.betaFormLink} target="_blank" rel="noreferrer">
           {Config.betaBar.betaFormText}
         </a>
