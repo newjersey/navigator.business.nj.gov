7d90f5002b1d9f0317263685c7d485b53102e100
Anne LoVerso <anne.loverso@gmail.com>
2022-03-23 11:12:19 -0400

    feat: [#2078] default formation notifications to be checked-off

--- web/src/components/tasks/BusinessFormation.test.tsx
+++ web/src/components/tasks/BusinessFormation.test.tsx
@@ -480,7 +480,6 @@ describe("<BusinessFormation />", () => {
       fillText("Contact last name", "Smith");
       fillText("Contact phone number", "123A45a678 90");
       fireEvent.click(subject.getByLabelText("Credit card"));
-      selectCheckBox(Config.businessFormationDefaults.optInAnnualReportText);
       selectCheckBox(Config.businessFormationDefaults.optInCorpWatchText);
       selectCheckBox("Certificate of standing");
       selectCheckBox("Certified copy of formation document");
@@ -525,7 +524,7 @@ describe("<BusinessFormation />", () => {
         expect(formationFormData.certifiedCopyOfFormationDocument).toEqual(true);
         expect(subject.getByText("$200.00")).toBeInTheDocument();
         expect(formationFormData.annualReportNotification).toEqual(true);
-        expect(formationFormData.corpWatchNotification).toEqual(true);
+        expect(formationFormData.corpWatchNotification).toEqual(false);
       });
     });
 
