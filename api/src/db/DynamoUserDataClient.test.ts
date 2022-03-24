885de145ce14a1d0930401f9c5c5a5e3bfcfc5b2
Dave Sueltenfuss <dsueltenfuss@fearless.tech>
2022-03-24 14:56:30 -0400

    refactor: remove some old unused code

--- api/src/db/DynamoUserDataClient.test.ts
+++ api/src/db/DynamoUserDataClient.test.ts
@@ -3,7 +3,6 @@ import AWS from "aws-sdk";
 import { generateUser, generateUserData } from "../../test/factories";
 import { UserDataClient } from "../domain/types";
 import { DynamoUserDataClient } from "./DynamoUserDataClient";
-// import { clearTable } from "../../test/dynamodb-utils";
 
 // references jest-dynalite-config values
 const dbConfig = {
@@ -44,64 +43,3 @@ describe("DynamoUserDataClient", () => {
     expect(await dynamoUserDataClient.findByEmail("email@example.com")).toEqual(userData);
   });
 });
-
-// describe("DynamoQlUserDataClient", () => {
-//   const config = {
-//     endpoint: "http://localhost:8004",
-//     sslEnabled: false,
-//     convertEmptyValues: true,
-//     region: "local",
-//   };
-
-//   let client: AWS.DynamoDB;
-//   let documentClient: AWS.DynamoDB.DocumentClient;
-//   let userDataQlClient: UserDataQlClient;
-//   let userDataDocumentClient: UserDataClient;
-//   const userData1 = generateUserData({ user: generateUser({ id: "some-id1", receiveNewsletter: false }) });
-//   const userData2 = generateUserData({
-//     user: generateUser({
-//       id: "some-id2",
-//       receiveNewsletter: true,
-//       externalStatus: { newsletter: { success: true, status: "SUCCESS" } },
-//     }),
-//   });
-//   const userData3 = generateUserData({
-//     user: generateUser({
-//       id: "some-id3",
-//       receiveNewsletter: true,
-//       externalStatus: { newsletter: { success: false, status: "RESPONSE_ERROR" } },
-//     }),
-//   });
-//   const userData4 = generateUserData({
-//     user: generateUser({ id: "some-id4", receiveNewsletter: true, externalStatus: {} }),
-//   });
-
-//   beforeEach(async () => {
-//     client = new AWS.DynamoDB(config);
-//     userDataQlClient = DynamoQlUserDataClient(client, dbConfig.tableName);
-//     documentClient = new AWS.DynamoDB.DocumentClient(config);
-//     userDataDocumentClient = DynamoUserDataClient(documentClient, dbConfig.tableName);
-//     await clearTable(client, dbConfig.tableName);
-//   });
-
-//   it("gets inserted items", async () => {
-//     expect(await userDataQlClient.search(`SELECT data FROM "${dbConfig.tableName}"`)).toEqual([]);
-//     await userDataDocumentClient.put(userData1);
-//     await userDataDocumentClient.put(userData2);
-//     await userDataDocumentClient.put(userData3);
-//     await userDataDocumentClient.put(userData4);
-//     const response = await userDataQlClient.search(`SELECT data FROM "${dbConfig.tableName}"`);
-//     expect(new Set(response)).toEqual(new Set([userData1, userData2, userData3, userData4]));
-//     expect(response.length).toBe(4);
-//   });
-//   it("gets valid newsletter users", async () => {
-//     expect(await userDataQlClient.getNeedNewsletterUsers()).toEqual([]);
-//     await userDataDocumentClient.put(userData1);
-//     await userDataDocumentClient.put(userData2);
-//     await userDataDocumentClient.put(userData3);
-//     await userDataDocumentClient.put(userData4);
-//     const response = await userDataQlClient.getNeedNewsletterUsers();
-//     expect(new Set(response)).toEqual(new Set([userData3, userData4]));
-//     expect(response.length).toBe(2);
-//   });
-// });
