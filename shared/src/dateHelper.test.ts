5f3a4e283ebba4a84493bcd92c3775e76eab55ed
Dave Sueltenfuss <dsueltenfuss@fearless.tech>
2022-04-20 21:13:58 -0400

    test: delete a temp test file

--- shared/src/dateHelper.test.ts
+++
@@ -1,7 +0,0 @@
-import { getCurrentDateFormatted } from "./dateHelpers";
-
-describe("dateHelper Tests", () => {
-  it("returns the formatted date", () => {
-    expect(getCurrentDateFormatted("YYYY-MM-DD")).toBe("2022-04-20");
-  });
-});
