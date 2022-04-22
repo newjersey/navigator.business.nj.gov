b1c1dae30b79b3169db5785062edb41201bc4dab
Dave Sueltenfuss <dsueltenfuss@fearless.tech>
2022-04-22 13:01:57 -0400

    test: add dateHelper tests

---
+++ shared/src/dateHelpers.test.ts
@@ -0,0 +1,17 @@
+import dayjs from "dayjs";
+import { parseDate, parseDateWithFormat } from "./dateHelpers";
+
+describe("dateHelpers", () => {
+  describe("parseDate", () => {
+    it("correctly parses the date", () => {
+      const value = parseDate("2001-01-01");
+      expect(value).toStrictEqual(dayjs("2001-01-01"));
+    });
+  });
+  describe("parseDateWithFormat", () => {
+    it("correctly parses the date", () => {
+      const value = parseDateWithFormat("01-02-2010", "MM-DD-YYYY");
+      expect(value).toStrictEqual(dayjs("2010-01-02"));
+    });
+  });
+});
