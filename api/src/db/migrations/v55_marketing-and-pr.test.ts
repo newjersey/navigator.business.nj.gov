aea72447262b2b2bb4eb96083be377c4c9cf6e77
Dave Sueltenfuss <dsueltenfuss@fearless.tech>
2022-05-05 09:32:57 -0400

    fix: [#2600] merge pr and marketing industries

---
+++ api/src/db/migrations/v55_marketing-and-pr.test.ts
@@ -0,0 +1,64 @@
+import {
+  generatev54FormationFormData,
+  generatev54ProfileData,
+  generatev54User,
+  v54ProfileData,
+  v54UserData,
+} from "./v54_add_business_purpose";
+import { migrate_v54_to_v55 } from "./v55_marketing-and-pr";
+
+describe("migrate_v54_to_v55", () => {
+  const user = generatev54User({});
+  const formProgress = "UNSTARTED";
+  const taskProgress = {};
+
+  it("does not change auto-body-repair", () => {
+    const profileData = generatev54ProfileData({ industryId: "auto-body-repair" });
+    const v54 = makeUserData(profileData);
+    const v55 = migrate_v54_to_v55(v54);
+
+    expect(v55.profileData.industryId).toBe("auto-body-repair");
+  });
+
+  it("changes marketing-consulting to marketing-pr-consulting", () => {
+    const profileData = generatev54ProfileData({ industryId: "marketing-consulting" });
+    const v54 = makeUserData(profileData);
+    const v55 = migrate_v54_to_v55(v54);
+
+    expect(v55.profileData.industryId).toBe("marketing-pr-consulting");
+  });
+
+  it("changes pr-consultant to marketing-pr-consulting", () => {
+    const profileData = generatev54ProfileData({ industryId: "pr-consultant" });
+    const v54 = makeUserData(profileData);
+    const v55 = migrate_v54_to_v55(v54);
+
+    expect(v55.profileData.industryId).toBe("marketing-pr-consulting");
+  });
+
+  const makeUserData = (profileData: v54ProfileData): v54UserData => {
+    return {
+      user,
+      profileData,
+      formProgress,
+      taskProgress,
+      licenseData: undefined,
+      preferences: {
+        roadmapOpenSections: ["PLAN", "START"],
+        roadmapOpenSteps: [],
+        hiddenCertificationIds: [],
+        hiddenFundingIds: [],
+      },
+      taskItemChecklist: {},
+      taxFilingData: {
+        filings: [],
+      },
+      formationData: {
+        formationFormData: generatev54FormationFormData({}),
+        formationResponse: undefined,
+        getFilingResponse: undefined,
+      },
+      version: 53,
+    };
+  };
+});
