37e020e5e2314fcc40675fedf49224373330d73e
Dave Sueltenfuss <dsueltenfuss@fearless.tech>
2022-05-10 07:57:22 -0400

    test: increase wait for smoke test page

--- web/cypress/integration/login-smoke.spec.ts
+++ web/cypress/integration/login-smoke.spec.ts
@@ -3,7 +3,7 @@
 describe("check login page [smoke]", () => {
   beforeEach(() => {
     cy.visit("/");
-    cy.wait(1000);
+    cy.wait(5000);
   });
   it("loads properly", () => {
     cy.get('button[data-testid="login-button"]').should("exist");
