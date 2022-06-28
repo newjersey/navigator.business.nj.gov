63cd65b4a54815cf42bcc1280b4b516c3cfc8919
Dave Sueltenfuss <dsueltenfuss@fearless.tech>
2022-06-28 15:06:25 -0400

    build: add unicron plugin to shared library

--- shared/src/stringHelpers.ts
+++ shared/src/stringHelpers.ts
@@ -2,6 +2,6 @@ export const toProperCase = (value: string | undefined): string | undefined => {
   if (!value) return undefined;
 
   return value.replace(/\w\S*/g, (txt) => {
-    return txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase();
+    return txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase();
   });
 };
