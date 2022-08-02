f5fd66f8e111c248a8d2b5b3005bf0cde479d0c3
Dave Sueltenfuss <dsueltenfuss@fearless.tech>
2022-08-02 08:50:25 -0400

    fix: remove the status text when a funding has a due date

--- web/src/pages/funding/[fundingUrlSlug].tsx
+++ web/src/pages/funding/[fundingUrlSlug].tsx
@@ -45,10 +45,14 @@ export const FundingElement = (props: { funding: Funding }): ReactElement => {
             ) : (
               <></>
             )}
-            {isLargeScreen ? (
-              <span className="text-base">{props.funding.status.toUpperCase()}</span>
-            ) : (
-              <div className="text-base margin-top-1">{props.funding.status.toUpperCase()}</div>
+            {!props.funding.dueDate && (
+              <>
+                {isLargeScreen ? (
+                  <span className="text-base">{props.funding.status.toUpperCase()}</span>
+                ) : (
+                  <div className="text-base margin-top-1">{props.funding.status.toUpperCase()}</div>
+                )}
+              </>
             )}
           </div>
         </div>
