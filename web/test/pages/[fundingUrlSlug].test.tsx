dc229830b41881682704f8824e84f3dcbe59086d
Dave Sueltenfuss <dsueltenfuss@fearless.tech>
2022-08-02 08:50:25 -0400

    fix: remove the status text when a funding has a due date

--- web/test/pages/[fundingUrlSlug].test.tsx
+++ web/test/pages/[fundingUrlSlug].test.tsx
@@ -16,7 +16,6 @@ describe("funding page", () => {
 
     expect(screen.getByText("Some Funding Name")).toBeInTheDocument();
     expect(screen.getByText("DUE: 07/01/2025")).toBeInTheDocument();
-    expect(screen.getByText("DEADLINE")).toBeInTheDocument();
     expect(screen.getByText("Click here")).toBeInTheDocument();
     expect(screen.getByText("Some content description")).toBeInTheDocument();
   });
@@ -50,5 +49,52 @@ describe("funding page", () => {
     render(<FundingPage funding={funding} operateReferences={{}} />);
 
     expect(screen.queryByTestId("funding-agency-header")).not.toBeInTheDocument();
+  });
+
+  describe("status", () => {
+    it("shows status when status is first come, first serve", () => {
+      const funding = generateFunding({
+        name: "Some Funding Name",
+        callToActionText: "Click here",
+        contentMd: "Some content description",
+        dueDate: "",
+        status: "first come, first serve",
+        agency: [],
+      });
+
+      render(<FundingPage funding={funding} operateReferences={{}} />);
+
+      expect(screen.getByText("FIRST COME, FIRST SERVE")).toBeInTheDocument();
+    });
+
+    it("shows status when status is deadline with no due date", () => {
+      const funding = generateFunding({
+        name: "Some Funding Name",
+        callToActionText: "Click here",
+        contentMd: "Some content description",
+        dueDate: "",
+        status: "deadline",
+        agency: [],
+      });
+
+      render(<FundingPage funding={funding} operateReferences={{}} />);
+
+      expect(screen.getByText("DEADLINE")).toBeInTheDocument();
+    });
+
+    it("does not show status when status is deadline with a due date", () => {
+      const funding = generateFunding({
+        name: "Some Funding Name",
+        callToActionText: "Click here",
+        contentMd: "Some content description",
+        dueDate: "07/25/2030",
+        status: "deadline",
+        agency: [],
+      });
+
+      render(<FundingPage funding={funding} operateReferences={{}} />);
+
+      expect(screen.queryByText("DEADLINE")).not.toBeInTheDocument();
+    });
   });
 });
