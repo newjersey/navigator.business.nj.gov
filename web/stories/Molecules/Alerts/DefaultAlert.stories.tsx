bc933bf9bf6a11d9ee94d2dfb1a45814c4e58160
Faraz <fa8338@gmail.com>
2022-08-10 12:14:39 -0400

    chore: storybook - alert and snackbar stories

---
+++ web/stories/Molecules/Alerts/DefaultAlert.stories.tsx
@@ -0,0 +1,44 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { withDesign } from "storybook-addon-designs";
+import { Alert } from "../../../src/components/njwds-extended/Alert";
+
+export default {
+  title: "Molecules/Alerts/DefaultAlerts",
+  component: Alert,
+  decorators: [withDesign, (story) => <div className="border width-tablet">{story()}</div>],
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/vAa8neaM0JJSmldck5vlBC/BFS-Design-System-(Sprint-33%2B)?node-id=1%3A1703",
+    },
+  },
+} as ComponentMeta<typeof Alert>;
+
+const Template: ComponentStory<typeof Alert> = ({ ...args }) => <Alert {...args} />;
+const defaultArgs = {
+  children: "Alert Body Text",
+};
+
+export const InfoAlert = Template.bind({});
+InfoAlert.args = {
+  ...defaultArgs,
+  variant: "info",
+};
+
+export const WarningAlert = Template.bind({});
+WarningAlert.args = {
+  ...defaultArgs,
+  variant: "warning",
+};
+
+export const ErrorAlert = Template.bind({});
+ErrorAlert.args = {
+  ...defaultArgs,
+  variant: "error",
+};
+
+export const SuccessAlert = Template.bind({});
+SuccessAlert.args = {
+  ...defaultArgs,
+  variant: "success",
+};
