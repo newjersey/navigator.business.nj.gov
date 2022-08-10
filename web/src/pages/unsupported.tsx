16dfbe678de2188695cf7928e8d18b5405139d63
Faraz <fa8338@gmail.com>
2022-08-10 10:12:21 -0400

    chore: [#182725656] fixed padding on dakota upsupported page

--- web/src/pages/unsupported.tsx
+++ web/src/pages/unsupported.tsx
@@ -12,11 +12,11 @@ const UnsupportedPage = (): ReactElement => {
     <PageSkeleton>
       <main className="usa-section padding-top-0 desktop:padding-top-8" id="main">
         <SingleColumnContainer>
-          <div className="padding-top-5 desktop:padding-top-13">
+          <div className="padding-top-5 desktop:padding-top-0">
             <h2 className="base-darkest text-left">{Config.unsupportedNavigatorUserPage.title}</h2>
             <div
               data-testid="unsupported-subtitle"
-              className="base-darkest margin-bottom-5 desktop:margin-top-5"
+              className="base-darkest margin-bottom-5 desktop:margin-top-2"
             >
               <Content>{Config.unsupportedNavigatorUserPage.subtitle}</Content>
             </div>
