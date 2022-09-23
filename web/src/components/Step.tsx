453974f03b8aa370333914d4f47cd60e8f2f8233
Faraz <fa8338@gmail.com>
2022-09-23 15:54:55 -0400

    chore: [#183209366] left align step icons on dashboard

--- web/src/components/Step.tsx
+++ web/src/components/Step.tsx
@@ -17,7 +17,7 @@ export const Step = (props: Props): ReactElement => {
 
   return (
     <div
-      className={`margin-top-3 margin-left-2px tablet:margin-left-3 ${props.last ? "margin-bottom-1" : ""}`}
+      className={`margin-top-3 ${props.last ? "margin-bottom-1" : ""}`}
       id={`vertical-content-${props.step.stepNumber}`}
     >
       <div className="tablet:margin-right-4 minh-4">
