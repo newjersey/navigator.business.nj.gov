f0609b82c3edc192ba0ea1708a46ec262c4bf4e0
Soma Sai Kumar Badri <somasai.badri@gmail.com>
2022-10-04 08:53:56 -0700

    feat: [#183117206] updated 404 page

---
+++ web/src/lib/cms/previews/PageNotFoundPreview.tsx
@@ -0,0 +1,20 @@
+import { ConfigContext } from "@/contexts/configContext";
+import { PreviewProps } from "@/lib/cms/helpers/previewHelpers";
+import { usePreviewConfig } from "@/lib/cms/helpers/usePreviewConfig";
+import { usePreviewRef } from "@/lib/cms/helpers/usePreviewRef";
+import PageNotFound from "@/pages/404";
+
+const PageNotFoundPreview = (props: PreviewProps) => {
+  const { config, setConfig } = usePreviewConfig(props);
+  const ref = usePreviewRef(props);
+
+  return (
+    <ConfigContext.Provider value={{ config, setOverrides: setConfig }}>
+      <div className="cms" ref={ref} style={{ margin: 40, pointerEvents: "none" }}>
+        <PageNotFound />
+      </div>
+    </ConfigContext.Provider>
+  );
+};
+
+export default PageNotFoundPreview;
