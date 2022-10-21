62cc64529dc615c88df513cb495b788bb1e4f87a
Joshua Alley <jauschalley@gmail.com>
2022-10-21 13:22:59 -0400

    fix: moved api port to 5002 to avoid port conflict in OSX

--- scripts/clear-ports.sh
+++ scripts/clear-ports.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 npx kill-port 3000
-npx kill-port 5000
+npx kill-port 5002
 npx kill-port 5050
 npx kill-port 6006
 npx kill-port 8000
--- scripts/local-feature-tests.sh
+++ scripts/local-feature-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 WEB_PORT=3000
-API_PORT=5000
+API_PORT=5002
 API_BASE_URL=http://localhost:${API_PORT}/local
 
 CYPRESS_API_BASE_URL=${API_BASE_URL} yarn workspace @businessnjgovnavigator/web cypress:open --config baseUrl=http://localhost:${WEB_PORT}
