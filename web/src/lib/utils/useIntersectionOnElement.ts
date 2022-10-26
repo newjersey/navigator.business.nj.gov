95841898a807e314e9b717729c8f18d3fe2c013d
Soma Sai Kumar Badri <somasai.badri@gmail.com>
2022-10-26 15:32:51 -0700

    feat: [#183197943] scroll analytics on landing page

---
+++ web/src/lib/utils/useIntersectionOnElement.ts
@@ -0,0 +1,22 @@
+import { RefObject, useEffect, useState } from "react";
+
+export const useIntersectionOnElement = (element: RefObject<HTMLElement>, rootMargin: string) => {
+  const [isVisible, setState] = useState(false);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setState(entry.isIntersecting);
+      },
+      { rootMargin }
+    );
+
+    element.current && observer.observe(element.current);
+
+    const el = element.current;
+    return () => {
+      observer.unobserve(el as HTMLElement);
+    };
+  }, [element, rootMargin]);
+
+  return isVisible;
+};
