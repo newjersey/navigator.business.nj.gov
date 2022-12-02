166a57d257d9eeb3f4a3fc362ad9a4e3661d8f2d
Michael Deeb <michael.f.deeb@gmail.com>
2022-12-02 10:00:50 -0500

    fix: prettier upgrade code fixes

--- web/src/components/dashboard/OpportunityCardStatus.tsx
+++ web/src/components/dashboard/OpportunityCardStatus.tsx
@@ -8,12 +8,14 @@ interface Props {
 export const OpportunityCardStatus = (props: Props) => {
   return (
     <>
-      {props.dueDate && props.status !== "first come, first serve" && props.status !== "rolling application" && (
-        <div className="dashboard-opportunity-card-due-date">
-          <span className="dashboard-opportunity-card-due-date-header">Due: </span>
-          {props.dueDate}{" "}
-        </div>
-      )}
+      {props.dueDate &&
+        props.status !== "first come, first serve" &&
+        props.status !== "rolling application" && (
+          <div className="dashboard-opportunity-card-due-date">
+            <span className="dashboard-opportunity-card-due-date-header">Due: </span>
+            {props.dueDate}{" "}
+          </div>
+        )}
       {(props.status === "first come, first serve" || props.status === "rolling application") && (
         <div className="dashboard-opportunity-card-due-date text-bold">{toProperCase(props.status)}</div>
       )}
