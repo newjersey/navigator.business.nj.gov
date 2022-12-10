4a3c4740c11625e3436c1d9adadb2836d246b48e
Soma Sai Kumar Badri <somasai.badri@gmail.com>
2022-12-09 18:41:54 -0800

    refactor: [#183973542] split onboarding helper functions

---
+++ web/src/lib/utils/onboardingPageHelpers.ts
@@ -0,0 +1,64 @@
+import { OnboardingFlow } from "@/components/onboarding/getOnboardingFlows";
+import Config from "@businessnjgovnavigator/content/fieldConfig/config.json";
+import { LookupIndustryById } from "@businessnjgovnavigator/shared/industry";
+import { ProfileData } from "@businessnjgovnavigator/shared/profileData";
+import { UserData } from "@businessnjgovnavigator/shared/userData";
+import { QUERY_PARAMS_VALUES } from "../domain-logic/routes";
+import { FlowType, Page } from "../types/types";
+import { getFlow, templateEval } from "./helpers";
+
+export const mapFlowQueryToPersona: Record<QUERY_PARAMS_VALUES["flow"], FlowType> = {
+  starting: "STARTING",
+  "out-of-state": "FOREIGN",
+  "up-and-running": "OWNING",
+};
+
+export const industryQueryParamIsValid = (industryId: string | undefined): boolean => {
+  return !!LookupIndustryById(industryId).id;
+};
+
+export const flowQueryParamIsValid = (flow: string): boolean => {
+  return Object.keys(mapFlowQueryToPersona).includes(flow);
+};
+
+export const pageQueryParamisValid = (
+  onboardingFlows: Record<FlowType, OnboardingFlow>,
+  userData: UserData,
+  page: number
+): boolean => {
+  const hasAnsweredBusinessPersona = userData?.profileData.businessPersona !== undefined;
+  const flow = getFlow(userData);
+  const requestedPageIsInRange = page <= onboardingFlows[flow].pages.length && page > 0;
+
+  return hasAnsweredBusinessPersona && requestedPageIsInRange;
+};
+
+export const evalHeaderStepsTemplate = (
+  onboardingFlows: Record<FlowType, OnboardingFlow>,
+  currentFlow: FlowType,
+  profileData: ProfileData,
+  page: Page
+): string => {
+  if (page.current === 1) {
+    return templateEval(Config.onboardingDefaults.stepXTemplate, {
+      currentPage: page.current.toString(),
+    });
+  } else if (page.current === 2 && profileData.businessPersona === "FOREIGN") {
+    return templateEval(Config.onboardingDefaults.stepXTemplate, {
+      currentPage: page.current.toString(),
+    });
+  } else {
+    return templateEval(Config.onboardingDefaults.stepXofYTemplate, {
+      currentPage: page.current.toString(),
+      totalPages: onboardingFlows[currentFlow].pages.length.toString(),
+    });
+  }
+};
+
+export const getAnimation = (page: Page): string => {
+  return page.previous < page.current ? "slide" : "slide-back";
+};
+
+export const getTimeout = (page: Page, slidePage: number): number => {
+  return slidePage === page.previous ? 100 : 300;
+};
