e30bf4a99c48e71e9009611a826c26c142032912
Anne LoVerso <anne.loverso@gmail.com>
2023-03-27 13:20:16 -0400

    chore: [#182479557] add roadmap steps to search page

---
+++ web/src/lib/search/searchSteps.ts
@@ -0,0 +1,31 @@
+import { findMatchInLabelledText } from "@/lib/search/helpers";
+import { Match } from "@/lib/search/typesForSearch";
+import { Step } from "@/lib/types/types";
+
+export const searchSteps = (steps: Step[], term: string, params: { filename: string }) => {
+  const matches: Match[] = [];
+
+  for (const step of steps) {
+    let match: Match = {
+      filename: params.filename,
+      snippets: [],
+    };
+
+    const name = step.name.toLowerCase();
+    const description = step.description.toLowerCase();
+
+    const labelledTexts = [
+      { content: description, label: "Description" },
+      { content: name, label: "Name" },
+    ];
+
+    match = findMatchInLabelledText(labelledTexts, term, match);
+
+    if (match.snippets.length > 0) {
+      match.filename = `${match.filename} (Step ${step.stepNumber})`;
+      matches.push(match);
+    }
+  }
+
+  return matches;
+};
