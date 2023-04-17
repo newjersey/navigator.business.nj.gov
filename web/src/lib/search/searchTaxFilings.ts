cbe97b85478a2054d1f4afb6f38f2af1d7e5f5c5
Anne LoVerso <anne.loverso@gmail.com>
2023-04-17 10:46:06 -0400

    chore: [#182479557] add tax filings to search page

---
+++ web/src/lib/search/searchTaxFilings.ts
@@ -0,0 +1,51 @@
+import { findMatchInBlock, findMatchInLabelledText } from "@/lib/search/helpers";
+import { Match } from "@/lib/search/typesForSearch";
+import { Filing } from "@/lib/types/types";
+
+export const searchTaxFilings = (filings: Filing[], term: string): Match[] => {
+  const matches: Match[] = [];
+
+  for (const filing of filings) {
+    let match: Match = {
+      filename: filing.filename,
+      snippets: [],
+    };
+
+    const content = filing.contentMd.toLowerCase();
+    const details = filing.filingDetails?.toLowerCase();
+    const rates = filing.taxRates?.toLowerCase();
+    const name = filing.name.toLowerCase();
+    const cta = filing.callToActionText?.toLowerCase();
+    const agency = filing.agency?.toLowerCase();
+    const method = filing.filingMethod?.toLowerCase();
+    const id = filing.id?.toLowerCase();
+    const info = filing.additionalInfo?.toLowerCase();
+    const frequency = filing.frequency?.toLowerCase();
+
+    const blockTexts = [content];
+    if (details) {
+      blockTexts.push(details);
+    }
+    if (rates) {
+      blockTexts.push(rates);
+    }
+    const labelledTexts = [
+      { content: cta, label: "CTA Text" },
+      { content: name, label: "Task name" },
+      { content: agency, label: "Agency" },
+      { content: method, label: "Filing Method" },
+      { content: id, label: "ID" },
+      { content: info, label: "Additional Info" },
+      { content: frequency, label: "Frequency" },
+    ];
+
+    match = findMatchInBlock(blockTexts, term, match);
+    match = findMatchInLabelledText(labelledTexts, term, match);
+
+    if (match.snippets.length > 0) {
+      matches.push(match);
+    }
+  }
+
+  return matches;
+};
