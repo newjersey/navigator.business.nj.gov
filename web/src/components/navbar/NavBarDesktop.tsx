c8dfdb22cba439755eab8aeffba0e423bd3a0105
Soma Sai Kumar Badri <somasai.badri@gmail.com>
2023-05-15 15:05:45 -0700

    feat: [#184870963] updated desktop navbar to sticky

--- web/src/components/navbar/NavBarDesktop.tsx
+++ web/src/components/navbar/NavBarDesktop.tsx
@@ -127,7 +127,7 @@ export const NavBarDesktop = (): ReactElement => {
     );
   };
   return (
-    <>
+    <div className="position-sticky top-0 z-500 bg-white">
       <nav aria-label="Primary" className="grid-container-widescreen desktop:padding-x-7">
         <div className="display-flex flex-row flex-justify flex-align-center height-8">
           <NavigatorLogo />
@@ -213,6 +213,6 @@ export const NavBarDesktop = (): ReactElement => {
         </div>
       </nav>
       <hr className="margin-0" />
-    </>
+    </div>
   );
 };
