57182486aca8fd8ff2a66d2511dd6cf373438f6d
Anne LoVerso <anne.loverso@gmail.com>
2023-06-13 11:15:47 -0400

    chore: [#185386851] rename license expiration events folder and header

--- web/src/lib/cms/previews/LicenseEventPreview.tsx
+++ web/src/lib/cms/previews/LicenseEventPreview.tsx
@@ -12,7 +12,7 @@ const LicenseEventPreview = (props: PreviewProps): ReactElement => {
   return (
     <div className="cms" ref={ref} style={{ margin: 40, pointerEvents: "none" }}>
       <LicenseElement
-        licenseName="Cosmetology"
+        licenseName="IndustryName"
         license={license}
         licenseEventType={license.previewType ?? "expiration"}
         dueDate={license.previewType === "renewal" ? "2024-02-01" : "2024-01-01"}
