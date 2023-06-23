5fd7d8420841fad61f89a5ed8e39af920ad505a1
Soma Sai Kumar Badri <somasai.badri@gmail.com>
2023-06-23 12:19:35 -0700

    fix: [#185471630] tax filing calendar lookup overwriting municipality

--- web/src/components/filings-calendar/FilingsCalendarTaxAccess.tsx
+++ web/src/components/filings-calendar/FilingsCalendarTaxAccess.tsx
@@ -4,6 +4,7 @@ import { PrimaryButton } from "@/components/njwds-extended/PrimaryButton";
 import { SnackbarAlert } from "@/components/njwds-extended/SnackbarAlert";
 import { AuthAlertContext } from "@/contexts/authAlertContext";
 import { postTaxFilingsLookup } from "@/lib/api-client/apiClient";
+import { fetchMunicipalityByName } from "@/lib/async-content-fetchers/fetchMunicipalities";
 import { IsAuthenticated } from "@/lib/auth/AuthContext";
 import { useConfig } from "@/lib/data-hooks/useConfig";
 import { useUserData } from "@/lib/data-hooks/useUserData";
@@ -40,7 +41,21 @@ export const FilingsCalendarTaxAccess = (): ReactElement => {
           taxId: userData.profileData.taxId as string,
           encryptedTaxId: userData.profileData.encryptedTaxId as string,
         });
-        updateQueue?.queue(updatedUserData).update();
+
+        let record;
+
+        if (updatedUserData.profileData.municipality?.name) {
+          record = await fetchMunicipalityByName(updatedUserData.profileData.municipality?.name || "");
+        }
+
+        const municipalityToSet = {
+          name: record?.townName || userData.profileData.municipality?.name || "",
+          county: record?.countyName || userData.profileData.municipality?.county || "",
+          id: record?.id || userData.profileData.municipality?.id || "",
+          displayName: record?.townDisplayName || userData.profileData.municipality?.displayName || "",
+        };
+
+        updateQueue?.queue(updatedUserData).queueProfileData({ municipality: municipalityToSet }).update();
       }
     })();
   }, userData);
