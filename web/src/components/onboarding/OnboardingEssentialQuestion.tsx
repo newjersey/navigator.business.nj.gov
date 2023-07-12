40e8806153121ff3d91300405b8489e07ba31b48
Faraz <fa8338@gmail.com>
2023-07-12 13:24:24 -0400

    chore: [#184883018] update styling in profile

--- web/src/components/onboarding/OnboardingEssentialQuestion.tsx
+++ web/src/components/onboarding/OnboardingEssentialQuestion.tsx
@@ -6,7 +6,6 @@ import { profileFormContext } from "@/contexts/profileFormContext";
 import { useConfig } from "@/lib/data-hooks/useConfig";
 import { useFormContextFieldHelpers } from "@/lib/data-hooks/useFormContextFieldHelpers";
 import { EssentialQuestion } from "@/lib/domain-logic/essentialQuestions";
-import { getProfileConfig } from "@/lib/domain-logic/getProfileConfig";
 import { FormContextFieldProps, ProfileContentField } from "@/lib/types/types";
 import { IndustrySpecificData, industrySpecificDataChoices } from "@businessnjgovnavigator/shared";
 import { ReactElement, useContext } from "react";
@@ -24,26 +23,13 @@ export const OnboardingEssentialQuestion = <T,>(props: Props<T>): ReactElement =
   );
 
   const { Config } = useConfig();
-  const essentialQuestionContentFromConfig = getProfileConfig({
-    config: Config,
-    persona: state.flow,
-    fieldName: props.essentialQuestion.fieldName as ProfileContentField,
-  });
   RegisterForOnSubmit(() => state.profileData[props.essentialQuestion.fieldName] !== undefined);
 
   return (
     <div
       data-testid={`industry-specific-${state.profileData.industryId}-${props.essentialQuestion.fieldName}`}
     >
-      <WithErrorBar
-        hasError={isFormFieldInValid}
-        type="ALWAYS"
-        className={
-          essentialQuestionContentFromConfig.headerNotBolded || essentialQuestionContentFromConfig.header
-            ? "margin-top-4"
-            : "margin-top-2"
-        }
-      >
+      <WithErrorBar hasError={isFormFieldInValid} type="ALWAYS" className="margin-top-2">
         <FieldLabelProfile fieldName={props.essentialQuestion.fieldName as ProfileContentField} />
         <OnboardingRadioQuestion<IndustrySpecificData[keyof IndustrySpecificData]>
           {...props.essentialQuestion}
