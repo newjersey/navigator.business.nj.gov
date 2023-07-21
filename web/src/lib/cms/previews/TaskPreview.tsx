8a3a0174816d8bb52b088e1c203b2d87758610de
Anne LoVerso <anne.loverso@gmail.com>
2023-07-21 15:21:14 -0400

    refactor: remove test functions from prod code

--- web/src/lib/cms/previews/TaskPreview.tsx
+++ web/src/lib/cms/previews/TaskPreview.tsx
@@ -3,15 +3,26 @@ import { PreviewProps } from "@/lib/cms/helpers/previewHelpers";
 import { usePageData } from "@/lib/cms/helpers/usePageData";
 import { usePreviewRef } from "@/lib/cms/helpers/usePreviewRef";
 import { Task } from "@/lib/types/types";
+import {
+  generateBusiness,
+  generateMunicipality,
+  generateProfileData,
+} from "@businessnjgovnavigator/shared/test";
 import { ReactElement } from "react";
 
 const TaskPreview = (props: PreviewProps): ReactElement => {
   const ref = usePreviewRef(props);
   const task = usePageData<Task>(props);
 
+  const fakeBusinessWithMunicipality = generateBusiness({
+    profileData: generateProfileData({
+      municipality: generateMunicipality({}),
+    }),
+  });
+
   return (
     <div className="cms" ref={ref} style={{ margin: 40, pointerEvents: "none" }}>
-      <TaskElement task={task} overrides={{ skipDeferredLocationPrompt: true }} />
+      <TaskElement task={task} CMS_ONLY_fakeBusiness={fakeBusinessWithMunicipality} />
     </div>
   );
 };
