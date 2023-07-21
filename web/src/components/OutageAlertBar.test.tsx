3322f82fc088c0f31ce5a47b5f5ecd879609aec8
Anne LoVerso <anne.loverso@gmail.com>
2023-07-21 14:56:05 -0400

    chore: [#185598851] control outage bar via github

--- web/src/components/OutageAlertBar.test.tsx
+++ web/src/components/OutageAlertBar.test.tsx
@@ -1,127 +1,197 @@
 import { OutageAlertBar } from "@/components/OutageAlertBar";
 import { IsAuthenticated } from "@/lib/auth/AuthContext";
+import { generateOutageConfig } from "@/test/factories";
 import { withAuth } from "@/test/helpers/helpers-renderers";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import axios from "axios";
+
+jest.mock("axios");
+const mockAxios = axios as jest.Mocked<typeof axios>;
 
 describe("<OutageAlertBar />", () => {
-  let originalFeatureValue: string | undefined;
-  let originalMessageValue: string | undefined;
-  let originalTypeValue: string | undefined;
+  let originalUrlValue: string | undefined;
+  const url = "www.example.com";
 
   beforeEach(() => {
-    originalFeatureValue = process.env.FEATURE_ENABLE_OUTAGE_ALERT_BAR;
-    originalMessageValue = process.env.OUTAGE_ALERT_MESSAGE;
-    originalTypeValue = process.env.OUTAGE_ALERT_TYPE;
+    jest.resetAllMocks();
+    originalUrlValue = process.env.OUTAGE_ALERT_CONFIG_URL;
+    process.env.OUTAGE_ALERT_CONFIG_URL = url;
   });
 
   afterEach(() => {
-    process.env.FEATURE_ENABLE_OUTAGE_ALERT_BAR = originalFeatureValue;
-    process.env.OUTAGE_ALERT_MESSAGE = originalMessageValue;
-    process.env.OUTAGE_ALERT_TYPE = originalTypeValue;
+    process.env.OUTAGE_ALERT_CONFIG_URL = originalUrlValue;
   });
 
-  it("does not show up if feature is disabled", () => {
-    process.env.FEATURE_ENABLE_OUTAGE_ALERT_BAR = "false";
+  it("does not show up if fetch call fails", async () => {
+    mockAxios.get.mockRejectedValue({});
     render(<OutageAlertBar />);
-    expect(screen.queryByTestId("outage-alert-bar")).not.toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.queryByTestId("outage-alert-bar")).not.toBeInTheDocument();
+    });
   });
 
-  it("does not show up if feature flag is undefined", () => {
-    delete process.env.FEATURE_ENABLE_OUTAGE_ALERT_BAR;
+  it("does not show up if feature is disabled", async () => {
+    mockAxios.get.mockResolvedValue({
+      data: generateOutageConfig({
+        FEATURE_ENABLE_OUTAGE_ALERT_BAR: false,
+        OUTAGE_ALERT_TYPE: "ALL",
+      }),
+    });
     render(<OutageAlertBar />);
-    expect(screen.queryByTestId("outage-alert-bar")).not.toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.queryByTestId("outage-alert-bar")).not.toBeInTheDocument();
+    });
   });
 
-  it("shows up if feature is enabled", () => {
-    process.env.FEATURE_ENABLE_OUTAGE_ALERT_BAR = "true";
+  it("does not show up if feature flag is undefined", async () => {
+    mockAxios.get.mockResolvedValue({
+      data: generateOutageConfig({
+        FEATURE_ENABLE_OUTAGE_ALERT_BAR: undefined,
+        OUTAGE_ALERT_TYPE: "ALL",
+      }),
+    });
     render(<OutageAlertBar />);
-    expect(screen.getByTestId("outage-alert-bar")).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.queryByTestId("outage-alert-bar")).not.toBeInTheDocument();
+    });
   });
 
-  it("shows message from environment", () => {
-    process.env.FEATURE_ENABLE_OUTAGE_ALERT_BAR = "true";
-    process.env.OUTAGE_ALERT_MESSAGE = "some cool message";
+  it("shows up with message from config when enabled", async () => {
+    mockAxios.get.mockResolvedValue({
+      data: generateOutageConfig({
+        FEATURE_ENABLE_OUTAGE_ALERT_BAR: true,
+        OUTAGE_ALERT_TYPE: "ALL",
+        OUTAGE_ALERT_MESSAGE: "some cool message",
+      }),
+    });
     render(<OutageAlertBar />);
-    expect(screen.getByTestId("outage-alert-bar")).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.getByTestId("outage-alert-bar")).toBeInTheDocument();
+    });
     expect(screen.getByText("some cool message")).toBeInTheDocument();
   });
 
-  it("does not show bar when message is undefined", () => {
-    process.env.FEATURE_ENABLE_OUTAGE_ALERT_BAR = "true";
-    delete process.env.OUTAGE_ALERT_MESSAGE;
+  it("does not show bar when message is undefined", async () => {
+    mockAxios.get.mockResolvedValue({
+      data: generateOutageConfig({
+        FEATURE_ENABLE_OUTAGE_ALERT_BAR: true,
+        OUTAGE_ALERT_MESSAGE: undefined,
+        OUTAGE_ALERT_TYPE: "ALL",
+      }),
+    });
     render(<OutageAlertBar />);
-    expect(screen.queryByTestId("outage-alert-bar")).not.toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.queryByTestId("outage-alert-bar")).not.toBeInTheDocument();
+    });
   });
 
   describe("alert type", () => {
-    beforeEach(() => {
-      process.env.FEATURE_ENABLE_OUTAGE_ALERT_BAR = "true";
-      process.env.OUTAGE_ALERT_MESSAGE = "some message ";
-    });
-
-    it("when undefined - does not show outage alert", () => {
-      delete process.env.OUTAGE_ALERT_TYPE;
+    it("when undefined - does not show outage alert", async () => {
+      mockAxios.get.mockResolvedValue({
+        data: generateOutageConfig({
+          FEATURE_ENABLE_OUTAGE_ALERT_BAR: true,
+          OUTAGE_ALERT_TYPE: undefined,
+        }),
+      });
       render(<OutageAlertBar />);
-      expect(screen.queryByTestId("outage-alert-bar")).not.toBeInTheDocument();
+      await waitFor(() => {
+        expect(screen.queryByTestId("outage-alert-bar")).not.toBeInTheDocument();
+      });
     });
 
     describe("when ALL", () => {
-      it("shows alert bar for logged-in users", () => {
-        process.env.OUTAGE_ALERT_TYPE = "ALL";
+      beforeEach(() => {
+        mockAxios.get.mockResolvedValue({
+          data: generateOutageConfig({
+            FEATURE_ENABLE_OUTAGE_ALERT_BAR: true,
+            OUTAGE_ALERT_TYPE: "ALL",
+          }),
+        });
+      });
+
+      it("shows alert bar for logged-in users", async () => {
         render(withAuth(<OutageAlertBar />, { isAuthenticated: IsAuthenticated.TRUE }));
-        expect(screen.getByTestId("outage-alert-bar")).toBeInTheDocument();
+        await waitFor(() => {
+          expect(screen.getByTestId("outage-alert-bar")).toBeInTheDocument();
+        });
       });
 
-      it("shows alert bar for non-logged-in users", () => {
-        process.env.OUTAGE_ALERT_TYPE = "ALL";
+      it("shows alert bar for non-logged-in users", async () => {
         render(withAuth(<OutageAlertBar />, { isAuthenticated: IsAuthenticated.FALSE }));
-        expect(screen.getByTestId("outage-alert-bar")).toBeInTheDocument();
+        await waitFor(() => {
+          expect(screen.getByTestId("outage-alert-bar")).toBeInTheDocument();
+        });
       });
 
-      it("shows alert bar for unknown-logged-in users", () => {
-        process.env.OUTAGE_ALERT_TYPE = "ALL";
+      it("shows alert bar for unknown-logged-in users", async () => {
         render(withAuth(<OutageAlertBar />, { isAuthenticated: IsAuthenticated.UNKNOWN }));
-        expect(screen.getByTestId("outage-alert-bar")).toBeInTheDocument();
+        await waitFor(() => {
+          expect(screen.getByTestId("outage-alert-bar")).toBeInTheDocument();
+        });
       });
     });
 
     describe("when LOGGED_IN_ONLY", () => {
-      it("shows alert bar for logged-in users", () => {
-        process.env.OUTAGE_ALERT_TYPE = "LOGGED_IN_ONLY";
+      beforeEach(() => {
+        mockAxios.get.mockResolvedValue({
+          data: generateOutageConfig({
+            FEATURE_ENABLE_OUTAGE_ALERT_BAR: true,
+            OUTAGE_ALERT_TYPE: "LOGGED_IN_ONLY",
+          }),
+        });
+      });
+
+      it("shows alert bar for logged-in users", async () => {
         render(withAuth(<OutageAlertBar />, { isAuthenticated: IsAuthenticated.TRUE }));
-        expect(screen.getByTestId("outage-alert-bar")).toBeInTheDocument();
+        await waitFor(() => {
+          expect(screen.getByTestId("outage-alert-bar")).toBeInTheDocument();
+        });
       });
 
-      it("does not show alert bar for non-logged-in users", () => {
-        process.env.OUTAGE_ALERT_TYPE = "LOGGED_IN_ONLY";
+      it("does not show alert bar for non-logged-in users", async () => {
         render(withAuth(<OutageAlertBar />, { isAuthenticated: IsAuthenticated.FALSE }));
-        expect(screen.queryByTestId("outage-alert-bar")).not.toBeInTheDocument();
+        await waitFor(() => {
+          expect(screen.queryByTestId("outage-alert-bar")).not.toBeInTheDocument();
+        });
       });
 
-      it("does not show alert bar for unknown-logged-in users", () => {
-        process.env.OUTAGE_ALERT_TYPE = "LOGGED_IN_ONLY";
+      it("does not show alert bar for unknown-logged-in users", async () => {
         render(withAuth(<OutageAlertBar />, { isAuthenticated: IsAuthenticated.UNKNOWN }));
-        expect(screen.queryByTestId("outage-alert-bar")).not.toBeInTheDocument();
+        await waitFor(() => {
+          expect(screen.queryByTestId("outage-alert-bar")).not.toBeInTheDocument();
+        });
       });
     });
 
     describe("when UNREGISTERED_ONLY", () => {
-      it("does not show alert bar for logged-in users", () => {
-        process.env.OUTAGE_ALERT_TYPE = "UNREGISTERED_ONLY";
+      beforeEach(() => {
+        mockAxios.get.mockResolvedValue({
+          data: generateOutageConfig({
+            FEATURE_ENABLE_OUTAGE_ALERT_BAR: true,
+            OUTAGE_ALERT_TYPE: "UNREGISTERED_ONLY",
+          }),
+        });
+      });
+
+      it("does not show alert bar for logged-in users", async () => {
         render(withAuth(<OutageAlertBar />, { isAuthenticated: IsAuthenticated.TRUE }));
-        expect(screen.queryByTestId("outage-alert-bar")).not.toBeInTheDocument();
+        await waitFor(() => {
+          expect(screen.queryByTestId("outage-alert-bar")).not.toBeInTheDocument();
+        });
       });
 
-      it("shows alert bar for non-logged-in users", () => {
-        process.env.OUTAGE_ALERT_TYPE = "UNREGISTERED_ONLY";
+      it("shows alert bar for non-logged-in users", async () => {
         render(withAuth(<OutageAlertBar />, { isAuthenticated: IsAuthenticated.FALSE }));
-        expect(screen.getByTestId("outage-alert-bar")).toBeInTheDocument();
+        await waitFor(() => {
+          expect(screen.getByTestId("outage-alert-bar")).toBeInTheDocument();
+        });
       });
 
-      it("does not show alert bar for unknown-logged-in users", () => {
-        process.env.OUTAGE_ALERT_TYPE = "UNREGISTERED_ONLY";
+      it("does not show alert bar for unknown-logged-in users", async () => {
         render(withAuth(<OutageAlertBar />, { isAuthenticated: IsAuthenticated.UNKNOWN }));
-        expect(screen.queryByTestId("outage-alert-bar")).not.toBeInTheDocument();
+        await waitFor(() => {
+          expect(screen.queryByTestId("outage-alert-bar")).not.toBeInTheDocument();
+        });
       });
     });
   });
