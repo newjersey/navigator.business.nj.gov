fc73ee11f1c54462321fbafa7c3bd4d2ebdd665f
Anne LoVerso <anne.loverso@gmail.com>
2023-07-31 18:29:08 -0400

    chore: [#185153256] display Veteran Nonprofit in review section

---
+++ web/src/components/tasks/business-formation/review/ReviewIsVeteranNonprofit.tsx
@@ -0,0 +1,27 @@
+import { Content } from "@/components/Content";
+import { ReviewLineItem } from "@/components/tasks/business-formation/review/section/ReviewLineItem";
+import { useConfig } from "@/lib/data-hooks/useConfig";
+import { ReactElement } from "rehype-react/lib";
+
+interface Props {
+  value: boolean | undefined;
+}
+
+export const ReviewIsVeteranNonprofit = (props: Props): ReactElement => {
+  const { Config } = useConfig();
+
+  const displayResponse = (): ReactElement => {
+    if (props.value) {
+      return <Content>{Config.formation.fields.isVeteranNonprofit.reviewTextYes}</Content>;
+    } else if (props.value === false) {
+      return <Content>{Config.formation.fields.isVeteranNonprofit.reviewTextNo}</Content>;
+    } else {
+      return <ReviewLineItem label={Config.formation.fields.isVeteranNonprofit.label} value={""} />;
+    }
+  };
+  return (
+    <div className="margin-top-1" data-testid="isVeteranNonprofit">
+      {displayResponse()}
+    </div>
+  );
+};
