c642aebd04e02439fdefdc659f55228b355d908f
Anne LoVerso <anne.loverso@gmail.com>
2023-07-31 18:17:18 -0400

    chore: [#185153256] display terms with Not Entered when in_form and empty

--- web/src/components/tasks/business-formation/review/ReviewNonprofitProvisions.tsx
+++ web/src/components/tasks/business-formation/review/ReviewNonprofitProvisions.tsx
@@ -5,34 +5,20 @@ import { ReviewNotEntered } from "@/components/tasks/business-formation/review/s
 import { ReviewSubSection } from "@/components/tasks/business-formation/review/section/ReviewSubSection";
 import { BusinessFormationContext } from "@/contexts/businessFormationContext";
 import { useConfig } from "@/lib/data-hooks/useConfig";
-import { InFormInBylaws } from "@businessnjgovnavigator/shared";
-import { ReactElement, useContext } from "react";
+import { FormationFields, InFormInBylaws } from "@businessnjgovnavigator/shared";
+import { Fragment, ReactElement, useContext } from "react";
 
 export const ReviewNonprofitProvisions = (): ReactElement => {
   const { state } = useContext(BusinessFormationContext);
   const { Config } = useConfig();
 
-  const {
-    hasNonprofitBoardMembers,
-    nonprofitBoardMemberQualificationsSpecified,
-    nonprofitBoardMemberQualificationsTerms,
-    nonprofitBoardMemberRightsSpecified,
-    nonprofitBoardMemberRightsTerms,
-    nonprofitTrusteesMethodSpecified,
-    nonprofitTrusteesMethodTerms,
-    nonprofitAssetDistributionSpecified,
-    nonprofitAssetDistributionTerms,
-  } = state.formationFormData;
-
-  const isVisibleInReview = (value: InFormInBylaws): boolean => {
-    return value === "IN_FORM";
-  };
+  const { hasNonprofitBoardMembers } = state.formationFormData;
 
   const showQuestionAnswer = ({
     fieldName,
     value,
   }: {
-    fieldName: keyof typeof Config.formation.fields;
+    fieldName: FormationFields;
     value: InFormInBylaws;
   }): ReactElement => {
     const endOfSentence = ((): string => {
@@ -46,7 +32,8 @@ export const ReviewNonprofitProvisions = (): ReactElement => {
       }
     })();
 
-    const bodyText = (Config.formation.fields[fieldName] as any).body;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const bodyText = ((Config.formation.fields as any)[fieldName] as any).body;
 
     return (
       <div data-testid={fieldName}>
@@ -61,6 +48,16 @@ export const ReviewNonprofitProvisions = (): ReactElement => {
       </div>
     );
   };
+
+  const provisionsToDisplay: Array<{ radioField: FormationFields; termsField: FormationFields }> = [
+    {
+      radioField: "nonprofitBoardMemberQualificationsSpecified",
+      termsField: "nonprofitBoardMemberQualificationsTerms",
+    },
+    { radioField: "nonprofitBoardMemberRightsSpecified", termsField: "nonprofitBoardMemberRightsTerms" },
+    { radioField: "nonprofitTrusteesMethodSpecified", termsField: "nonprofitTrusteesMethodTerms" },
+    { radioField: "nonprofitAssetDistributionSpecified", termsField: "nonprofitAssetDistributionTerms" },
+  ];
 
   return (
     <>
@@ -78,81 +75,42 @@ export const ReviewNonprofitProvisions = (): ReactElement => {
           )}
         </div>
 
-        <div className="margin-top-2">
-          {hasNonprofitBoardMembers &&
-            showQuestionAnswer({
-              fieldName: "nonprofitBoardMemberQualificationsSpecified",
-              value: nonprofitBoardMemberQualificationsSpecified,
-            })}
-        </div>
+        {provisionsToDisplay.map(({ radioField, termsField }) => {
+          const radioValue = state.formationFormData[radioField] as InFormInBylaws;
+          const termsValue = state.formationFormData[termsField] as string;
 
-        {isVisibleInReview(nonprofitBoardMemberQualificationsSpecified) && (
-          <div className="margin-top-2 margin-left-3">
-            <ExpandCollapseString
-              text={nonprofitBoardMemberQualificationsTerms}
-              viewMoreText={Config.formation.general.viewMoreButtonText}
-              viewLessText={Config.formation.general.viewLessButtonText}
-              lines={2}
-            />
-          </div>
-        )}
+          return (
+            <Fragment key={radioField}>
+              <div className="margin-top-2">
+                {hasNonprofitBoardMembers &&
+                  showQuestionAnswer({
+                    fieldName: radioField,
+                    value: radioValue,
+                  })}
+              </div>
 
-        <div className="margin-top-2">
-          {hasNonprofitBoardMembers &&
-            showQuestionAnswer({
-              fieldName: "nonprofitBoardMemberRightsSpecified",
-              value: nonprofitBoardMemberRightsSpecified,
-            })}
-        </div>
-
-        {isVisibleInReview(nonprofitBoardMemberRightsSpecified) && (
-          <div className="margin-top-2 margin-left-3">
-            <ExpandCollapseString
-              text={nonprofitBoardMemberRightsTerms}
-              viewMoreText={Config.formation.general.viewMoreButtonText}
-              viewLessText={Config.formation.general.viewLessButtonText}
-              lines={2}
-            />
-          </div>
-        )}
-
-        <div className="margin-top-2">
-          {hasNonprofitBoardMembers &&
-            showQuestionAnswer({
-              fieldName: "nonprofitTrusteesMethodSpecified",
-              value: nonprofitTrusteesMethodSpecified,
-            })}
-        </div>
-
-        {isVisibleInReview(nonprofitTrusteesMethodSpecified) && (
-          <div className="margin-top-2 margin-left-3">
-            <ExpandCollapseString
-              text={nonprofitTrusteesMethodTerms}
-              viewMoreText={Config.formation.general.viewMoreButtonText}
-              viewLessText={Config.formation.general.viewLessButtonText}
-              lines={2}
-            />
-          </div>
-        )}
-
-        <div className="margin-top-2">
-          {hasNonprofitBoardMembers &&
-            showQuestionAnswer({
-              fieldName: "nonprofitAssetDistributionSpecified",
-              value: nonprofitAssetDistributionSpecified,
-            })}
-        </div>
-
-        {isVisibleInReview(nonprofitAssetDistributionSpecified) && (
-          <div className="margin-top-2 margin-left-3">
-            <ExpandCollapseString
-              text={nonprofitAssetDistributionTerms}
-              viewMoreText={Config.formation.general.viewMoreButtonText}
-              viewLessText={Config.formation.general.viewLessButtonText}
-              lines={2}
-            />
-          </div>
-        )}
+              {radioValue === "IN_FORM" && (
+                <div className="margin-top-2 margin-left-3" data-testid={`${radioField}-terms`}>
+                  {termsValue ? (
+                    <ExpandCollapseString
+                      text={termsValue}
+                      viewMoreText={Config.formation.general.viewMoreButtonText}
+                      viewLessText={Config.formation.general.viewLessButtonText}
+                      lines={2}
+                    />
+                  ) : (
+                    <div className="display-flex flex-row">
+                      <span className="margin-right-1">
+                        <strong>{Config.formation.nonprofitProvisions.description}:</strong>
+                      </span>
+                      <ReviewNotEntered />
+                    </div>
+                  )}
+                </div>
+              )}
+            </Fragment>
+          );
+        })}
       </ReviewSubSection>
     </>
   );
