517971563879abd1401f28aa737c1f1ed4afdf59
Anne LoVerso <anne.loverso@gmail.com>
2023-08-02 13:48:37 -0400

    feat: [#184770173] update postOnboarding to change task status with checkboxes

--- web/src/components/post-onboarding/PostOnboardingRadioQuestion.tsx
+++ web/src/components/post-onboarding/PostOnboardingRadioQuestion.tsx
@@ -1,17 +1,19 @@
 import { Content } from "@/components/Content";
 import { fetchPostOnboarding } from "@/lib/async-content-fetchers/fetchPostOnboarding";
 import { useUserData } from "@/lib/data-hooks/useUserData";
+import { postOnboardingCheckboxes } from "@/lib/domain-logic/postOnboardingCheckboxes";
 import { PostOnboarding } from "@/lib/types/types";
 import { ProfileData } from "@businessnjgovnavigator/shared/";
 import { FormControl, FormControlLabel, Radio, RadioGroup } from "@mui/material";
-import React, { ReactElement, useEffect, useState } from "react";
+import React, { ReactElement, useEffect, useMemo, useState } from "react";
 
 interface Props {
-  id: string;
+  postOnboardingQuestionId: keyof typeof postOnboardingCheckboxes;
   onboardingKey: keyof ProfileData;
+  taskId: string;
 }
 
-export const RadioQuestion = (props: Props): ReactElement => {
+export const PostOnboardingRadioQuestion = (props: Props): ReactElement => {
   const { updateQueue, business } = useUserData();
   const [onboardingQuestion, setOnboardingQuestion] = useState<PostOnboarding>({
     contentMd: "",
@@ -21,21 +23,57 @@ export const RadioQuestion = (props: Props): ReactElement => {
     radioNoContent: "",
   });
 
+  const checkboxes = postOnboardingCheckboxes[props.postOnboardingQuestionId];
+  const selectedCheckboxes = useMemo(() => {
+    return checkboxes.filter((key) => business?.taskItemChecklist[key] === true).join(",");
+  }, [business?.taskItemChecklist, checkboxes]);
+
   useEffect(() => {
-    fetchPostOnboarding(props.id).then((postOnboarding) => {
+    if (!business || !updateQueue) return;
+    if (business.profileData[props.onboardingKey] !== true) return;
+
+    const allCheckboxesSelected = checkboxes.every((key) => {
+      return business.taskItemChecklist[key];
+    });
+
+    if (allCheckboxesSelected) {
+      updateQueue.queueTaskProgress({ [props.taskId]: "COMPLETED" }).update();
+    } else {
+      updateQueue.queueTaskProgress({ [props.taskId]: "IN_PROGRESS" }).update();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedCheckboxes]);
+
+  useEffect(() => {
+    fetchPostOnboarding(props.postOnboardingQuestionId).then((postOnboarding) => {
       setOnboardingQuestion(postOnboarding);
     });
-  }, [props.id]);
+  }, [props.postOnboardingQuestionId]);
 
   const handleRadioChange = async (
     event: React.ChangeEvent<{ name?: string; value: string }>
   ): Promise<void> => {
-    if (!updateQueue) {
+    if (!updateQueue || !business) {
       return;
     }
+
+    if (event.target.value === "false") {
+      const uncheckedCheckboxes = checkboxes.reduce((acc, checkboxId) => {
+        return {
+          ...acc,
+          [checkboxId]: false,
+        };
+      }, {});
+
+      updateQueue.queueTaskItemChecklist(uncheckedCheckboxes);
+    }
+
     await updateQueue
       .queueProfileData({
         [props.onboardingKey]: event.target.value === "true",
+      })
+      .queueTaskProgress({
+        [props.taskId]: event.target.value === "true" ? "IN_PROGRESS" : "COMPLETED",
       })
       .update();
   };
@@ -45,14 +83,19 @@ export const RadioQuestion = (props: Props): ReactElement => {
       {onboardingQuestion.question && (
         <>
           <div className="margin-top-205">
-            <div role="heading" aria-level={2} data-testid={props.id} className="h4-styling ">
+            <div
+              role="heading"
+              aria-level={2}
+              data-testid={props.postOnboardingQuestionId}
+              className="h4-styling "
+            >
               {onboardingQuestion.question}
             </div>
           </div>
           <FormControl fullWidth>
             <RadioGroup
               aria-label={onboardingQuestion.question}
-              name={props.id}
+              name={props.postOnboardingQuestionId}
               value={business?.profileData[props.onboardingKey] ?? ""}
               onChange={handleRadioChange}
               row
@@ -76,6 +119,7 @@ export const RadioQuestion = (props: Props): ReactElement => {
               />
             </RadioGroup>
           </FormControl>
+          <hr className="margin-y-2" />
           {business?.profileData[props.onboardingKey] && (
             <div data-testid="post-onboarding-true-content">
               <Content>{onboardingQuestion.contentMd}</Content>
