d59cdbccf0eaefab9b9710097a87ba9851c0bf1c
adidner <adidner@gmail.com>
2023-08-03 14:32:31 -0400

    feat: [#185355762] update broken link checker to only run locally and allow for file download

--- web/src/pages/mgmt/deadurls.tsx
+++ web/src/pages/mgmt/deadurls.tsx
@@ -1,4 +1,5 @@
 import { MgmtAuth } from "@/components/auth/MgmtAuth";
+import { PrimaryButton } from "@/components/njwds-extended/PrimaryButton";
 import { SingleColumnContainer } from "@/components/njwds/SingleColumnContainer";
 import { PageSkeleton } from "@/components/PageSkeleton";
 import { findDeadLinks } from "@/lib/static/admin/findDeadLinks";
@@ -15,9 +16,50 @@ const DeadUrlsPage = (props: Props): ReactElement => {
   const [isAuthed, setIsAuthed] = useState<boolean>(false);
   const [password, setPassword] = useState<string>("");
 
+  const generateContent = (): string => {
+    let deadLinkFileOutput = "Possibly Blocked Or Deadlinks \n\n";
+
+    Object.keys(props.deadLinks).map((page) => {
+      if (props.deadLinks[page].length > 0) {
+        deadLinkFileOutput = `${deadLinkFileOutput}\n\n Page: ${page}`;
+        {
+          props.deadLinks[page].map((link) => {
+            deadLinkFileOutput = `${deadLinkFileOutput}\n\t${link}`;
+          });
+        }
+      }
+    });
+    return deadLinkFileOutput;
+  };
+
+  const handleDownloadClick = (): void => {
+    const content = generateContent();
+
+    const blob = new Blob([content], { type: "text/plain" });
+    const blobUrl = URL.createObjectURL(blob);
+
+    const link = document.createElement("a");
+    link.href = blobUrl;
+    link.download = "deadurls.txt";
+    link.textContent = "Download Text File";
+
+    document.body.append(link);
+    link.click();
+
+    URL.revokeObjectURL(blobUrl);
+    link.remove();
+  };
+
   const authedView = (
     <>
       <h1>Dead URLs</h1>
+      <h2>
+        NOTE: this only works locally, (ask a dev to run this locally for results
+        https://www.pivotaltracker.com/story/show/185355762)
+      </h2>
+      <PrimaryButton onClick={handleDownloadClick} isColor={"primary"}>
+        Click Here to Download, as a txt file
+      </PrimaryButton>
       <h2>Potentially broken links:</h2>
       {Object.keys(props.deadLinks).map((page, i) => {
         return (
