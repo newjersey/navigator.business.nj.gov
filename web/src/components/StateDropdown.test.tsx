dbef46c2de8bf356bec6b97a89b710d4d93b00ff
Faraz <fa8338@gmail.com>
2023-08-22 17:02:55 -0400

    feat: [#184749103] remove US Territories from formation state dropdown

---
+++ web/src/components/StateDropdown.test.tsx
@@ -0,0 +1,32 @@
+import { StateDropdown } from "@/components/StateDropdown";
+import { fireEvent, render, screen } from "@testing-library/react";
+
+describe("<StateDropdown />", () => {
+  beforeEach(() => {});
+
+  it("renders list with NJ and US Territories", () => {
+    render(
+      <StateDropdown value={undefined} fieldName={"test"} onSelect={(): void => {}} excludeNJ={false} />
+    );
+    fireEvent.click(screen.getByTestId("test"));
+    expect(screen.getByText("NJ")).toBeInTheDocument();
+    expect(screen.getByText("AS")).toBeInTheDocument();
+    expect(screen.getByText("VI")).toBeInTheDocument();
+    expect(screen.getByText("GU")).toBeInTheDocument();
+  });
+
+  it("renders list without US Territories", () => {
+    render(
+      <StateDropdown
+        value={undefined}
+        fieldName={"test"}
+        onSelect={(): void => {}}
+        excludeTerritories={true}
+      />
+    );
+    fireEvent.click(screen.getByTestId("test"));
+    expect(screen.queryByText("AS")).not.toBeInTheDocument();
+    expect(screen.queryByText("VI")).not.toBeInTheDocument();
+    expect(screen.queryByText("GU")).not.toBeInTheDocument();
+  });
+});
