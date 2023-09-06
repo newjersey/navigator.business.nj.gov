57aa19abdf2d49addbd10485139a869b963c2db8
Anne LoVerso <anne.loverso@gmail.com>
2023-09-06 12:13:17 -0400

    fix: [#185977080] prevent not-registered card from showing after registration

--- web/src/lib/auth/useAuthAlertPage.ts
+++ web/src/lib/auth/useAuthAlertPage.ts
@@ -1,13 +1,11 @@
 import { AuthAlertContext } from "@/contexts/authAlertContext";
 import { IsAuthenticated } from "@/lib/auth/AuthContext";
-import { useSidebarCards } from "@/lib/data-hooks/useSidebarCards";
 import { checkQueryValue, QUERIES } from "@/lib/domain-logic/routes";
 import { useMountEffectWhenDefined } from "@/lib/utils/helpers";
 import { useRouter } from "next/router";
 import { useContext, useEffect } from "react";
 
 export const useAuthAlertPage = (): void => {
-  const { showCard } = useSidebarCards();
   const { isAuthenticated, setRegistrationAlertIsVisible, registrationModalIsVisible } =
     useContext(AuthAlertContext);
   const router = useRouter();
@@ -18,10 +16,6 @@ export const useAuthAlertPage = (): void => {
     ) {
       setRegistrationAlertIsVisible(true);
       return;
-    }
-
-    if (isAuthenticated === IsAuthenticated.FALSE) {
-      showCard("not-registered");
     }
 
     setRegistrationAlertIsVisible(false);
