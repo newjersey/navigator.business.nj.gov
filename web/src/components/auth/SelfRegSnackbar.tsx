bcdee047dabac1504e3a4325f084bf506032aa94
adidner <adidner@gmail.com>
2023-09-07 10:54:40 -0400

    feat: [#185455538] adjusted error alert when email is taken

--- web/src/components/auth/SelfRegSnackbar.tsx
+++ web/src/components/auth/SelfRegSnackbar.tsx
@@ -5,8 +5,8 @@ import { Icon } from "@/components/njwds/Icon";
 import { AuthAlertContext } from "@/contexts/authAlertContext";
 import { AuthContext } from "@/contexts/authContext";
 import { IsAuthenticated } from "@/lib/auth/AuthContext";
+import { useConfig } from "@/lib/data-hooks/useConfig";
 import analytics from "@/lib/utils/analytics";
-import Config from "@businessnjgovnavigator/content/fieldConfig/config.json";
 import { RegistrationStatus } from "@businessnjgovnavigator/shared/";
 import { IconButton } from "@mui/material";
 import { ReactElement, useContext, useEffect } from "react";
@@ -15,6 +15,7 @@ export const SelfRegSnackbar = (): ReactElement => {
   const { isAuthenticated, registrationAlertStatus, setRegistrationAlertStatus } =
     useContext(AuthAlertContext);
   const { state } = useContext(AuthContext);
+  const { Config } = useConfig();
 
   useEffect(() => {
     if (registrationAlertStatus === "IN_PROGRESS" && isAuthenticated === IsAuthenticated.TRUE) {
@@ -36,7 +37,7 @@ export const SelfRegSnackbar = (): ReactElement => {
     RESPONSE_ERROR: Config.selfRegistration.errorTextGeneric,
   };
 
-  const getTitle = (): string => {
+  const getSuccessTitle = (): string => {
     return state.activeUser?.encounteredMyNjLinkingError
       ? Config.navigationDefaults.guestSuccessTitleExistingAccount
       : Config.navigationDefaults.guestSuccessTitle;
@@ -46,48 +47,55 @@ export const SelfRegSnackbar = (): ReactElement => {
     return <></>;
   }
 
-  return (
-    <SnackbarAlert
-      isOpen={!!registrationAlertStatus}
-      close={(): void => setRegistrationAlertStatus(undefined)}
-      variant={alertMap[registrationAlertStatus]}
-      noIcon={true}
-      autoHideDuration={null}
-      snackBarProps={{ sx: { paddingX: 0 } }}
-      dataTestid="reg-snackbar"
-    >
-      <div className="fin fac padding-y-2 padding-right-3">
-        {alertMap[registrationAlertStatus] === "success" ? (
+  if (alertMap[registrationAlertStatus] === "success") {
+    return (
+      <SnackbarAlert
+        isOpen={!!registrationAlertStatus}
+        close={(): void => setRegistrationAlertStatus(undefined)}
+        variant={alertMap[registrationAlertStatus]}
+        noIcon={true}
+        autoHideDuration={null}
+        snackBarProps={{ sx: { paddingX: 0 } }}
+        dataTestid="reg-snackbar"
+      >
+        <div className={"fin fac padding-y-2 padding-right-3"}>
           <img
             src={`/img/congratulations-green.svg`}
             alt="congratulations"
             data-testid={"congratulations-logo"}
             style={{ marginRight: "20px", width: "64px", height: "64px" }}
           />
-        ) : (
-          <></>
-        )}
-        <div>
-          {alertMap[registrationAlertStatus] === "success" ? (
-            <h3 className="padding-right-2">{getTitle()}</h3>
-          ) : (
-            <></>
-          )}
-          <IconButton
-            aria-label="close"
-            onClick={(): void => setRegistrationAlertStatus(undefined)}
-            sx={{
-              position: "absolute",
-              right: 0,
-              top: 0,
-              color: "#757575",
-            }}
-          >
-            <Icon className="usa-icon--size-4">close</Icon>
-          </IconButton>
-          <Content>{contentMap[registrationAlertStatus]}</Content>
+          <div>
+            <h3 className="padding-right-2">{getSuccessTitle()}</h3>
+            <IconButton
+              aria-label="close"
+              onClick={(): void => setRegistrationAlertStatus(undefined)}
+              sx={{
+                position: "absolute",
+                right: 0,
+                top: 0,
+                color: "#757575",
+              }}
+            >
+              <Icon className={"usa-icon--size-4"}>close</Icon>
+            </IconButton>
+            <Content>{contentMap[registrationAlertStatus]}</Content>
+          </div>
         </div>
-      </div>
+      </SnackbarAlert>
+    );
+  }
+  return (
+    <SnackbarAlert
+      isOpen={!!registrationAlertStatus}
+      close={(): void => setRegistrationAlertStatus(undefined)}
+      closeIcon={true}
+      variant={alertMap[registrationAlertStatus]}
+      autoHideDuration={null}
+      dataTestid="reg-snackbar"
+      heading={Config.profileDefaults.default.errorTextHeader}
+    >
+      <Content>{contentMap[registrationAlertStatus]}</Content>
     </SnackbarAlert>
   );
 };
