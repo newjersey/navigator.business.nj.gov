6249c851ca7810537cb2caa1fc5b68cc36a07c64
JP Hechter <jhechter@fearless.tech>
2023-09-07 16:20:14 -0400

    feat: [#184432499] add accent-cool-darkest to additional links and unstyled buttons

--- web/src/components/njwds-extended/SidebarPageLayout.tsx
+++ web/src/components/njwds-extended/SidebarPageLayout.tsx
@@ -35,10 +35,10 @@ export const SidebarPageLayout = ({
         className="usa-link fdr fac margin-top-3 margin-bottom-3 usa-link-hover-override desktop:margin-top-0 desktop:margin-bottom-4"
         onClick={analytics.event.task_back_to_roadmap.click.view_roadmap}
       >
-        <div className="circle-3 bg-primary icon-green-bg-color-hover">
+        <div className="circle-3 bg-accent-cool-darkest icon-blue-bg-color-hover">
           <Icon className="text-white usa-icon--size-3">arrow_back</Icon>
         </div>
-        <div className="margin-left-2 margin-y-auto text-primary font-sans-xs usa-link text-green-color-hover">
+        <div className="margin-left-2 margin-y-auto text-accent-cool-darkest font-sans-xs usa-link text-blue-color-hover">
           {Config.taskDefaults.backToRoadmapText}
         </div>
       </a>
