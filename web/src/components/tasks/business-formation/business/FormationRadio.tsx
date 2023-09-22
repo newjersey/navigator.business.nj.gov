2e0ec12dcc35033dff74f57a3044da95f4113ff2
Anne LoVerso <anne.loverso@gmail.com>
2023-09-22 15:08:37 -0400

    refactor: [#185628613] combine nonprofit provisions and partnership rights fields

---
+++ web/src/components/tasks/business-formation/business/FormationRadio.tsx
@@ -0,0 +1,71 @@
+import { Content } from "@/components/Content";
+import { FormationField } from "@/components/tasks/business-formation/FormationField";
+import { WithErrorBar } from "@/components/WithErrorBar";
+import { BusinessFormationContext } from "@/contexts/businessFormationContext";
+import { useConfig } from "@/lib/data-hooks/useConfig";
+import { useFormationErrors } from "@/lib/data-hooks/useFormationErrors";
+import { camelCaseToSentence } from "@/lib/utils/cases-helpers";
+import { FormationFields, InFormInBylaws } from "@businessnjgovnavigator/shared/formationData";
+import { FormControl, FormControlLabel, FormHelperText, Radio, RadioGroup } from "@mui/material";
+import { ChangeEvent, ReactElement, useContext } from "react";
+
+type InFormBylawsRadioType = Exclude<InFormInBylaws, undefined>;
+type TrueFalseRadioType = "true" | "false";
+
+interface Props {
+  fieldName: FormationFields;
+  title: string;
+  values: InFormBylawsRadioType[] | TrueFalseRadioType[];
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const FormationRadio = (props: Props): ReactElement => {
+  const { doesFieldHaveError } = useFormationErrors();
+  const { state } = useContext(BusinessFormationContext);
+  const { Config } = useConfig();
+
+  const hasError = doesFieldHaveError(props.fieldName);
+
+  const getRadioLabel = (value: InFormBylawsRadioType | TrueFalseRadioType): string => {
+    const labelMap = {
+      true: Config.formation.nonprofitProvisions.radioYesText,
+      false: Config.formation.nonprofitProvisions.radioNoText,
+      IN_FORM: Config.formation.nonprofitProvisions.radioInFormText,
+      IN_BYLAWS: Config.formation.nonprofitProvisions.radioInBylawsText,
+    };
+
+    return labelMap[value];
+  };
+
+  return (
+    <WithErrorBar className="margin-top-2" hasError={hasError} type="ALWAYS">
+      <strong>
+        <Content>{props.title}</Content>
+      </strong>
+      <FormationField fieldName={props.fieldName}>
+        <FormControl error={hasError}>
+          <RadioGroup
+            aria-label={camelCaseToSentence(props.fieldName)}
+            name={camelCaseToSentence(props.fieldName)}
+            value={state.formationFormData[props.fieldName]?.toString() ?? ""}
+            onChange={props.onChange}
+            row
+          >
+            {props.values.map((value) => (
+              <FormControlLabel
+                key={`${props.fieldName}-${value}`}
+                style={{ alignItems: "center" }}
+                value={value}
+                control={
+                  <Radio data-testid={`${props.fieldName}-${value}`} color={hasError ? "error" : "primary"} />
+                }
+                label={getRadioLabel(value)}
+              />
+            ))}
+          </RadioGroup>
+          <FormHelperText>{hasError ? Config.formation.general.genericErrorText : ""}</FormHelperText>
+        </FormControl>
+      </FormationField>
+    </WithErrorBar>
+  );
+};
