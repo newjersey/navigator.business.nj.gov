2cfb8936eda12e3b72cd83182c15bfa7a90bc913
Anne LoVerso <anne.loverso@gmail.com>
2023-09-25 12:40:26 -0400

    refactor: [#186099467] reorganize Header tests to use describe blocks

--- web/src/components/Header.test.tsx
+++ web/src/components/Header.test.tsx
@@ -44,88 +44,85 @@ describe("<Header />", () => {
     );
   };
 
-  it("shows date based on New Jersey Local time", () => {
-    const date = getCurrentDateInNewJersey().format("MMMM DD, YYYY");
-    useMockProfileData({});
-    renderHeader();
-
-    expect(screen.getByText(date)).toBeInTheDocument();
-  });
-
-  it("displays guest mode content when user is not authenticated and routes to profile page on button click", () => {
-    useMockProfileData({
-      businessName: "Business Name",
-      legalStructureId: randomPublicFilingLegalStructure(),
+  describe("heading", () => {
+    it("greets user when name is undefined", () => {
+      useMockUserData({ user: generateUser({ name: undefined }) });
+      renderHeader();
+      expect(screen.getByText(Config.headerDefaults.noUserNameHeaderText)).toBeInTheDocument();
     });
-    renderHeaderWithAuth({ isAuthenticated: IsAuthenticated.FALSE });
 
-    fireEvent.click(screen.getByText(Config.headerDefaults.guestModeToProfileButtonText));
-
-    expect(mockPush).toHaveBeenCalledWith(ROUTES.profile);
-  });
-
-  it("displays generic content when business name is not an empty string and user is authenticated then routes to profile page on button click", () => {
-    useMockProfileData({ businessName: "", legalStructureId: randomPublicFilingLegalStructure() });
-    renderHeaderWithAuth({ isAuthenticated: IsAuthenticated.TRUE });
-
-    fireEvent.click(screen.getByText(Config.headerDefaults.genericToProfileButtonText));
-
-    expect(mockPush).toHaveBeenCalledWith(ROUTES.profile);
-  });
-
-  it("displays generic content when trade name is undefined and user is authenticated then routes to profile page on button click", () => {
-    useMockProfileData({ tradeName: undefined, legalStructureId: randomTradeNameLegalStructure() });
-    renderHeaderWithAuth({ isAuthenticated: IsAuthenticated.TRUE });
-
-    fireEvent.click(screen.getByText(Config.headerDefaults.genericToProfileButtonText));
-
-    expect(mockPush).toHaveBeenCalledWith(ROUTES.profile);
-  });
-
-  it("displays generic content when business name is undefined and user is authenticated then routes to profile page on button click", () => {
-    useMockProfileData({ businessName: undefined, legalStructureId: randomPublicFilingLegalStructure() });
-    renderHeaderWithAuth({ isAuthenticated: IsAuthenticated.TRUE });
-
-    fireEvent.click(screen.getByText(Config.headerDefaults.genericToProfileButtonText));
-
-    expect(mockPush).toHaveBeenCalledWith(ROUTES.profile);
-  });
-
-  it("displays business name when legal structure is public filing", () => {
-    const businessName = `Test Company ${randomInt(6)}`;
-    useMockProfileData({
-      businessName: businessName,
-      tradeName: "tradeName",
-      legalStructureId: randomPublicFilingLegalStructure(),
+    it("includes user full name in header", () => {
+      useMockUserData({ user: generateUser({ name: "Ada Lovelace" }) });
+      renderHeader();
+      const expectedHeaderText = templateEval(Config.headerDefaults.defaultHeaderText, {
+        name: "Ada Lovelace",
+      });
+      expect(screen.getByText(expectedHeaderText)).toBeInTheDocument();
     });
-    renderHeaderWithAuth({ isAuthenticated: IsAuthenticated.TRUE });
-
-    expect(screen.getByText(businessName)).toBeInTheDocument();
   });
 
-  it("displays trade name when legal structure is trade name", () => {
-    const tradeName = `Test Company ${randomInt(6)}`;
-    useMockProfileData({
-      businessName: "businessName",
-      tradeName: tradeName,
-      legalStructureId: randomTradeNameLegalStructure(),
+  describe("date", () => {
+    it("shows date based on New Jersey Local time", () => {
+      const date = getCurrentDateInNewJersey().format("MMMM DD, YYYY");
+      useMockProfileData({});
+      renderHeader();
+
+      expect(screen.getByText(date)).toBeInTheDocument();
     });
-    renderHeaderWithAuth({ isAuthenticated: IsAuthenticated.TRUE });
-    expect(screen.getByText(tradeName)).toBeInTheDocument();
   });
 
-  it("greets user when name is undefined", () => {
-    useMockUserData({ user: generateUser({ name: undefined }) });
-    renderHeader();
-    expect(screen.getByText(Config.headerDefaults.noUserNameHeaderText)).toBeInTheDocument();
-  });
-
-  it("includes user full name in header", () => {
-    useMockUserData({ user: generateUser({ name: "Ada Lovelace" }) });
-    renderHeader();
-    const expectedHeaderText = templateEval(Config.headerDefaults.defaultHeaderText, {
-      name: "Ada Lovelace",
+  describe("go to profile link", () => {
+    it("displays guest mode content when user is not authenticated and routes to profile page on button click", () => {
+      useMockProfileData({
+        businessName: "Business Name",
+        legalStructureId: randomPublicFilingLegalStructure(),
+      });
+      renderHeaderWithAuth({ isAuthenticated: IsAuthenticated.FALSE });
+      fireEvent.click(screen.getByText(Config.headerDefaults.guestModeToProfileButtonText));
+      expect(mockPush).toHaveBeenCalledWith(ROUTES.profile);
     });
-    expect(screen.getByText(expectedHeaderText)).toBeInTheDocument();
+
+    it("displays generic content when business name is not an empty string and user is authenticated then routes to profile page on button click", () => {
+      useMockProfileData({ businessName: "", legalStructureId: randomPublicFilingLegalStructure() });
+      renderHeaderWithAuth({ isAuthenticated: IsAuthenticated.TRUE });
+      fireEvent.click(screen.getByText(Config.headerDefaults.genericToProfileButtonText));
+      expect(mockPush).toHaveBeenCalledWith(ROUTES.profile);
+    });
+
+    it("displays generic content when trade name is undefined and user is authenticated then routes to profile page on button click", () => {
+      useMockProfileData({ tradeName: undefined, legalStructureId: randomTradeNameLegalStructure() });
+      renderHeaderWithAuth({ isAuthenticated: IsAuthenticated.TRUE });
+      fireEvent.click(screen.getByText(Config.headerDefaults.genericToProfileButtonText));
+      expect(mockPush).toHaveBeenCalledWith(ROUTES.profile);
+    });
+
+    it("displays generic content when business name is undefined and user is authenticated then routes to profile page on button click", () => {
+      useMockProfileData({ businessName: undefined, legalStructureId: randomPublicFilingLegalStructure() });
+      renderHeaderWithAuth({ isAuthenticated: IsAuthenticated.TRUE });
+      fireEvent.click(screen.getByText(Config.headerDefaults.genericToProfileButtonText));
+      expect(mockPush).toHaveBeenCalledWith(ROUTES.profile);
+    });
+
+    it("displays business name when legal structure is public filing", () => {
+      const businessName = `Test Company ${randomInt(6)}`;
+      useMockProfileData({
+        businessName: businessName,
+        tradeName: "tradeName",
+        legalStructureId: randomPublicFilingLegalStructure(),
+      });
+      renderHeaderWithAuth({ isAuthenticated: IsAuthenticated.TRUE });
+      expect(screen.getByText(businessName)).toBeInTheDocument();
+    });
+
+    it("displays trade name when legal structure is trade name", () => {
+      const tradeName = `Test Company ${randomInt(6)}`;
+      useMockProfileData({
+        businessName: "businessName",
+        tradeName: tradeName,
+        legalStructureId: randomTradeNameLegalStructure(),
+      });
+      renderHeaderWithAuth({ isAuthenticated: IsAuthenticated.TRUE });
+      expect(screen.getByText(tradeName)).toBeInTheDocument();
+    });
   });
 });
