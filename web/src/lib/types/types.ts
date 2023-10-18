b3cd95ed8bb192414956bea6937af47d49b2c055
Spencer Wohlers <4006420+seidior@users.noreply.github.com>
2023-10-18 11:36:12 -0700

    feat: [#185151322] support CTA dropdowns as well as buttons for tasks with post-onboarding
    Linked story: [#185151322](https://www.pivotaltracker.com/story/show/185151322).

--- web/src/lib/types/types.ts
+++ web/src/lib/types/types.ts
@@ -420,6 +420,11 @@ export interface PostOnboarding {
   radioNo: string;
   radioNoContent: string;
   filename: string;
+  callToActionYesText1?: string | undefined;
+  callToActionYesLink1?: string | undefined;
+  callToActionYesText2?: string | undefined;
+  callToActionYesLink2?: string | undefined;
+  callToActionYesDropdownText?: string | undefined;
 }
 
 export interface PostOnboardingFile extends PostOnboarding {
