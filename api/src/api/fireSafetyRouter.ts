2e506caeb5cd9ce8073898d377f1357af5e907aa
sirsamueljoseph <samueljberin@gmail.com>
2023-11-21 07:26:16 -0700

    feat: [#186519052] Add fire safety to api

---
+++ api/src/api/fireSafetyRouter.ts
@@ -0,0 +1,21 @@
+import { FireSafetyInspection } from "@domain/types";
+
+import { FireSafetyInspectionResult } from "@shared/fireSafety";
+import { Router } from "express";
+
+export const fireSafetyRouterFactory = (fireSafetyInspection: FireSafetyInspection): Router => {
+  const router = Router();
+
+  router.get("/fire-safety", async (req, res) => {
+    const { address: address } = req.body;
+    fireSafetyInspection(address)
+      .then(async (inspectionData: FireSafetyInspectionResult[]) => {
+        return res.json(inspectionData);
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
+  });
+
+  return router;
+};
