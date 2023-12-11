49b22fdd8a8faf805a20cb7ef523579bedcf8b89
Spencer Wohlers <4006420+seidior@users.noreply.github.com>
2023-12-11 14:04:10 -0800

    chore: proposed configuration changes to make Jest more reliable on CircleCI

--- shared/jest.config.ts
+++ shared/jest.config.ts
@@ -3,7 +3,18 @@ import type { Config } from "@jest/types";
 export default async (): Promise<Config.InitialOptions> => {
   return {
     coverageReporters: ["json-summary", "text"],
-    reporters: ["default", "jest-junit"],
+    reporters: [
+      "default",
+      [
+        "jest-junit",
+        {
+          outputDirectory: "../coverage/test_results",
+          uniqueOutputName: "false",
+          outputName: "shared-jest.xml",
+          addFileAttribute: "true",
+        },
+      ],
+    ],
     // eslint-disable-next-line unicorn/prefer-module
     ...require("ts-jest/jest-preset"),
     verbose: true,
--- web/jest.config.ts
+++ web/jest.config.ts
@@ -1,44 +1,55 @@
-import type { Config } from "@jest/types";
+import type { Config } from "jest";
 
 const esModules = ["rehype-react", "remark-gfm", "remark-parse", "remark-rehype", "unified"];
 
-export default async (): Promise<Config.InitialOptions> => {
-  return {
-    coverageReporters: ["json-summary", "text"],
-    setupFilesAfterEnv: ["./setupTests.js"],
-    testEnvironment: "jsdom",
-    testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/", "<rootDir>/cypress/"],
-    rootDir: "./",
-    moduleDirectories: ["node_modules", "<rootDir>"],
-    moduleNameMapper: {
-      "\\.(scss|sass|css)$": "identity-obj-proxy",
-      "@/components/(.*)": "<rootDir>/src/components/$1",
-      "@/contexts/(.*)": "<rootDir>/src/contexts/$1",
-      "@/lib/(.*)": "<rootDir>/src/lib/$1",
-      "@/test/(.*)": "<rootDir>/test/$1",
-      "@/pages/(.*)": "<rootDir>/src/pages/$1",
-      "@/styles/(.*)": "<rootDir>/src/styles/$1",
-      "@businessnjgovnavigator/shared/(.*)": "<rootDir>/../shared/lib/shared/src/$1",
-      "@businessnjgovnavigator/content/(.*)": "<rootDir>/../content/src/$1",
-    },
-    resolver: `${__dirname}/test/resolver.js`,
-    transformIgnorePatterns: [`<rootDir>/node_modules/(?!(${esModules.join("|")}))`],
-    transform: {
-      "\\.md$": "<rootDir>/test/jest-raw-loader.js",
-      "\\.m?[jt]sx?$": [
-        "@swc/jest",
-        {
-          jsc: {
-            transform: {
-              react: {
-                runtime: "automatic",
-              },
+const config: Config = {
+  coverageReporters: ["json-summary", "text"],
+  setupFilesAfterEnv: ["./setupTests.js"],
+  testEnvironment: "jsdom",
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/", "<rootDir>/cypress/"],
+  rootDir: "./",
+  moduleDirectories: ["node_modules", "<rootDir>"],
+  moduleNameMapper: {
+    "\\.(scss|sass|css)$": "identity-obj-proxy",
+    "@/components/(.*)": "<rootDir>/src/components/$1",
+    "@/contexts/(.*)": "<rootDir>/src/contexts/$1",
+    "@/lib/(.*)": "<rootDir>/src/lib/$1",
+    "@/test/(.*)": "<rootDir>/test/$1",
+    "@/pages/(.*)": "<rootDir>/src/pages/$1",
+    "@/styles/(.*)": "<rootDir>/src/styles/$1",
+    "@businessnjgovnavigator/shared/(.*)": "<rootDir>/../shared/lib/shared/src/$1",
+    "@businessnjgovnavigator/content/(.*)": "<rootDir>/../content/src/$1",
+  },
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: "../coverage/test_results",
+        outputName: "web-jest.xml",
+        uniqueOutputName: "false",
+        addFileAttribute: "true",
+      },
+    ],
+  ],
+  resolver: `${__dirname}/test/resolver.js`,
+  transformIgnorePatterns: [`<rootDir>/node_modules/(?!(${esModules.join("|")}))`],
+  transform: {
+    "\\.md$": "<rootDir>/test/jest-raw-loader.js",
+    "\\.m?[jt]sx?$": [
+      "@swc/jest",
+      {
+        jsc: {
+          transform: {
+            react: {
+              runtime: "automatic",
             },
           },
         },
-      ],
-    },
-  };
+      },
+    ],
+  },
+  verbose: true,
 };
 
 process.env = Object.assign(process.env, {
@@ -46,3 +57,5 @@ process.env = Object.assign(process.env, {
   FEATURE_EXPORT_PDF: "true",
   WEBFLOW_API_TOKEN: 12345678910,
 });
+
+export default config;
