fe36819fb27888de5dfd0e94cad230ac1fc828ab
Faraz <fa8338@gmail.com>
2024-02-06 11:11:48 -0500

    chore: [#186993249] add funding card preview in cms

--- web/src/lib/cms/previews/FundingsPreview.tsx
+++ web/src/lib/cms/previews/FundingsPreview.tsx
@@ -1,3 +1,4 @@
+import { OpportunityCard } from "@/components/dashboard/OpportunityCard";
 import { PreviewProps } from "@/lib/cms/helpers/previewHelpers";
 import { usePageData } from "@/lib/cms/helpers/usePageData";
 import { usePreviewRef } from "@/lib/cms/helpers/usePreviewRef";
@@ -11,6 +12,9 @@ const FundingsPreview = (props: PreviewProps): ReactElement => {
 
   return (
     <div className="cms" ref={ref} style={{ margin: 40, pointerEvents: "none" }}>
+      <h1 className="margin-top-5">----------Card in For You Section (My Account)----------</h1>
+      <OpportunityCard key={funding.id} opportunity={funding} urlPath="funding" />
+      <h1 className="margin-top-5">----------Funding Details Page (My Account)----------</h1>
       <FundingElement funding={funding} />
     </div>
   );
