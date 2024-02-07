7fd8e5679e4557e0922c4c6df32773ae525474fe
AnJu Hyppolite <anjuhyppolite@pursuit.org>
2024-02-07 11:37:45 -0500

    Revert "Merge pull request #7157 from newjersey/biz-name-search-error-#184764052"
    This reverts commit b564b02e51fc48026b4afd05d6a4625741e14f27, reversing
    changes made to 23857b2b393c2b1b3d9c3b4c3d71367760c3b39f.

--- web/src/components/tasks/business-formation/getErrorStateForField.test.ts
+++ web/src/components/tasks/business-formation/getErrorStateForField.test.ts
@@ -88,7 +88,7 @@ describe("getErrorStateForField", () => {
       ).toEqual(Config.formation.fields.businessName.errorInlineNeedsToSearch);
     });
 
-    it("sets label to undefined if name availability is UNAVAILABLE", () => {
+    it("uses errorInlineUnavailable as label if name availability is UNAVAILABLE", () => {
       const formationFormData = generateFormationFormData({ businessName: "some name" });
       const businessNameAvailability = generateBusinessNameAvailability({ status: "UNAVAILABLE" });
       expect(
@@ -97,10 +97,10 @@ describe("getErrorStateForField", () => {
           formationFormData,
           businessNameAvailability,
         }).label
-      ).toEqual(undefined);
+      ).toEqual(Config.formation.fields.businessName.errorInlineUnavailable);
     });
 
-    it("sets label to undefined when name availability is error", () => {
+    it("uses errorInlineUnavailable as label if name availability is error", () => {
       const formationFormData = generateFormationFormData({ businessName: "some name" });
       const businessNameAvailability = generateBusinessNameAvailability({ status: "DESIGNATOR_ERROR" });
       expect(
@@ -109,7 +109,7 @@ describe("getErrorStateForField", () => {
           formationFormData,
           businessNameAvailability,
         }).label
-      ).toEqual(undefined);
+      ).toEqual(Config.formation.fields.businessName.errorInlineUnavailable);
     });
 
     it("uses field name as label if name availability is AVAILABLE", () => {
