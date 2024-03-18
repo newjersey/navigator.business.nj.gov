434d13479f147f34a34b09c68d9e451825ed4d77
Soma Sai Kumar Badri <somasai.badri@gmail.com>
2024-03-18 09:51:47 -0700

    chore: rename

--- api/src/functions/healthCheck/app.ts
+++ api/src/functions/healthCheck/app.ts
@@ -3,6 +3,6 @@ import { LogWriter } from "@libs/logWriter";
 
 export default async function handler(): Promise<void> {
   const STAGE = process.env.STAGE || "local";
-  const logger = LogWriter(`HealthCheckWebService/${STAGE}`, "ApiLogs");
+  const logger = LogWriter(`HealthCheckService/${STAGE}`, "ApiLogs");
   await runHealthChecks(logger);
 }
