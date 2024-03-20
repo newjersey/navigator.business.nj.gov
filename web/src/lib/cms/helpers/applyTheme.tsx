290120086973106c137fcfa17cd800b469e03eb7
Faraz <fa8338@gmail.com>
2024-03-20 14:13:13 -0400

    chore: apply correct theme to CMS preview

--- web/src/lib/cms/helpers/applyTheme.tsx
+++ web/src/lib/cms/helpers/applyTheme.tsx
@@ -1,11 +1,12 @@
 import { PreviewProps } from "@/lib/cms/helpers/previewHelpers";
-import { createTheme, ThemeProvider } from "@mui/material";
+import { ThemeProvider } from "@mui/material";
+import muiTheme from "../../muiTheme";
 
 type ReturnType = (props: PreviewProps) => JSX.Element;
 
 export const applyTheme = (child: ReturnType): ReturnType => {
   // eslint-disable-next-line react/display-name
   return (props: PreviewProps): JSX.Element => {
-    return <ThemeProvider theme={createTheme()}>{child(props)}</ThemeProvider>;
+    return <ThemeProvider theme={muiTheme}>{child(props)}</ThemeProvider>;
   };
 };
