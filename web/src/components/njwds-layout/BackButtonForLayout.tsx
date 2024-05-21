2550ddafca09c9d9876fbe909cbd6a9f9904e041
JP Hechter <jhechter@fearless.tech>
2024-05-21 17:31:38 -0400

    chore: reduce back button clickable area

--- web/src/components/njwds-layout/BackButtonForLayout.tsx
+++ web/src/components/njwds-layout/BackButtonForLayout.tsx
@@ -18,7 +18,7 @@ export const BackButtonForLayout = (props: Props): ReactElement => {
         <a
           href={ROUTES.dashboard}
           data-testid="back-to-dashboard"
-          className="usa-link fdr fac margin-top-3 margin-bottom-3 usa-link-hover-override desktop:margin-top-0 desktop:margin-bottom-4"
+          className="usa-link fdr fac margin-top-3 margin-bottom-3 usa-link-hover-override desktop:margin-top-0 desktop:margin-bottom-4 width-max-content"
           onClick={analytics.event.task_back_to_roadmap.click.view_roadmap}
         >
           <div className="circle-3 bg-accent-cool-darkest icon-blue-bg-color-hover">
