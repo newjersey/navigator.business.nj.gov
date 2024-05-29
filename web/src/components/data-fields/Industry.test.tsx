047e9e52a04b59eedbd2902459484e6893ad50a3
Spencer Wohlers <4006420+seidior@users.noreply.github.com>
2024-05-29 12:16:29 -0400

    wip [skip ci]

--- web/src/components/data-fields/Industry.test.tsx
+++ web/src/components/data-fields/Industry.test.tsx
@@ -100,10 +100,8 @@ describe("<Industry />", () => {
       businessPersonas.map((persona) => {
         const choices = industrySpecificDataChoices[el.fieldName];
         const flowConfig = Config.profileDefaults.fields;
-        const fieldContent = Config.profileDefaults.fields[
-          el.fieldName as keyof typeof flowConfig
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        ].default as any;
+        const fieldName = el.fieldName as keyof typeof flowConfig;
+        const fieldContent = Config.profileDefaults.fields[fieldName].default;
 
         const chooseRadioWithContent = (div: HTMLElement, choice: string): void => {
           fireEvent.click(
