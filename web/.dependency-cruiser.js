bc39005d6459de831897b9dd31aff9e8e3256476
Lex Littleton <littale12@gmail.com>
2024-07-15 12:02:23 -0400

    chore: remove jspdf and html2canvas from web 🔥

--- web/.dependency-cruiser.js
+++ web/.dependency-cruiser.js
@@ -12,14 +12,6 @@ module.exports = {
     },
     {
       from: {},
-      to: { path: "jspdf" },
-    },
-    {
-      from: {},
-      to: { path: "html2canvas" },
-    },
-    {
-      from: {},
       to: { path: "@aws-crypto/sha256-browser" },
     },
     {
