1aef9edb71e4c25c52c8cfaddbab4c90fa70eee0
Lex Littleton <littale12@gmail.com>
2024-08-28 09:23:04 -0400

    chore: convert b tags to strong; extract out StepsSection component; remove aria labels from section

--- web/src/components/starter-kits/StepInfo.tsx
+++ web/src/components/starter-kits/StepInfo.tsx
@@ -7,14 +7,14 @@ export const StepInfo = (props: { step: Step; taskNames: string[] }): ReactEleme
     <div className="flex-basis-100">
       <div className="margin-top-05 margin-bottom-2">
         <span className="border-2px border-secondary-vivid-dark text-secondary-vivid-dark radius-lg padding-x-1 padding-y-05">
-          <b>STEP {props.step.stepNumber}</b>
+          <strong>STEP {props.step.stepNumber}</strong>
         </span>
       </div>
       <Heading level={3} styleVariant="h4">
         {props.step.name.replace("${OoS}", "")}
       </Heading>
       <p>{props.step.description}</p>
-      <b className="text-base-dark">{`(${props.step.timeEstimate})`}</b>
+      <strong className="text-base-dark">{`(${props.step.timeEstimate})`}</strong>
       <ul className="padding-left-205">
         {props.taskNames.map((taskName) => (
           <li key={taskName}>{taskName}</li>
---
+++ web/src/components/starter-kits/StepSection.tsx
@@ -0,0 +1,50 @@
+import { Heading } from "@/components/njwds-extended/Heading";
+import { PrimaryButton } from "@/components/njwds-extended/PrimaryButton";
+import { StepInfo } from "@/components/starter-kits/StepInfo";
+import { Roadmap, Task } from "@/lib/types/types";
+import analytics from "@/lib/utils/analytics";
+import { ReactElement } from "react";
+
+interface Props {
+  stepsTitle: string;
+  roadmap: Roadmap;
+  onCtaClick: () => void;
+  ctaText: string;
+}
+
+export const StepSection = (props: Props): ReactElement => {
+  const getTaskNamesForStep = (tasks: Task[], step: number): string[] => {
+    return tasks.filter((task) => task.stepNumber === step).map((task) => task.name);
+  };
+
+  return (
+    <section className="display-flex flex-column gap-4 margin-x-2 desktop:margin-x-neg-4 bg-cool-extra-light radius-lg margin-bottom-3 padding-x-2 padding-y-5 desktop:padding-y-10 desktop:padding-x-5">
+      <div className="padding-x-2 display-flex flex-column flex-justify gap-5 flex-align-center">
+        <div className="border-top-1 border-secondary-vivid-dark width-8" />
+        <Heading level={2}>{props.stepsTitle}</Heading>
+      </div>
+      <div className="display-flex flex-column desktop:flex-row gap-4 padding-x-2 desktop:padding-x-0">
+        {props.roadmap.steps.map((step) => (
+          <StepInfo
+            step={step}
+            taskNames={getTaskNamesForStep(props.roadmap.tasks, step.stepNumber)}
+            key={step.stepNumber}
+          />
+        ))}
+      </div>
+      <div className="flex-align-self-stretch desktop:flex-align-self-center tablet:flex-align-self-center">
+        <PrimaryButton
+          isColor={"secondary-vivid-dark"}
+          onClick={() => {
+            analytics.event.starter_kit_landing.click.get_my_starter_kit_button_footer();
+            props.onCtaClick();
+          }}
+          isRightMarginRemoved={true}
+          isLargeButton={true}
+        >
+          {props.ctaText}
+        </PrimaryButton>
+      </div>
+    </section>
+  );
+};
