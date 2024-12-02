90f38d90df667befdc28b292b207ce4978b0da84
Spencer Wohlers <4006420+seidior@users.noreply.github.com>
2024-12-02 16:34:09 -0500

    chore: [#188640176] update dynamodb download link [skip ci]

--- api/scripts/dynamodb-local.sh
+++ api/scripts/dynamodb-local.sh
@@ -4,7 +4,10 @@ cd "$(git rev-parse --show-toplevel)/api"
 
 set -e
 
-wget -c http://dynamodb-local.s3-website-us-west-2.amazonaws.com/dynamodb_local_latest.tar.gz
+# Reference: https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.DownloadingAndRunning.html#DynamoDBLocal.DownloadingAndRunning.title
+# sha256 for verification: https://d1ni2b6xgvw0s0.cloudfront.net/v2.x/dynamodb_local_latest.tar.gz.sha256
+
+wget -c https://d1ni2b6xgvw0s0.cloudfront.net/v2.x/dynamodb_local_latest.tar.gz
 rm -rf .dynamodb
 mkdir .dynamodb
 tar zxvf dynamodb_local_latest.tar.gz -C .dynamodb
