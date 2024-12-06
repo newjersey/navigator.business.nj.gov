7c43e601f15b878946c1aa2097dd8362d4fa3d2f
sirsamueljoseph <samueljberin@gmail.com>
2024-12-06 13:52:37 -0700

    feat: [#188623677] Add air waste task

---
+++ content/src/roadmaps/env-tasks/air-permitting.md
@@ -0,0 +1,8 @@
+---
+summaryDescriptionMd: The New Jersey Department of Environmental Protection's (DEP) has environmental regulations for businesses in the State.
+urlSlug: air-permitting
+filename: air-permitting
+displayname: Check Your Air Permit Requirements
+name: Check Your Air Permit Requirements
+id: air-permitting
+---
--- shared/src/environment.ts
+++ shared/src/environment.ts
@@ -1,5 +1,6 @@
 export type EnvironmentData = {
   waste?: WasteData;
+  air?: AirData;
 };
 
 export type WasteData = {
@@ -14,4 +15,13 @@ export type WasteQuestionnaireFieldIds =
   | "constructionDebris"
   | "noWaste";
 
+export type AirData = {
+  questionaireData?: AirQuestionnaireData;
+  submitted: boolean;
+};
+
+export type AirQuestionnaireFieldIds = "airPollutants" | "airEmissions" | "constructionActivities" | "noAir";
+
 export type WasteQuestionnaireData = Record<WasteQuestionnaireFieldIds, boolean>;
+
+export type AirQuestionnaireData = Record<AirQuestionnaireFieldIds, boolean>;
