c7da4b7c7fedb297404569451b72819a9971dcfe
Josephine Lee <josephine.lee@innovation.nj.gov>
2025-03-10 14:13:04 -0400

    same seed all tests

--- api/src/globalSetup.ts
+++ api/src/globalSetup.ts
@@ -1,3 +1,12 @@
 export default async (): Promise<void> => {
   process.env.INTERCOM_HASH_SECRET = "some-secret-key";
+  if (!process.env.RANDOM_SEED) {
+    process.env.RANDOM_SEED = Math.random().toString(36).slice(2);
+  }
+  const message = `Random seed: ${process.env.TESTING_SEED} ${
+    process.env.CIRCLE_NODE_INDEX
+      ? `${Number(process.env.CIRCLE_NODE_INDEX) + 1}/${process.env.CIRCLE_NODE_TOTAL} `
+      : ""
+  }(${expect.getState().currentTestName})`;
+  console.log(message);
 };
