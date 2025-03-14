387f2cc4ef91294edc2cb8fb3236ad962e9e9ec6
Josephine Lee <josephine.lee@innovation.nj.gov>
2025-03-14 12:29:44 -0400

    save [skip ci]

--- api/test/global.d.ts
+++
@@ -1,5 +0,0 @@
-declare module globalThis {
-  // It needs to be var https://stackoverflow.com/questions/68481686/type-typeof-globalthis-has-no-index-signature#answer-69230938
-  // eslint-disable-next-line no-var
-  var testRandomSeeds: Map<string, string>;
-}
--- api/test/setupFileAfterEnv.ts
+++ api/test/setupFileAfterEnv.ts
@@ -1,15 +1,19 @@
 import "jest-dynalite/withDb";
 import seedrandom from "seedrandom";
 
+type TestGlobalThis = {
+  testRandomSeeds: Map<string, string>;
+};
+
 beforeEach(function () {
   const currentTestName = expect.getState().currentTestName;
   if (currentTestName === undefined) {
     throw new Error("expect.getState().currentTestName is undefined");
   }
-  if (global.testRandomSeeds.has(currentTestName)) {
+  if ((global as unknown as TestGlobalThis).testRandomSeeds.has(currentTestName)) {
     throw new Error("Unexpected duplicate test name");
   }
   const randomSeed = process.env.RANDOM_SEED || Math.random().toString(36).slice(2);
-  global.testRandomSeeds.set(currentTestName, randomSeed);
+  (global as unknown as TestGlobalThis).testRandomSeeds.set(currentTestName, randomSeed);
   seedrandom(randomSeed, { global: true });
 });
