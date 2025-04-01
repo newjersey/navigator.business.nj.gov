e76f2b1e1b5b2310bc49ef5f4216c4cd2bbb270f
Faraz Akhtar <fa8338@gmail.com>
2025-04-01 09:47:01 -0400

    feat: [AB#7752] successful tax clearance document response

---
+++ api/src/api/taxClearanceCertificateRouter.ts
@@ -0,0 +1,25 @@
+import { ExpressRequestBody } from "@api/types";
+import { TaxClearanceCertificateClient } from "@domain/types";
+import { UserData } from "@shared/userData";
+import { Router } from "express";
+import { StatusCodes } from "http-status-codes";
+
+export const taxClearanceCertificateRouterFactory = (
+  taxClearanceCertificateClient: TaxClearanceCertificateClient
+): Router => {
+  const router = Router();
+  router.post("/postTaxClearanceCertificate", async (req: ExpressRequestBody<UserData>, res) => {
+    const userData = req.body;
+
+    try {
+      const response = await taxClearanceCertificateClient.postTaxClearanceCertificate(userData);
+      res.json(response);
+    } catch (error) {
+      // TODO: Missing Field Error
+      // TODO: Validation Error
+      // TODO: Server Error
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
+    }
+  });
+  return router;
+};
--- web/src/components/tasks/anytime-action/tax-clearance-certificate/AnytimeActionTaxClearanceCertificateElement.tsx
+++ web/src/components/tasks/anytime-action/tax-clearance-certificate/AnytimeActionTaxClearanceCertificateElement.tsx
@@ -1,5 +1,6 @@
 import { Heading } from "@/components/njwds-extended/Heading";
 import { HorizontalStepper } from "@/components/njwds-extended/HorizontalStepper";
+import { TaxClearanceDownload } from "@/components/tasks/anytime-action/tax-clearance-certificate/TaxClearanceDownload";
 import { TaxClearanceStepOne } from "@/components/tasks/anytime-action/tax-clearance-certificate/TaxClearanceStepOne";
 import { TaxClearanceStepThree } from "@/components/tasks/anytime-action/tax-clearance-certificate/TaxClearanceStepThree";
 import { TaxClearanceStepTwo } from "@/components/tasks/anytime-action/tax-clearance-certificate/TaxClearanceStepTwo";
@@ -20,6 +21,7 @@ import { ReactElement, useEffect, useState } from "react";
 interface Props {
   anytimeAction: AnytimeActionLicenseReinstatement | AnytimeActionTask;
   CMS_ONLY_stepIndex?: number;
+  CMS_ONLY_downloadPage?: [];
 }
 
 const Config = getMergedConfig();
@@ -42,6 +44,9 @@ export const AnytimeActionTaxClearanceCertificateElement = (props: Props): React
   );
   const [formationAddressData, setAddressData] = useState<FormationAddress>(emptyFormationAddressData);
   const [profileData, setProfileData] = useState<ProfileData>(createEmptyProfileData());
+  const [certificatePdfArray, setCertificatePdfArray] = useState<number[] | undefined>(
+    props.CMS_ONLY_downloadPage || undefined
+  );
 
   const saveTaxClearanceCertificateData = (): void => {
     const newTaxClearanceCertificateData = {
@@ -177,24 +182,35 @@ export const AnytimeActionTaxClearanceCertificateElement = (props: Props): React
                   <Heading level={1}>{props.anytimeAction.name}</Heading>
                 </div>
               </div>
-              <HorizontalStepper
-                steps={stateTaxClearanceCertificateSteps}
-                currentStep={stepIndex}
-                onStepClicked={(step: number): void => {
-                  if (step === 2 && stepIndex === 1) {
-                    saveTaxClearanceCertificateData();
-                  }
-                  setStepIndex(step);
-                }}
-              />
-              {stepIndex === 0 && <TaxClearanceStepOne setStepIndex={setStepIndex} />}
-              {stepIndex === 1 && (
-                <TaxClearanceStepTwo
-                  setStepIndex={setStepIndex}
-                  saveTaxClearanceCertificateData={saveTaxClearanceCertificateData}
-                />
+              {certificatePdfArray && <TaxClearanceDownload />}
+
+              {!certificatePdfArray && (
+                <>
+                  <HorizontalStepper
+                    steps={stateTaxClearanceCertificateSteps}
+                    currentStep={stepIndex}
+                    onStepClicked={(step: number): void => {
+                      if (step === 2 && stepIndex === 1) {
+                        saveTaxClearanceCertificateData();
+                      }
+                      setStepIndex(step);
+                    }}
+                  />
+                  {stepIndex === 0 && <TaxClearanceStepOne setStepIndex={setStepIndex} />}
+                  {stepIndex === 1 && (
+                    <TaxClearanceStepTwo
+                      setStepIndex={setStepIndex}
+                      saveTaxClearanceCertificateData={saveTaxClearanceCertificateData}
+                    />
+                  )}
+                  {stepIndex === 2 && (
+                    <TaxClearanceStepThree
+                      setStepIndex={setStepIndex}
+                      setCertificatePdfArray={setCertificatePdfArray}
+                    />
+                  )}
+                </>
               )}
-              {stepIndex === 2 && <TaxClearanceStepThree setStepIndex={setStepIndex} />}
             </div>
           </ProfileDataContext.Provider>
         </TaxClearanceCertificateDataContext.Provider>
