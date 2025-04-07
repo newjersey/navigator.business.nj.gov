1455b6e67c0671f7a329c5488bf59032972c5531
sirsamueljoseph <samueljberin@gmail.com>
2025-04-07 11:36:14 -0600

    feat: [AB#13557] add styling to all parts

--- web/src/components/tasks/abc-emergency-trip-permit/EmergencyTripPermit.tsx
+++ web/src/components/tasks/abc-emergency-trip-permit/EmergencyTripPermit.tsx
@@ -1,8 +1,13 @@
 import { HorizontalStepper } from "@/components/njwds-extended/HorizontalStepper";
+import { PrimaryButton } from "@/components/njwds-extended/PrimaryButton";
+import { SecondaryButton } from "@/components/njwds-extended/SecondaryButton";
+import { CtaContainer } from "@/components/njwds-extended/cta/CtaContainer";
+import { ActionBarLayout } from "@/components/njwds-layout/ActionBarLayout";
 import { EmergencyTripPermitSteps } from "@/components/tasks/abc-emergency-trip-permit/EmergencyTripPermitSteps";
 import { EmergencyTripPermitStepsConfiguration } from "@/components/tasks/abc-emergency-trip-permit/EmergencyTripPermitStepsConfiguration";
 import { EmergencyTripPermitContext } from "@/contexts/EmergencyTripPermitContext";
-import { createProfileFieldErrorMap, ProfileFormContext } from "@/contexts/profileFormContext";
+import { createDataFormErrorMap, DataFormErrorMapContext } from "@/contexts/dataFormErrorMapContext";
+import { useConfig } from "@/lib/data-hooks/useConfig";
 import { useFormContextHelper } from "@/lib/data-hooks/useFormContextHelper";
 import { StepperStep } from "@/lib/types/types";
 import {
@@ -11,6 +16,7 @@ import {
 } from "@businessnjgovnavigator/shared/emergencyTripPermit";
 import { ReactElement, useState } from "react";
 export const EmergencyTripPermit = (): ReactElement => {
+  const { Config } = useConfig();
   const [stepIndex, setStepIndex] = useState<number>(0);
   const [applicationInfo, setApplicationInfo] = useState<EmergencyTripPermitApplicationInfo>(
     generateEmptyEmergencyTripPermitData()
@@ -19,7 +25,16 @@ export const EmergencyTripPermit = (): ReactElement => {
     return { name: step.name, hasError: false };
   });
 
-  const { state: formContextState } = useFormContextHelper(createProfileFieldErrorMap());
+  const { state: formContextState } = useFormContextHelper(createDataFormErrorMap());
+
+  const getNextStepText = (): string => {
+    switch (stepIndex) {
+      case 4:
+        return Config.abcEmergencyTripPermit.submitText;
+      default:
+        return Config.abcEmergencyTripPermit.nextStepText;
+    }
+  };
 
   return (
     <EmergencyTripPermitContext.Provider
@@ -32,9 +47,11 @@ export const EmergencyTripPermit = (): ReactElement => {
         setApplicationInfo: setApplicationInfo,
       }}
     >
-      <ProfileFormContext.Provider value={formContextState}>
-        <div className="padding-bottom-4 margin-x-4 bg-accent-cool-lightest">
-          <h1>Test Name</h1>
+      <DataFormErrorMapContext.Provider value={formContextState}>
+        <div
+          className={`bg-base-extra-light margin-x-neg-4 margin-top-neg-4 padding-x-4 padding-bottom-2 padding-top-4 margin-bottom-2 radius-top-lg`}
+        >
+          <h1> {Config.abcEmergencyTripPermit.taskName}</h1>
         </div>
         <HorizontalStepper
           currentStep={stepIndex}
@@ -44,7 +61,34 @@ export const EmergencyTripPermit = (): ReactElement => {
           steps={steps}
         />
         {EmergencyTripPermitSteps[stepIndex].component}
-      </ProfileFormContext.Provider>
+        <CtaContainer>
+          <ActionBarLayout>
+            {stepIndex !== 0 && (
+              <SecondaryButton
+                onClick={() => {
+                  setStepIndex(stepIndex - 1);
+                }}
+                isColor={"primary"}
+              >
+                {Config.abcEmergencyTripPermit.previousStepText}
+              </SecondaryButton>
+            )}
+
+            <PrimaryButton
+              isColor="primary"
+              onClick={(): void => {
+                if (stepIndex < 4) {
+                  setStepIndex(stepIndex + 1);
+                }
+              }}
+              isRightMarginRemoved={true}
+              dataTestId="next-button"
+            >
+              {getNextStepText()}
+            </PrimaryButton>
+          </ActionBarLayout>
+        </CtaContainer>
+      </DataFormErrorMapContext.Provider>
     </EmergencyTripPermitContext.Provider>
   );
 };
