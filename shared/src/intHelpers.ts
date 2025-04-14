5c239360a65953c85035425a31833de49ebd01b3
Josephine Lee <josephine.lee@innovation.nj.gov>
2025-04-14 12:12:33 -0400

    feat: [AB#13315] Tax Clearance Certificate Download
    This commit adds downloading the tax clearance pdf that is returned from taxation via our API.
    
    It also
    - Wiremocks the success response of hitting the taxation tax clearance api
    - Extends `DocumentTile.tsx` to potentially take in a `downloadFilename` for blob file downlods
    - Adds a `.prettierignore` to ignore large files that don't allow comments in them
    
    This commit resolves #[13315](https://dev.azure.com/NJInnovation/Business%20First%20Stop/_workitems/edit/13315).

--- shared/src/intHelpers.ts
+++ shared/src/intHelpers.ts
@@ -7,3 +7,11 @@ export const randomInt = (length = 8): number => {
 export const randomIntFromInterval = (min: string, max: string): number => {
   return Math.floor(Math.random() * (Number(max) - Number(min) + 1) + Number(min));
 };
+
+export const convertSignedByteArrayToUnsigned = (signedByteArray: number[]): number[] =>
+  signedByteArray.map((signedByte) => {
+    if (signedByte < -128 || signedByte > 127) {
+      throw new Error(`"Invalid signedByte ${signedByte}, expected -128 <= signedByte <= 127"`);
+    }
+    return signedByte & 0xff;
+  });
