c20e5158b65915c801f66c3495deaabe99f1a970
Soma Sai Kumar Badri <somasai.badri@gmail.com>
2025-04-30 15:02:46 -0700

    chore: [AB#7017] add additional address matching & expired busines logic

---
+++ api/src/client/dep/filterSquashByRegNumAndAddress.ts
@@ -0,0 +1,24 @@
+import type { XrayRegistrationEntry } from "@shared/xray";
+
+export const filterSquashByRegNumAndAddress = (
+  entries: XrayRegistrationEntry[],
+  addressLine1: string
+): XrayRegistrationEntry[] => {
+  const entriesRecord: Record<string, XrayRegistrationEntry> = {};
+
+  for (const entry of entries) {
+    const existingEntry = entriesRecord[entry.registrationNumber];
+    if (existingEntry) {
+      const squashedEntry: XrayRegistrationEntry = {
+        ...entry,
+        streetAddress:
+          existingEntry.streetAddress === addressLine1 ? existingEntry.streetAddress : entry.streetAddress,
+      };
+      entriesRecord[entry.registrationNumber] = squashedEntry;
+    } else {
+      entriesRecord[entry.registrationNumber] = entry;
+    }
+  }
+
+  return Object.values(entriesRecord);
+};
