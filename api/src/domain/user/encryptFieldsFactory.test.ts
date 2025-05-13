2233241e303534dfe31cf9c9502235dde3fb5fbc
Josephine Lee <josephine.lee@innovation.nj.gov>
2025-05-13 10:25:33 -0400

    feat: [AB#12793] Encrypt tax clearance on user save

--- api/src/domain/user/encryptFieldsFactory.test.ts
+++ api/src/domain/user/encryptFieldsFactory.test.ts
@@ -1,22 +1,89 @@
 import { EncryptionDecryptionClient, EncryptTaxId } from "@domain/types";
 import { encryptFieldsFactory } from "@domain/user/encryptFieldsFactory";
 import { modifyCurrentBusiness } from "@shared/domain-logic/modifyCurrentBusiness";
-import { generateBusiness, generateProfileData, generateUserDataForBusiness } from "@shared/test";
+import {
+  generateBusiness,
+  generateProfileData,
+  generateTaxClearanceCertificateData,
+  generateUserDataForBusiness,
+} from "@shared/test";
+import { UserData } from "@shared/userData";
 
 describe("encryptFieldsFactory", () => {
   let stubEncryptionDecryptionClient: jest.Mocked<EncryptionDecryptionClient>;
   let encryptTaxId: EncryptTaxId;
-  const numFieldsToEncrypt = 2;
-  const taxId = "123456789000";
-  const taxPin = "1234";
+  const numFieldsToEncrypt = 4;
+  const profileTaxId = "123456789000";
+  const profileTaxPin = "1234";
+  const taxClearanceTaxId = "111111111111";
+  const taxClearanceTaxPin = "1111";
+  const encryptedProfileTaxId = "encrypted-profile-tax-id";
+  const encryptedProfileTaxPin = "encrypted-profile-tax-pin";
+  const encryptedTaxClearanceTaxId = "encrypted-tax-clearance-tax-id";
+  const encryptedTaxClearanceTaxPin = "encrypted-tax-clearance-tax-pin";
+
+  const generateUnencryptedUserData = (
+    profileDataOverrides = {},
+    taxClearanceCertificateDataOverrides = {},
+  ): UserData => {
+    return generateUserDataForBusiness(
+      generateBusiness({
+        profileData: generateProfileData({
+          taxId: profileTaxId,
+          encryptedTaxId: undefined,
+          taxPin: profileTaxPin,
+          encryptedTaxPin: undefined,
+          ...profileDataOverrides,
+        }),
+        taxClearanceCertificateData: generateTaxClearanceCertificateData({
+          taxId: taxClearanceTaxId,
+          encryptedTaxId: undefined,
+          taxPin: taxClearanceTaxPin,
+          encryptedTaxPin: undefined,
+          ...taxClearanceCertificateDataOverrides,
+        }),
+      }),
+    );
+  };
+
+  const getExpectedEncryptedCurrentBusiness = (
+    userData: UserData,
+    profileDataOverrides = {},
+    taxClearanceCertificateDataOverrides = {},
+  ): UserData => {
+    const expectedUserData = modifyCurrentBusiness(userData, (business) => ({
+      ...business,
+      profileData: {
+        ...business.profileData,
+        taxId: "*******89000",
+        encryptedTaxId: encryptedProfileTaxId,
+        taxPin: "****",
+        encryptedTaxPin: encryptedProfileTaxPin,
+        ...profileDataOverrides,
+      },
+      taxClearanceCertificateData: business.taxClearanceCertificateData
+        ? {
+            ...business.taxClearanceCertificateData,
+            taxId: "*******11111",
+            encryptedTaxId: encryptedTaxClearanceTaxId,
+            taxPin: "****",
+            encryptedTaxPin: encryptedTaxClearanceTaxPin,
+            ...taxClearanceCertificateDataOverrides,
+          }
+        : undefined,
+    }));
+    return expectedUserData;
+  };
 
   beforeEach(async () => {
     jest.resetAllMocks();
     stubEncryptionDecryptionClient = {
       encryptValue: jest.fn((valueToBeEncrypted: string) => {
         const encryptedValues: { [key: string]: string } = {
-          "123456789000": "encrypted-tax-id",
-          "1234": "encrypted-tax-pin",
+          [profileTaxId]: encryptedProfileTaxId,
+          [profileTaxPin]: encryptedProfileTaxPin,
+          [taxClearanceTaxId]: encryptedTaxClearanceTaxId,
+          [taxClearanceTaxPin]: encryptedTaxClearanceTaxPin,
         };
         return Promise.resolve(encryptedValues[valueToBeEncrypted] ?? "unexpected value");
       }),
@@ -26,87 +93,115 @@ describe("encryptFieldsFactory", () => {
   });
 
   it("updates user by masking and encrypting tax id and tax pin", async () => {
-    const userData = generateUserDataForBusiness(
-      generateBusiness({
-        profileData: generateProfileData({
-          taxId: taxId,
-          encryptedTaxId: undefined,
-          taxPin: taxPin,
-          encryptedTaxPin: undefined,
-        }),
-      }),
-    );
+    const userData = generateUnencryptedUserData();
     const response = await encryptTaxId(userData);
-    expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledWith(taxId);
-    expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledWith(taxPin);
+    expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledWith(profileTaxId);
+    expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledWith(profileTaxPin);
+    expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledWith(taxClearanceTaxId);
+    expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledWith(taxClearanceTaxPin);
     expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledTimes(numFieldsToEncrypt);
 
-    const expectedUserData = modifyCurrentBusiness(userData, (business) => ({
-      ...business,
-      profileData: {
-        ...business.profileData,
-        taxId: "*******89000",
-        encryptedTaxId: "encrypted-tax-id",
-        taxPin: "****",
-        encryptedTaxPin: "encrypted-tax-pin",
-      },
-    }));
+    const expectedUserData = getExpectedEncryptedCurrentBusiness(userData);
     expect(response).toEqual(expectedUserData);
   });
 
-  it("does not encrypt tax id if it is already masked", async () => {
-    const userData = generateUserDataForBusiness(
-      generateBusiness({
-        profileData: generateProfileData({
-          taxId: "*******89000",
-          encryptedTaxId: "already-encrypted",
-          taxPin: taxPin,
-          encryptedTaxPin: undefined,
-        }),
-      }),
-    );
+  it("does not encrypt profile tax id if it is already masked", async () => {
+    const userData = generateUnencryptedUserData({
+      taxId: "*******89000",
+      encryptedTaxId: "already-encrypted",
+    });
     const response = await encryptTaxId(userData);
-    expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledWith(taxPin);
+    expect(stubEncryptionDecryptionClient.encryptValue).not.toHaveBeenCalledWith(profileTaxId);
+    expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledWith(profileTaxPin);
+    expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledWith(taxClearanceTaxId);
+    expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledWith(taxClearanceTaxPin);
     expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledTimes(
       numFieldsToEncrypt - 1,
     );
 
-    const expectedUserData = modifyCurrentBusiness(userData, (business) => ({
-      ...business,
-      profileData: {
-        ...business.profileData,
-        taxPin: "****",
-        encryptedTaxPin: "encrypted-tax-pin",
-      },
-    }));
+    const expectedUserData = getExpectedEncryptedCurrentBusiness(userData, {
+      taxId: "*******89000",
+      encryptedTaxId: "already-encrypted",
+    });
     expect(response).toEqual(expectedUserData);
   });
 
-  it("does not encrypt tax pin if it is already masked", async () => {
-    const userData = generateUserDataForBusiness(
-      generateBusiness({
-        profileData: generateProfileData({
-          taxId: taxId,
-          encryptedTaxId: undefined,
-          taxPin: "****",
-          encryptedTaxPin: "already-encrypted",
-        }),
-      }),
-    );
+  it("does not encrypt profile tax pin if it is already masked", async () => {
+    const userData = generateUnencryptedUserData({
+      taxPin: "****",
+      encryptedTaxPin: "already-encrypted",
+    });
     const response = await encryptTaxId(userData);
-    expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledWith(taxId);
+    expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledWith(profileTaxId);
+    expect(stubEncryptionDecryptionClient.encryptValue).not.toHaveBeenCalledWith(profileTaxPin);
+    expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledWith(taxClearanceTaxId);
+    expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledWith(taxClearanceTaxPin);
     expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledTimes(
       numFieldsToEncrypt - 1,
     );
 
-    const expectedUserData = modifyCurrentBusiness(userData, (business) => ({
-      ...business,
-      profileData: {
-        ...business.profileData,
+    const expectedUserData = getExpectedEncryptedCurrentBusiness(userData, {
+      taxPin: "****",
+      encryptedTaxPin: "already-encrypted",
+    });
+    expect(response).toEqual(expectedUserData);
+  });
+
+  it("does not encrypt tax clearance tax id if it is already masked", async () => {
+    const userData = generateUnencryptedUserData(
+      {},
+      {
         taxId: "*******89000",
-        encryptedTaxId: "encrypted-tax-id",
+        encryptedTaxId: "already-encrypted",
       },
-    }));
+    );
+    const response = await encryptTaxId(userData);
+    expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledWith(profileTaxId);
+    expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledWith(profileTaxPin);
+    expect(stubEncryptionDecryptionClient.encryptValue).not.toHaveBeenCalledWith(taxClearanceTaxId);
+    expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledWith(taxClearanceTaxPin);
+    expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledTimes(
+      numFieldsToEncrypt - 1,
+    );
+
+    const expectedUserData = getExpectedEncryptedCurrentBusiness(
+      userData,
+      {},
+      {
+        taxId: "*******89000",
+        encryptedTaxId: "already-encrypted",
+      },
+    );
+    expect(response).toEqual(expectedUserData);
+  });
+
+  it("does not encrypt tax clearance tax pin if it is already masked", async () => {
+    const userData = generateUnencryptedUserData(
+      {},
+      {
+        taxPin: "****",
+        encryptedTaxPin: "already-encrypted",
+      },
+    );
+    const response = await encryptTaxId(userData);
+    expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledWith(profileTaxId);
+    expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledWith(profileTaxPin);
+    expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledWith(taxClearanceTaxId);
+    expect(stubEncryptionDecryptionClient.encryptValue).not.toHaveBeenCalledWith(
+      taxClearanceTaxPin,
+    );
+    expect(stubEncryptionDecryptionClient.encryptValue).toHaveBeenCalledTimes(
+      numFieldsToEncrypt - 1,
+    );
+
+    const expectedUserData = getExpectedEncryptedCurrentBusiness(
+      userData,
+      {},
+      {
+        taxPin: "****",
+        encryptedTaxPin: "already-encrypted",
+      },
+    );
     expect(response).toEqual(expectedUserData);
   });
 });
