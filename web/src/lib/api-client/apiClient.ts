3e4fc910ce758f43e0b123de85f1ea3e382348f2
sirsamueljoseph <samueljberin@gmail.com>
2025-05-16 11:17:52 -0600

    fix: [AB#7863] Allow requests to emergency trip permit api without auth

--- web/src/lib/api-client/apiClient.ts
+++ web/src/lib/api-client/apiClient.ts
@@ -136,7 +136,7 @@ export const searchBusinessName = (name: string): Promise<NameAvailability> => {
 export const postEmergencyTripPermitApplication = (
   applicationInfo: EmergencyTripPermitApplicationInfo,
 ): Promise<EmergencyTripPermitSubmitResponse> => {
-  return post("/emergencyTripPermit", applicationInfo);
+  return post("/emergencyTripPermit", applicationInfo, false);
 };
 
 export const postSelfReg = (userData: UserData): Promise<SelfRegResponse> => {
