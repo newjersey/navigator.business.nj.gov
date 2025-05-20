9eff3fb8bb5792c23c40aa623dd57e73a72939ab
Josephine Lee <josephine.lee@innovation.nj.gov>
2025-05-20 15:36:38 -0400

    test

--- web/test/pages/profile/profile-starting.test.tsx
+++ web/test/pages/profile/profile-starting.test.tsx
@@ -1102,22 +1102,25 @@ describe("profile - starting business", () => {
   });
 
   it("shows the home-based question when user changes to applicable industry, even before saving", () => {
+    const industry = randomNonHomeBasedIndustry();
     renderPage({
       business: generateBusinessForProfile({
         profileData: generateProfileData({
-          industryId: randomNonHomeBasedIndustry(),
+          industryId: industry,
           businessPersona: "STARTING",
         }),
       }),
     });
+    console.log("industry", industry);
     chooseTab("permits");
-    expect(screen.queryByLabelText("Home based business")).not.toBeInTheDocument();
+    expect(false).toBeTruthy();
+    // expect(screen.queryByLabelText("Home based business")).not.toBeInTheDocument();
 
-    chooseTab("info");
-    selectByValue("Industry", randomHomeBasedIndustry());
+    // chooseTab("info");
+    // selectByValue("Industry", randomHomeBasedIndustry());
 
-    chooseTab("permits");
-    expect(screen.getByLabelText("Home based business")).toBeInTheDocument();
+    // chooseTab("permits");
+    // expect(screen.getByLabelText("Home based business")).toBeInTheDocument();
   });
 
   it("does not show the home-based question if not applicable to industry", () => {
