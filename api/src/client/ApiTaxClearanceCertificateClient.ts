a62e137c89a80ac2e1fe805d3cf550598acc35f9
ZacharyRizer <zacharyrizer@gmail.com>
2025-05-23 16:03:38 -0400

    fix: [AB#7751] pr comments

--- api/src/client/ApiTaxClearanceCertificateClient.ts
+++ api/src/client/ApiTaxClearanceCertificateClient.ts
@@ -22,7 +22,6 @@ export const FAILED_TAX_ID_AND_PIN_VALIDATION =
 export const MISSING_FIELD =
   "Mandatory Field Missing. TaxpayerId, TaxpayerName, AddressLine1, City, State, Zip, Agency name, Rep Id and RepName are required.";
 export const NATURAL_PROGRAM_ERROR = "Error calling Natural Program. Please try again later.";
-export const GENERIC_ERROR = "Something went wrong. Try again later.";
 
 export const ApiTaxClearanceCertificateClient = (
   logWriter: LogWriterType,
@@ -140,14 +139,6 @@ export const ApiTaxClearanceCertificateClient = (
               },
             };
           }
-        }
-        if (response?.status === StatusCodes.INTERNAL_SERVER_ERROR || !response) {
-          return {
-            error: {
-              type: "GENERIC_ERROR",
-              message: GENERIC_ERROR,
-            },
-          };
         }
         logWriter.LogError(
           `Tax Clearance Certificate Client - Id:${logId} - Unexpected Error Response`,
