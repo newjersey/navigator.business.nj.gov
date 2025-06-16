26a4324966dfaec99623001547bde347f7c2f185
sirsamueljoseph <samueljberin@gmail.com>
2025-06-16 08:37:07 -0600

    feat: [AB#14385] Add location based information to profile accordion

--- web/src/components/profile/PersonalizeYourTasksTab.tsx
+++ web/src/components/profile/PersonalizeYourTasksTab.tsx
@@ -1,19 +1,29 @@
 import { Content } from "@/components/Content";
+import { ElevatorOwningBusiness } from "@/components/data-fields/ElevatorOwningBusiness";
+import { HomeBasedBusiness } from "@/components/data-fields/HomeBasedBusiness";
 import { NonEssentialQuestionsSection } from "@/components/data-fields/non-essential-questions/NonEssentialQuestionsSection";
+import { RenovationQuestion } from "@/components/data-fields/RenovationQuestion";
 import { Heading } from "@/components/njwds-extended/Heading";
 import { Icon } from "@/components/njwds/Icon";
+import {
+  displayElevatorQuestion,
+  displayHomedBaseBusinessQuestion,
+  displayPlannedRenovationQuestion,
+} from "@/components/profile/profileDisplayLogicHelpers";
+import { ProfileField } from "@/components/profile/ProfileField";
 import { ProfileTabHeader } from "@/components/profile/ProfileTabHeader";
 import { ProfileDataContext } from "@/contexts/profileDataContext";
 import { useConfig } from "@/lib/data-hooks/useConfig";
+import { useUserData } from "@/lib/data-hooks/useUserData";
 import { hasNonEssentialQuestions } from "@/lib/utils/non-essential-questions-helpers";
 import { Accordion, AccordionDetails, AccordionSummary, Avatar } from "@mui/material";
 import { ReactElement, useContext } from "react";
 
 export const PersonalizeYourTasksTab = (): ReactElement => {
   const { Config } = useConfig();
-
-  const { state } = useContext(ProfileDataContext);
-
+  const { business } = useUserData();
+  const { state: profileDataState } = useContext(ProfileDataContext);
+  const profileData = profileDataState.profileData;
   const AccordionHeader = (props: {
     icon: string;
     headerText: string;
@@ -74,7 +84,52 @@ export const PersonalizeYourTasksTab = (): ReactElement => {
           headerText={Config.profileDefaults.default.locationBasedRequirementsHeader}
           description={Config.profileDefaults.default.locationBasedRequirementsSubText}
         />
-        <AccordionDetails sx={{ marginLeft: 6 }}>LOCATION BASED DETAILS</AccordionDetails>
+        <AccordionDetails sx={{ marginLeft: 6 }}>
+          {displayHomedBaseBusinessQuestion(profileData, business) && (
+            <div className={"padding-top-1"}>
+              <ProfileField
+                fieldName="homeBasedBusiness"
+                hideLine
+                fullWidth
+                hideHeader
+                boldAltDescription
+                boldDescription
+              >
+                <HomeBasedBusiness />
+              </ProfileField>
+            </div>
+          )}
+
+          {displayPlannedRenovationQuestion(profileData, business) && (
+            <div className={"padding-top-1"}>
+              <ProfileField
+                fieldName="plannedRenovationQuestion"
+                hideLine
+                fullWidth
+                hideHeader
+                displayAltDescription
+                boldAltDescription
+              >
+                <RenovationQuestion />
+              </ProfileField>
+            </div>
+          )}
+
+          {displayElevatorQuestion(profileData, business) && (
+            <div className={"padding-top-1"}>
+              <ProfileField
+                fieldName="elevatorOwningBusiness"
+                hideLine
+                fullWidth
+                hideHeader
+                boldAltDescription
+                boldDescription
+              >
+                <ElevatorOwningBusiness />
+              </ProfileField>
+            </div>
+          )}
+        </AccordionDetails>
       </Accordion>
       <hr className="margin-y-4" />
       <Accordion data-testid="nonEssentialQuestionsAccordianSection">
@@ -82,7 +137,7 @@ export const PersonalizeYourTasksTab = (): ReactElement => {
           icon={"search"}
           headerText={Config.profileDefaults.fields.nonEssentialQuestions.default.sectionHeader}
           description={Config.profileDefaults.fields.nonEssentialQuestions.default.sectionSubText}
-          hideExpandIcon={!hasNonEssentialQuestions(state.profileData)}
+          hideExpandIcon={!hasNonEssentialQuestions(profileData)}
         />
         <AccordionDetails sx={{ marginLeft: 6 }}>
           <NonEssentialQuestionsSection />
---
+++ web/src/components/profile/profileDisplayLogicHelpers.test.ts
@@ -0,0 +1,115 @@
+import {
+  displayHomedBaseBusinessQuestion,
+  displayPlannedRenovationQuestion,
+} from "@/components/profile/profileDisplayLogicHelpers";
+import { NexusBusinessTypeIds } from "@businessnjgovnavigator/shared/domain-logic/businessPersonaHelpers";
+import { getIndustries } from "@businessnjgovnavigator/shared/industry";
+import { createEmptyProfileData } from "@businessnjgovnavigator/shared/profileData";
+import {
+  generateBusiness,
+  generateProfileData,
+} from "@businessnjgovnavigator/shared/test/factories";
+import { createEmptyBusiness } from "@businessnjgovnavigator/shared/userData";
+
+describe("profileDisplayLogicHelpers", () => {
+  describe("home based business question", () => {
+    it("returns false if no business is provided", () => {
+      expect(displayHomedBaseBusinessQuestion(createEmptyProfileData())).toBe(false);
+    });
+
+    it("returns true if the profile data doesn't have an industry", () => {
+      const business = createEmptyBusiness({ userId: "user-id" });
+      expect(displayHomedBaseBusinessQuestion(business.profileData, business)).toBe(true);
+    });
+
+    it("returns false if the business is nexus and has location in NJ", () => {
+      const business = generateBusiness({
+        profileData: generateProfileData({
+          businessPersona: "FOREIGN",
+          foreignBusinessTypeIds: NexusBusinessTypeIds,
+        }),
+      });
+      expect(displayHomedBaseBusinessQuestion(business.profileData, business)).toBe(false);
+    });
+
+    it("returns true if the business is in an industry that accepts the home based question", () => {
+      const homeBasedBusinessIndustries = getIndustries().filter((industry) => {
+        return industry.industryOnboardingQuestions.canBeHomeBased === true;
+      });
+      const business = generateBusiness({
+        profileData: generateProfileData({
+          businessPersona: "STARTING",
+          industryId:
+            homeBasedBusinessIndustries[
+              Math.floor(Math.random() * homeBasedBusinessIndustries.length)
+            ].id,
+        }),
+      });
+      expect(displayHomedBaseBusinessQuestion(business.profileData, business)).toBe(true);
+    });
+
+    it("returns false if the business is in an industry that can not be home based", () => {
+      const homeBasedBusinessIndustries = getIndustries().filter((industry) => {
+        return industry.industryOnboardingQuestions.canBeHomeBased === false;
+      });
+      const business = generateBusiness({
+        profileData: generateProfileData({
+          businessPersona: "STARTING",
+          industryId:
+            homeBasedBusinessIndustries[
+              Math.floor(Math.random() * homeBasedBusinessIndustries.length)
+            ].id,
+        }),
+      });
+      expect(displayHomedBaseBusinessQuestion(business.profileData, business)).toBe(false);
+    });
+  });
+
+  describe("planned renovation question", () => {
+    it("returns false if business is home based", () => {
+      const business = generateBusiness({
+        profileData: generateProfileData({
+          homeBasedBusiness: true,
+        }),
+      });
+      expect(displayPlannedRenovationQuestion(business.profileData, business)).toBe(false);
+    });
+
+    it("returns true for non home based nexus businesses", () => {
+      const business = generateBusiness({
+        profileData: generateProfileData({
+          homeBasedBusiness: false,
+          businessPersona: "FOREIGN",
+          foreignBusinessTypeIds: NexusBusinessTypeIds,
+        }),
+      });
+      expect(displayPlannedRenovationQuestion(business.profileData, business)).toBe(true);
+    });
+
+    it("returns true for non home based starting businesses", () => {
+      const business = generateBusiness({
+        profileData: generateProfileData({
+          homeBasedBusiness: false,
+          businessPersona: "STARTING",
+        }),
+      });
+      expect(displayPlannedRenovationQuestion(business.profileData, business)).toBe(true);
+    });
+  });
+
+  describe("elevator question", () => {
+    it("returns false if business is not provided", () => {
+      expect(displayPlannedRenovationQuestion(generateProfileData({}))).toBe(false);
+    });
+
+    it("returns true for non home based starting businesses", () => {
+      const business = generateBusiness({
+        profileData: generateProfileData({
+          homeBasedBusiness: false,
+          businessPersona: "STARTING",
+        }),
+      });
+      expect(displayPlannedRenovationQuestion(business.profileData, business)).toBe(true);
+    });
+  });
+});
