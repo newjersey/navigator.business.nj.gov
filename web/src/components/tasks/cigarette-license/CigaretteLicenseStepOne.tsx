65c38ddd7f5810062f432ce6ee2a7a634eef73c6
ashlsun <janniesun00@gmail.com>
2025-07-02 16:59:27 -0400

    feat: [AB#14929] step 1 of cigarette license

---
+++ web/src/components/tasks/cigarette-license/CigaretteLicenseStepOne.tsx
@@ -0,0 +1,42 @@
+import { Content } from "@/components/Content";
+import { HorizontalLine } from "@/components/HorizontalLine";
+import { Alert } from "@/components/njwds-extended/Alert";
+import { CtaContainer } from "@/components/njwds-extended/cta/CtaContainer";
+import { LiveChatHelpButton } from "@/components/njwds-extended/LiveChatHelpButton";
+import { PrimaryButton } from "@/components/njwds-extended/PrimaryButton";
+import { ActionBarLayout } from "@/components/njwds-layout/ActionBarLayout";
+import { useConfig } from "@/lib/data-hooks/useConfig";
+import { ReactElement } from "react";
+
+interface Props {
+  setStepIndex: (step: number) => void;
+}
+
+export const CigaretteLicenseStepOne = (props: Props): ReactElement => {
+  const { Config } = useConfig();
+
+  return (
+    <>
+      <Alert variant={"info"}>
+        <Content>{Config.cigaretteLicenseStep1.alert}</Content>
+      </Alert>
+      <Content>{Config.cigaretteLicenseStep1.content}</Content>
+      <HorizontalLine />
+      <span className="h5-styling">{Config.cigaretteLicenseShared.issuingAgencyLabelText}: </span>
+      <span className="h6-styling">{Config.cigaretteLicenseShared.issuingAgencyText}</span>
+      <CtaContainer>
+        <ActionBarLayout>
+          <LiveChatHelpButton />
+          <PrimaryButton
+            isColor="primary"
+            onClick={() => props.setStepIndex(1)}
+            dataTestId="cta-primary-1"
+            isRightMarginRemoved={true}
+          >
+            {Config.cigaretteLicenseStep1.continueButtonText}
+          </PrimaryButton>
+        </ActionBarLayout>
+      </CtaContainer>
+    </>
+  );
+};
