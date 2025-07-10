6411731a95f2946d3a899211b93475a1e46815d6
adidner <adidner@gmail.com>
2025-07-10 16:18:56 -0400

    feat: [AB#7340] search for usage(s) of tasks

---
+++ web/src/lib/search/usageHelpers.test.ts
@@ -0,0 +1,222 @@
+import { IndustryRoadmap } from "@/lib/roadmap/roadmapBuilder";
+import { Match } from "@/lib/search/typesForSearch";
+import {
+  AddAddOnUsage,
+  AddIndustryUsage,
+  AddTaskDependencyUsage,
+  composeAddOnLicenseTaskString,
+  composeAddOnReplaceWithFileNameString,
+  composeAddOnTaskString,
+  composeAddOnTaskToReplaceFilenameString,
+  composeHasLicenseTaskDependenciesString,
+  composeHasTaskDependenciesString,
+  composeIndstryLicenseTaskString,
+  composeIndstryTaskString,
+  composeIsLicenseTaskDependencyString,
+  composeIsTaskDependencyTaskString,
+} from "@/lib/search/usageHelpers";
+
+jest.mock("@businessnjgovnavigator/content/roadmaps/task-dependencies.json", () => ({
+  dependencies: [
+    {
+      task: "task-name",
+      taskDependencies: [""],
+    },
+    {
+      licenseTask: "license-task-name",
+      taskDependencies: [""],
+    },
+    {
+      task: "task-with-dependencies",
+      taskDependencies: ["task-dependency"],
+    },
+    {
+      licenseTask: "license-task-with-dependencies",
+      licenseTaskDependencies: ["license-task-dependency"],
+    },
+  ],
+}));
+
+describe("usageHelpers", () => {
+  const createMatches = (filename: string): Match[] => {
+    return [
+      {
+        filename: filename,
+        cmsCollectionName: "",
+        displayTitle: "displayTitle",
+        snippets: ["", ""],
+        additionalUsageLocations: { taskDependencies: [], industries: [], addOns: [] },
+      },
+    ];
+  };
+
+  describe("taskDependency", () => {
+    it("task match", () => {
+      const matches = createMatches("task-name");
+      AddTaskDependencyUsage(matches);
+      expect(matches[0].additionalUsageLocations?.taskDependencies?.[0]).toStrictEqual({
+        description: composeHasTaskDependenciesString(),
+        link: "/mgmt/cms#/collections/settings/entries/task-dependencies",
+      });
+    });
+
+    it("license task match", () => {
+      const matches = createMatches("license-task-name");
+      AddTaskDependencyUsage(matches);
+      expect(matches[0].additionalUsageLocations?.taskDependencies?.[0]).toStrictEqual({
+        description: composeHasLicenseTaskDependenciesString(),
+        link: "/mgmt/cms#/collections/settings/entries/task-dependencies",
+      });
+    });
+
+    it("task dependency match", () => {
+      const matches = createMatches("task-dependency");
+      AddTaskDependencyUsage(matches);
+      expect(matches[0].additionalUsageLocations?.taskDependencies?.[0]).toStrictEqual({
+        description: composeIsTaskDependencyTaskString("task-with-dependencies"),
+        link: "/mgmt/cms#/collections/settings/entries/task-dependencies",
+      });
+    });
+
+    it("license task dependency match", () => {
+      const matches = createMatches("license-task-dependency");
+      AddTaskDependencyUsage(matches);
+      expect(matches[0].additionalUsageLocations?.taskDependencies?.[0]).toStrictEqual({
+        description: composeIsLicenseTaskDependencyString("license-task-with-dependencies"),
+        link: "/mgmt/cms#/collections/settings/entries/task-dependencies",
+      });
+    });
+  });
+
+  describe("industry", () => {
+    const industry = {
+      id: "id",
+      name: "name",
+      description: "",
+      canHavePermanentLocation: true,
+      roadmapSteps: [
+        {
+          step: 1,
+          weight: 1,
+          task: "task-match",
+          licenseTask: "",
+          required: false,
+        },
+        {
+          step: 1,
+          weight: 1,
+          task: "",
+          licenseTask: "license-task-match",
+          required: false,
+        },
+      ],
+      nonEssentialQuestionsIds: [],
+      naicsCodes: "",
+      isEnabled: false,
+      industryOnboardingQuestions: {
+        isProvidesStaffingServicesApplicable: undefined,
+        isCertifiedInteriorDesignerApplicable: undefined,
+        isRealEstateAppraisalManagementApplicable: undefined,
+        canBeReseller: undefined,
+        canBeHomeBased: undefined,
+        isLiquorLicenseApplicable: undefined,
+        isCpaRequiredApplicable: undefined,
+        isTransportation: undefined,
+        isCarServiceApplicable: undefined,
+        isInterstateLogisticsApplicable: undefined,
+        isInterstateMovingApplicable: undefined,
+        isChildcareForSixOrMore: undefined,
+        willSellPetCareItems: undefined,
+        isPetCareHousingApplicable: undefined,
+      },
+    };
+
+    it("industry task match", () => {
+      const matches = createMatches("task-match");
+
+      AddIndustryUsage(matches, [industry]);
+      expect(matches[0].additionalUsageLocations?.industries?.[0]).toStrictEqual({
+        description: composeIndstryTaskString("name"),
+        link: "/mgmt/cms#/collections/roadmaps/entries/id",
+      });
+    });
+
+    it("industry license task match", () => {
+      const matches = createMatches("license-task-match");
+
+      AddIndustryUsage(matches, [industry]);
+      expect(matches[0].additionalUsageLocations?.industries?.[0]).toStrictEqual({
+        description: composeIndstryLicenseTaskString("name"),
+        link: "/mgmt/cms#/collections/roadmaps/entries/id",
+      });
+    });
+  });
+
+  describe("addOns", () => {
+    const addOns: IndustryRoadmap[] = [
+      {
+        id: "id",
+        roadmapSteps: [
+          {
+            step: 1,
+            weight: 1,
+            task: "task-match",
+            licenseTask: "",
+            required: false,
+          },
+          {
+            step: 1,
+            weight: 1,
+            task: "",
+            licenseTask: "license-task-match",
+            required: false,
+          },
+        ],
+        modifications: [
+          { taskToReplaceFilename: "task-replace-match", replaceWithFilename: "" },
+          { taskToReplaceFilename: "", replaceWithFilename: "replace-with-match" },
+        ],
+      },
+    ];
+
+    it("task-match", () => {
+      const matches = createMatches("task-match");
+
+      AddAddOnUsage(matches, addOns);
+      expect(matches[0].additionalUsageLocations?.addOns?.[0]).toStrictEqual({
+        description: composeAddOnTaskString("id"),
+        link: "/mgmt/cms#/collections/addons/entries/id",
+      });
+    });
+
+    it("license-task-match", () => {
+      const matches = createMatches("license-task-match");
+
+      AddAddOnUsage(matches, addOns);
+      expect(matches[0].additionalUsageLocations?.addOns?.[0]).toStrictEqual({
+        description: composeAddOnLicenseTaskString("id"),
+        link: "/mgmt/cms#/collections/addons/entries/id",
+      });
+    });
+
+    it("task-replace-match", () => {
+      const matches = createMatches("task-replace-match");
+
+      AddAddOnUsage(matches, addOns);
+      expect(matches[0].additionalUsageLocations?.addOns?.[0]).toStrictEqual({
+        description: composeAddOnTaskToReplaceFilenameString("id"),
+        link: "/mgmt/cms#/collections/addons/entries/id",
+      });
+    });
+
+    it("replace-with-match", () => {
+      const matches = createMatches("replace-with-match");
+
+      AddAddOnUsage(matches, addOns);
+      expect(matches[0].additionalUsageLocations?.addOns?.[0]).toStrictEqual({
+        description: composeAddOnReplaceWithFileNameString("id"),
+        link: "/mgmt/cms#/collections/addons/entries/id",
+      });
+    });
+  });
+});
---
+++ web/src/lib/search/usageHelpers.ts
@@ -0,0 +1,174 @@
+import { IndustryRoadmap } from "@/lib/roadmap/roadmapBuilder";
+import { Match } from "@/lib/search/typesForSearch";
+import TaskDependencies from "@businessnjgovnavigator/content/roadmaps/task-dependencies.json";
+import { Industry } from "@businessnjgovnavigator/shared/industry";
+
+const addTaskDependency = (match: Match, dependency: string): void => {
+  if (!match.additionalUsageLocations) {
+    match.additionalUsageLocations = {};
+  }
+
+  if (!match.additionalUsageLocations.taskDependencies) {
+    match.additionalUsageLocations.taskDependencies = [];
+  }
+
+  match.additionalUsageLocations.taskDependencies.push({
+    description: dependency,
+    link: "/mgmt/cms#/collections/settings/entries/task-dependencies",
+  });
+};
+
+const addIndustryDependency = (match: Match, dependency: string, linkEnd: string): void => {
+  if (!match.additionalUsageLocations) {
+    match.additionalUsageLocations = {};
+  }
+
+  if (!match.additionalUsageLocations.industries) {
+    match.additionalUsageLocations.industries = [];
+  }
+
+  match.additionalUsageLocations.industries.push({
+    description: dependency,
+    link: `/mgmt/cms#/collections/roadmaps/entries/${linkEnd.toLowerCase()}`,
+  });
+};
+
+const addAddOnDependency = (match: Match, dependency: string, linkEnd: string): void => {
+  if (!match.additionalUsageLocations) {
+    match.additionalUsageLocations = {};
+  }
+
+  if (!match.additionalUsageLocations.addOns) {
+    match.additionalUsageLocations.addOns = [];
+  }
+
+  match.additionalUsageLocations.addOns.push({
+    description: dependency,
+    link: `/mgmt/cms#/collections/addons/entries/${linkEnd.toLowerCase()}`,
+  });
+};
+
+export const AddTaskDependencyUsage = (matches: Match[]): void => {
+  for (const [index, match] of matches.entries()) {
+    for (const dependency of TaskDependencies.dependencies) {
+      if (match.filename === dependency.task) {
+        const addedPhrase = composeHasTaskDependenciesString();
+
+        addTaskDependency(matches[index], addedPhrase);
+      }
+      if (match.filename === dependency.licenseTask) {
+        const addedPhrase = composeHasLicenseTaskDependenciesString();
+
+        addTaskDependency(matches[index], addedPhrase);
+      }
+      if (dependency.taskDependencies) {
+        for (const TaskDependnecy of dependency.taskDependencies) {
+          if (match.filename === TaskDependnecy) {
+            const addedPhrase = composeIsTaskDependencyTaskString(
+              dependency.task ?? dependency.licenseTask,
+            );
+            addTaskDependency(matches[index], addedPhrase);
+          }
+        }
+      }
+      if (dependency.licenseTaskDependencies) {
+        for (const LicenseTaskDependnecy of dependency.licenseTaskDependencies) {
+          if (match.filename === LicenseTaskDependnecy) {
+            const addedPhrase = composeIsLicenseTaskDependencyString(
+              dependency.task ?? dependency.licenseTask,
+            );
+            addTaskDependency(matches[index], addedPhrase);
+          }
+        }
+      }
+    }
+  }
+};
+
+export const composeHasTaskDependenciesString = (): string => {
+  return `this is a Task which has dependencies`;
+};
+
+export const composeHasLicenseTaskDependenciesString = (): string => {
+  return `this is a License Task which has dependencies`;
+};
+
+export const composeIsTaskDependencyTaskString = (task: string): string => {
+  return `this task is a Task dependency for the "${task}" task`;
+};
+
+export const composeIsLicenseTaskDependencyString = (licenseTask: string): string => {
+  return `this task is a License Task dependency for the "${licenseTask}" task`;
+};
+
+export const AddIndustryUsage = (matches: Match[], industries: Industry[]): void => {
+  for (const [index, match] of matches.entries()) {
+    for (const industry of industries) {
+      for (const roadMapStep of industry.roadmapSteps) {
+        if (match.filename === roadMapStep.task) {
+          const addedPhrase = composeIndstryTaskString(industry.name);
+
+          addIndustryDependency(matches[index], addedPhrase, industry.id);
+        }
+        if (match.filename === roadMapStep.licenseTask) {
+          const addedPhrase = composeIndstryLicenseTaskString(industry.name);
+
+          addIndustryDependency(matches[index], addedPhrase, industry.id);
+        }
+      }
+    }
+  }
+};
+
+export const composeIndstryTaskString = (name: string): string => {
+  return `this Task is used in the "${name}" industry roadmap`;
+};
+
+export const composeIndstryLicenseTaskString = (name: string): string => {
+  return `this License Task is used in the "${name}" industry roadmap`;
+};
+
+export const AddAddOnUsage = (matches: Match[], addOns: IndustryRoadmap[]): void => {
+  for (const [index, match] of matches.entries()) {
+    for (const addOn of addOns) {
+      for (const roadMapStep of addOn.roadmapSteps) {
+        if (match.filename === roadMapStep.task) {
+          const addedPhrase = composeAddOnTaskString(addOn.id);
+          addAddOnDependency(matches[index], addedPhrase, addOn.id);
+        }
+        if (match.filename === roadMapStep.licenseTask) {
+          const addedPhrase = composeAddOnLicenseTaskString(addOn.id);
+
+          addAddOnDependency(matches[index], addedPhrase, addOn.id);
+        }
+      }
+      if (addOn.modifications !== undefined) {
+        for (const roadMapStep of addOn.modifications) {
+          if (match.filename === roadMapStep.taskToReplaceFilename) {
+            const addedPhrase = composeAddOnTaskToReplaceFilenameString(addOn.id);
+
+            addAddOnDependency(matches[index], addedPhrase, addOn.id);
+          }
+          if (match.filename === roadMapStep.replaceWithFilename) {
+            const addedPhrase = composeAddOnReplaceWithFileNameString(addOn.id);
+
+            addAddOnDependency(matches[index], addedPhrase, addOn.id);
+          }
+        }
+      }
+    }
+  }
+};
+
+export const composeAddOnReplaceWithFileNameString = (id: string): string => {
+  return `this Task replaces another task "${id}" add-on modification section`;
+};
+export const composeAddOnTaskToReplaceFilenameString = (id: string): string => {
+  return `this Task is replaced with another task "${id}" add-on modification section`;
+};
+export const composeAddOnTaskString = (id: string): string => {
+  return `this Task is used in the "${id}" add-on roadmap`;
+};
+export const composeAddOnLicenseTaskString = (id: string): string => {
+  return `this license task is used in the "${id}" add-on roadmap`;
+};
