d14f3143fd42b4fe69d04b52dc7fdd99f576add1
abim91 <53064345+abim91@users.noreply.github.com>
2025-07-11 13:44:23 -0400

    fix: [AB#15014] Resolve Untracked Sections in CMS

---
+++ web/src/lib/search/searchBusinessFormation.ts
@@ -0,0 +1,38 @@
+import { findMatchInLabelledText } from "@/lib/search/helpers";
+import { Match } from "@/lib/search/typesForSearch";
+import { TaskWithoutLinks } from "@/lib/types/types";
+
+export const searchBusinessFormation = (
+  tasks: TaskWithoutLinks[],
+  term: string,
+  cmsCollectionName: string,
+): Match[] => {
+  const matches: Match[] = [];
+
+  for (const task of tasks) {
+    let match: Match = {
+      filename: task.id,
+      snippets: [],
+      cmsCollectionName: cmsCollectionName,
+    };
+
+    const name = task.name.toLowerCase();
+    const description = task.summaryDescriptionMd.toLowerCase();
+    const contentMd = task.contentMd?.toLowerCase() ?? "";
+    const callToActionText = task.callToActionText?.toLowerCase() ?? "";
+    const labelledTexts = [
+      { content: name, label: "Name" },
+      { content: description, label: "Summary" },
+      { content: contentMd, label: "Content" },
+      { content: callToActionText, label: "CTA Text" },
+    ];
+
+    match = findMatchInLabelledText(labelledTexts, term, match);
+
+    if (match.snippets.length > 0) {
+      matches.push(match);
+    }
+  }
+
+  return matches;
+};
