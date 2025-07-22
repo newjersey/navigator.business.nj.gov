5dd86110e81e653f148478faa856231f27d58184
adidner <adidner@gmail.com>
2025-07-21 21:27:38 -0400

    feat: [AB#13035] Preview Improvements, NJEDA and Email Login Check

---
+++ web/src/lib/cms/previews/EmailLoginCheckPreview.tsx
@@ -0,0 +1,21 @@
+import { LoginEmailCheck } from "@/components/LoginEmailCheck";
+import { ConfigContext } from "@/contexts/configContext";
+import { PreviewProps } from "@/lib/cms/helpers/previewHelpers";
+import { usePreviewConfig } from "@/lib/cms/helpers/usePreviewConfig";
+import { usePreviewRef } from "@/lib/cms/helpers/usePreviewRef";
+import { ReactElement } from "react";
+
+const LoginEmailCheckPreview = (props: PreviewProps): ReactElement => {
+  const ref = usePreviewRef(props);
+  const { config, setConfig } = usePreviewConfig(props);
+
+  return (
+    <ConfigContext.Provider value={{ config, setOverrides: setConfig }}>
+      <div className="cms" ref={ref} style={{ margin: 40, pointerEvents: "none" }}>
+        <LoginEmailCheck />
+      </div>
+    </ConfigContext.Provider>
+  );
+};
+
+export default LoginEmailCheckPreview;
