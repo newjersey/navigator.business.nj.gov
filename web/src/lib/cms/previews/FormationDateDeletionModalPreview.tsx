125000e27f7d0a521249bf2a08c3afccc48a010e
adidner <adidner@gmail.com>
2025-07-21 21:33:30 -0400

    feat: [AB#13035] Preview Improvements, Formation Date Deletion Modal

---
+++ web/src/lib/cms/previews/FormationDateDeletionModalPreview.tsx
@@ -0,0 +1,28 @@
+import { FormationDateDeletionModal } from "@/components/FormationDateDeletionModal";
+import { ConfigContext } from "@/contexts/configContext";
+import { PreviewProps } from "@/lib/cms/helpers/previewHelpers";
+import { usePreviewConfig } from "@/lib/cms/helpers/usePreviewConfig";
+import { usePreviewRef } from "@/lib/cms/helpers/usePreviewRef";
+import { ReactElement, useState } from "react";
+
+const FormationDateDeletionModalPreview = (props: PreviewProps): ReactElement => {
+  const { config, setConfig } = usePreviewConfig(props);
+  const ref = usePreviewRef(props);
+
+  const [modalOpen, setModalOpen] = useState(false);
+
+  return (
+    <ConfigContext.Provider value={{ config, setOverrides: setConfig }}>
+      <button onClick={() => setModalOpen(true)}>toggle modal</button>
+      <div className="cms" ref={ref} style={{ margin: 40, pointerEvents: "none" }}>
+        <FormationDateDeletionModal
+          isOpen={modalOpen}
+          handleCancel={() => setModalOpen(false)}
+          handleDelete={() => setModalOpen(false)}
+        />
+      </div>
+    </ConfigContext.Provider>
+  );
+};
+
+export default FormationDateDeletionModalPreview;
