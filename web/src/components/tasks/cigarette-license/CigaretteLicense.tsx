a008f6f244b53341d0d60c82a13b6143f6092e7d
ZacharyRizer <zacharyrizer@gmail.com>
2025-07-23 12:08:09 -0400

    wip [skip ci]

--- web/src/components/tasks/cigarette-license/CigaretteLicense.tsx
+++ web/src/components/tasks/cigarette-license/CigaretteLicense.tsx
@@ -2,12 +2,13 @@ import { HorizontalStepper } from "@/components/njwds-extended/HorizontalStepper
 import { TaskHeader } from "@/components/TaskHeader";
 import { GeneralInfo } from "@/components/tasks/cigarette-license/GeneralInfo";
 import { LicenseeInfo } from "@/components/tasks/cigarette-license/LicenseeInfo";
-import { LicenseeInfo2 } from "@/components/tasks/cigarette-license/LicenseeInfo2";
 import { AddressContext } from "@/contexts/addressContext";
 import { CigaretteLicenseContext } from "@/contexts/cigaretteLicenseContext";
+import { getInitialData } from "@/components/tasks/cigarette-license/helpers";
 import {
   createDataFormErrorMap,
   DataFormErrorMapContext,
+  pickData,
 } from "@/contexts/dataFormErrorMapContext";
 import { ProfileDataContext } from "@/contexts/profileDataContext";
 import { useConfig } from "@/lib/data-hooks/useConfig";
@@ -39,6 +40,8 @@ export const CigaretteLicense = (props: Props): ReactElement => {
   const [profileData, setProfileData] = useState<ProfileData>(emptyProfileData);
   const [formationAddressData, setAddressData] =
     useState<FormationAddress>(emptyFormationAddressData);
+  const [cigaretteLicenseData, setCigaretteLicenseData] =
+    useState<CigaretteLicenseData>(emptyCigaretteLicenseData);
 
   const setAddress: Dispatch<SetStateAction<FormationAddress>> = (action) => {
     setAddressData((prevAddress) => {
@@ -47,26 +50,22 @@ export const CigaretteLicense = (props: Props): ReactElement => {
           ? (action as (prevState: FormationAddress) => FormationAddress)(prevAddress)
           : action;
 
-      // const relevantFields = pickData(newAddress, [
-      //   "addressLine1",
-      //   "addressLine2",
-      //   "addressCity",
-      //   "addressState",
-      //   "addressZipCode",
-      // ]);
-      // setTaxClearanceCertificateData({
-      //   ...taxClearanceCertificateData,
-      //   ...relevantFields,
-      // });
+      const relevantFields = pickData(newAddress, [
+        "addressLine1",
+        "addressLine2",
+        "addressCity",
+        "addressState",
+        "addressZipCode",
+      ]);
+      setCigaretteLicenseData({
+        ...cigaretteLicenseData,
+        ...relevantFields,
+      });
 
       return newAddress;
     });
   };
-  // const { business, userData } = useUserData();
-  const { business } = useUserData();
-
-  const [cigaretteLicenseData, setCigaretteLicenseData] =
-    useState<CigaretteLicenseData>(emptyCigaretteLicenseData);
+  const { business, userData } = useUserData();
 
   const stepperSteps: StepperStep[] = [
     {
@@ -98,30 +97,88 @@ export const CigaretteLicense = (props: Props): ReactElement => {
           ? (action as (prevState: ProfileData) => ProfileData)(prevProfileData)
           : action;
 
+      setCigaretteLicenseData({
+        ...cigaretteLicenseData,
+        businessName: profileData.businessName,
+        responsibleOwnerName: profileData.responsibleOwnerName,
+        tradeName: profileData.tradeName,
+        taxId: profileData.taxId || "",
+        encryptedTaxId: profileData.taxId || "",
+      });
+
       return profileData;
     });
   };
 
   useMountEffectWhenDefined(() => {
-    if (business) {
-      setProfileData({
-        ...profileData,
-        businessName: business.profileData.businessName,
-        taxId: business.profileData.taxId,
-        encryptedTaxId: business.profileData.encryptedTaxId,
+    if (business && userData) {
+      const {
+        businessName,
+        responsibleOwnerName,
+        tradeName,
+        taxId,
+        encryptedTaxId,
+        addressLine1,
+        addressLine2,
+        addressCity,
+        addressState,
+        addressZipCode,
+        mailingAddressIsTheSame,
+        mailingAddressLine1,
+        mailingAddressLine2,
+        mailingAddressCity,
+        mailingAddressState,
+        mailingAddressZipCode,
+        contactName,
+        contactPhoneNumber,
+        contactEmail,
+        salesInfoStartDate,
+        salesInfoSupplier,
+        lastUpdatedISO,
+      } = getInitialData(userData, business);
+
+      setCigaretteLicenseData({
+        businessName,
+        responsibleOwnerName,
+        tradeName,
+        taxId,
+        encryptedTaxId,
+        addressLine1,
+        addressLine2,
+        addressCity,
+        addressState,
+        addressZipCode,
+        mailingAddressIsTheSame,
+        mailingAddressLine1,
+        mailingAddressLine2,
+        mailingAddressCity,
+        mailingAddressState,
+        mailingAddressZipCode,
+        contactName,
+        contactPhoneNumber,
+        contactEmail,
+        salesInfoStartDate,
+        salesInfoSupplier,
+        lastUpdatedISO,
       });
 
-      // Initialize formationAddressData with business address data
-      // setAddressDataWithSync({
-      //   addressLine1: business.formationData?.formationFormData?.addressLine1 || "",
-      //   addressLine2: business.formationData?.formationFormData?.addressLine2 || "",
-      //   addressCity: business.formationData?.formationFormData?.addressMunicipality?.name || "",
-      //   addressState: business.formationData?.formationFormData?.addressState || undefined,
-      //   addressZipCode: business.formationData?.formationFormData?.addressZipCode || "",
-      //   addressCountry: business.formationData?.formationFormData?.addressCountry || undefined,
-      //   businessLocationType:
-      //     business.formationData?.formationFormData?.businessLocationType || undefined,
-      // });
+      setProfileData({
+        ...profileData,
+        businessName,
+        responsibleOwnerName,
+        tradeName,
+        taxId,
+        encryptedTaxId,
+      });
+
+      setAddressData({
+        ...formationAddressData,
+        addressLine1,
+        addressLine2,
+        addressCity,
+        addressState,
+        addressZipCode,
+      });
     }
   }, business);
 
@@ -136,38 +193,36 @@ export const CigaretteLicense = (props: Props): ReactElement => {
           setStepIndex(step);
         }}
       />
-      <CigaretteLicenseContext.Provider
-        value={{
-          state: cigaretteLicenseData,
-          setCigaretteLicenseData,
-        }}
-      >
-        <ProfileDataContext.Provider
+      <DataFormErrorMapContext.Provider value={formContextState}>
+        <CigaretteLicenseContext.Provider
           value={{
-            state: {
-              profileData: profileData,
-              flow: getFlow(profileData),
-            },
-            setProfileData: setProfile,
-            onBack: (): void => {},
+            state: cigaretteLicenseData,
+            setCigaretteLicenseData,
           }}
         >
-          <AddressContext.Provider
+          <ProfileDataContext.Provider
             value={{
               state: {
-                formationAddressData: formationAddressData,
+                profileData: profileData,
+                flow: getFlow(profileData),
               },
-              setAddressData: setAddress,
+              setProfileData: setProfile,
+              onBack: (): void => {},
             }}
           >
-            <DataFormErrorMapContext.Provider value={formContextState}>
+            <AddressContext.Provider
+              value={{
+                state: { formationAddressData },
+                setAddressData: setAddress,
+              }}
+            >
               {stepIndex === 0 && <GeneralInfo setStepIndex={setStepIndex} />}
               {stepIndex === 1 && <LicenseeInfo setStepIndex={setStepIndex} />}
-              {stepIndex === 2 && <LicenseeInfo2 setStepIndex={setStepIndex} />}
-            </DataFormErrorMapContext.Provider>
-          </AddressContext.Provider>
-        </ProfileDataContext.Provider>
-      </CigaretteLicenseContext.Provider>
+              {stepIndex === 2 && <></>}
+            </AddressContext.Provider>
+          </ProfileDataContext.Provider>
+        </CigaretteLicenseContext.Provider>
+      </DataFormErrorMapContext.Provider>
     </>
   );
 };
---
+++ web/src/components/tasks/cigarette-license/fields/MailingAddress.tsx
@@ -0,0 +1,244 @@
+import { GenericTextField } from "@/components/GenericTextField";
+import { Checkbox, FormControlLabel } from "@mui/material";
+import { StateDropdown } from "@/components/StateDropdown";
+import { WithErrorBar } from "@/components/WithErrorBar";
+import { CigaretteLicenseContext } from "@/contexts/cigaretteLicenseContext";
+import { ReactElement, useContext } from "react";
+import { useFormContextFieldHelpers } from "@/lib/data-hooks/useFormContextFieldHelpers";
+import { DataFormErrorMapContext } from "@/contexts/dataFormErrorMapContext";
+import { StateObject } from "@businessnjgovnavigator/shared/states";
+import { MediaQueries } from "@/lib/PageSizes";
+import { useMediaQuery } from "@mui/material";
+import { useConfig } from "@/lib/data-hooks/useConfig";
+import { Content } from "@/components/Content";
+import { isZipCodeNj } from "@/lib/domain-logic/isZipCodeNj";
+import { isZipCodeUs } from "@/lib/domain-logic/isZipCodeUs";
+
+interface Props {
+  className?: string;
+}
+
+export const MailingAddress = (props: Props): ReactElement => {
+  const { Config } = useConfig();
+  const { state, setCigaretteLicenseData } = useContext(CigaretteLicenseContext);
+  const { setIsValid: setIsLine1Valid, isFormFieldInvalid: isLine1FieldInvalid } =
+    useFormContextFieldHelpers("mailingAddressLine1", DataFormErrorMapContext);
+  const { setIsValid: setIsLine2Valid, isFormFieldInvalid: isLine2FieldInvalid } =
+    useFormContextFieldHelpers("mailingAddressLine2", DataFormErrorMapContext);
+  const { setIsValid: setIsCityValid, isFormFieldInvalid: isCityFieldInvalid } =
+    useFormContextFieldHelpers("mailingAddressCity", DataFormErrorMapContext);
+  const { setIsValid: setIsStateValid, isFormFieldInvalid: isStateFieldInvalid } =
+    useFormContextFieldHelpers("mailingAddressState", DataFormErrorMapContext);
+  const { setIsValid: setIsZipCodeValid, isFormFieldInvalid: isZipCodeFieldInvalid } =
+    useFormContextFieldHelpers("mailingAddressZipCode", DataFormErrorMapContext);
+  const isMobile = useMediaQuery(MediaQueries.isMobile);
+
+  type TextFields =
+    | "mailingAddressLine1"
+    | "mailingAddressLine2"
+    | "mailingAddressCity"
+    | "mailingAddressState"
+    | "mailingAddressZipCode";
+
+  const handleChange = (val: string | StateObject | undefined, fieldName: TextFields): void => {
+    if (val !== undefined) {
+      setCigaretteLicenseData({
+        ...state,
+        [fieldName]: val,
+      });
+      performValidation();
+    }
+  };
+
+  const handleAddressIsTheSame = (): void => {
+    if (state.mailingAddressIsTheSame) {
+      setCigaretteLicenseData({
+        ...state,
+        mailingAddressIsTheSame: false,
+      });
+      performValidation();
+    } else {
+      setCigaretteLicenseData({
+        ...state,
+        mailingAddressIsTheSame: true,
+      });
+      setIsLine1Valid(true);
+      setIsLine2Valid(true);
+      setIsCityValid(true);
+      setIsStateValid(true);
+      setIsZipCodeValid(true);
+    }
+  };
+
+  const getErrorMessage = (field: TextFields): string => {
+    if (!state[field]) return Config.cigaretteLicenseStep2.fields[field].errorRequiredText;
+
+    if (field === "mailingAddressZipCode" && state.mailingAddressState?.shortCode === "NJ")
+      return Config.cigaretteLicenseStep2.fields.mailingAddressZipCode.errorValidationTextAlt;
+
+    return Config.cigaretteLicenseStep2.fields[field].errorValidationText;
+  };
+
+  const validateLine1AndLine2 = (val: string, required: boolean): boolean => {
+    if (required && val === "") return false;
+
+    return val.length <= 35;
+  };
+
+  const validateZip = (zip: string): boolean => {
+    if (!zip) return false;
+
+    const isValidUsZipCode = isZipCodeUs(zip);
+    const isValidNjZipCode = isZipCodeNj(zip);
+
+    return state.mailingAddressState?.shortCode === "NJ" ? isValidNjZipCode : isValidUsZipCode;
+  };
+
+  const performValidation = (): void => {
+    setIsLine1Valid(validateLine1AndLine2(state.mailingAddressLine1, true));
+    setIsLine2Valid(validateLine1AndLine2(state.mailingAddressLine2, false));
+    setIsCityValid(state.mailingAddressCity !== "");
+    setIsStateValid(state.mailingAddressState !== undefined);
+    setIsZipCodeValid(validateZip(state.mailingAddressZipCode));
+  };
+
+  return (
+    <div>
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={state.mailingAddressIsTheSame}
+            onChange={handleAddressIsTheSame}
+            id="mailing-address-the-same"
+          />
+        }
+        label={Config.cigaretteLicenseStep2.mailingIsSameCheck}
+      />
+      {!state.mailingAddressIsTheSame && (
+        <>
+          <div className="margin-y-2">
+            <WithErrorBar
+              className={"padding-bottom-1"}
+              hasError={isLine1FieldInvalid}
+              type={"ALWAYS"}
+            >
+              <label htmlFor="mailingAddressLine1">
+                <span className={"text-bold"}>
+                  <Content>{Config.cigaretteLicenseStep2.fields.mailingAddressLine1.label}</Content>
+                </span>
+                <GenericTextField
+                  inputWidth={"full"}
+                  {...props}
+                  fieldName="mailingAddressLine1"
+                  handleChange={(val) => handleChange(val, "mailingAddressLine1")}
+                  disabled={state.mailingAddressIsTheSame}
+                  formContext={DataFormErrorMapContext}
+                  value={state.mailingAddressLine1}
+                  error={isLine1FieldInvalid}
+                  validationText={getErrorMessage("mailingAddressLine1")}
+                  preventRefreshWhenUnmounted
+                  onValidation={performValidation}
+                />
+              </label>
+            </WithErrorBar>
+          </div>
+          <div className="margin-y-2">
+            <WithErrorBar
+              className={"padding-bottom-1"}
+              hasError={isLine2FieldInvalid}
+              type={"ALWAYS"}
+            >
+              <label htmlFor="mailingAddressLine2">
+                <span className={"text-bold"}>
+                  <Content>{Config.cigaretteLicenseStep2.fields.mailingAddressLine2.label}</Content>
+                </span>
+                <GenericTextField
+                  inputWidth={"full"}
+                  {...props}
+                  fieldName="mailingAddressLine2"
+                  handleChange={(val) => handleChange(val, "mailingAddressLine2")}
+                  disabled={state.mailingAddressIsTheSame}
+                  formContext={DataFormErrorMapContext}
+                  value={state.mailingAddressLine2}
+                  error={isLine2FieldInvalid}
+                  validationText={getErrorMessage("mailingAddressLine2")}
+                  preventRefreshWhenUnmounted
+                  onValidation={performValidation}
+                />
+              </label>
+            </WithErrorBar>
+          </div>
+          <div className={"grid-row grid-gap"}>
+            <span className={`${isMobile ? "width-100" : "grid-col-6"}`}>
+              <WithErrorBar
+                className={"padding-bottom-1"}
+                hasError={isCityFieldInvalid}
+                type={"ALWAYS"}
+              >
+                <label htmlFor="mailingAddressCity">
+                  <span className={"text-bold"}>
+                    <Content>
+                      {Config.cigaretteLicenseStep2.fields.mailingAddressCity.label}
+                    </Content>
+                  </span>
+                  <GenericTextField
+                    inputWidth={"full"}
+                    {...props}
+                    fieldName="mailingAddressCity"
+                    handleChange={(val) => handleChange(val, "mailingAddressCity")}
+                    disabled={state.mailingAddressIsTheSame}
+                    formContext={DataFormErrorMapContext}
+                    value={state.mailingAddressCity}
+                    error={isCityFieldInvalid}
+                    validationText={getErrorMessage("mailingAddressCity")}
+                    preventRefreshWhenUnmounted
+                    onValidation={performValidation}
+                  />
+                </label>
+              </WithErrorBar>
+            </span>
+            <span className={`${isMobile ? "grid-col-6" : "grid-col-3"}`}>
+              <label htmlFor="mailingAddressState">
+                <span className={"text-bold"}>
+                  <Content>{Config.cigaretteLicenseStep2.fields.mailingAddressState.label}</Content>
+                </span>
+                <StateDropdown
+                  fieldName="mailingAddressState"
+                  onSelect={(val) => handleChange(val, "mailingAddressState")}
+                  disabled={state.mailingAddressIsTheSame}
+                  value={state.mailingAddressState?.shortCode}
+                  error={isStateFieldInvalid}
+                  validationText={getErrorMessage("mailingAddressState")}
+                  onValidation={performValidation}
+                />
+              </label>
+            </span>
+            <span className={`${isMobile ? "grid-col-6" : "grid-col-3"}`}>
+              <label htmlFor="mailingAddressZipCode">
+                <span className={"text-bold"}>
+                  <Content>
+                    {Config.cigaretteLicenseStep2.fields.mailingAddressZipCode.label}
+                  </Content>
+                </span>
+                <GenericTextField
+                  inputWidth={"full"}
+                  {...props}
+                  fieldName="mailingAddressZipCode"
+                  handleChange={(val) => handleChange(val, "mailingAddressZipCode")}
+                  disabled={state.mailingAddressIsTheSame}
+                  formContext={DataFormErrorMapContext}
+                  numericProps={{ maxLength: 5 }}
+                  value={state.mailingAddressZipCode}
+                  error={isZipCodeFieldInvalid}
+                  validationText={getErrorMessage("mailingAddressZipCode")}
+                  preventRefreshWhenUnmounted
+                  onValidation={performValidation}
+                />
+              </label>
+            </span>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
