f1f6bbec23ab2f6c6bf4726f09061d96db0066cd
adidner <adidner@gmail.com>
2025-07-30 16:41:44 -0400

    feat: [AB#13035] added CMS Preview for Emergency Trip Permit, legal message, self registration

---
+++ web/src/lib/cms/previews/LegalMessagePreview.tsx
@@ -0,0 +1,21 @@
+import { LegalMessage } from "@/components/LegalMessage";
+import { ConfigContext } from "@/contexts/configContext";
+import { PreviewProps } from "@/lib/cms/helpers/previewHelpers";
+import { usePreviewConfig } from "@/lib/cms/helpers/usePreviewConfig";
+import { usePreviewRef } from "@/lib/cms/helpers/usePreviewRef";
+import { ReactElement } from "react";
+
+const LegalMessagePreview = (props: PreviewProps): ReactElement => {
+  const ref = usePreviewRef(props);
+  const { config, setConfig } = usePreviewConfig(props);
+
+  return (
+    <ConfigContext.Provider value={{ config, setOverrides: setConfig }}>
+      <div className="cms" ref={ref} style={{ margin: 40, pointerEvents: "none" }}>
+        <LegalMessage />
+      </div>
+    </ConfigContext.Provider>
+  );
+};
+
+export default LegalMessagePreview;
