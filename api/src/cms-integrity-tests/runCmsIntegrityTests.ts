3d12e9f5be23038570a465149abdd045add757bc
adidner <adidner@gmail.com>
2025-09-18 16:57:53 -0400

    WIP:

--- api/src/cms-integrity-tests/runCmsIntegrityTests.ts
+++ api/src/cms-integrity-tests/runCmsIntegrityTests.ts
@@ -1,14 +1,13 @@
 import { checkAnytimeActionCategoryUsage } from "@businessnjgovnavigator/api/src/cms-integrity-tests/anytimeActionCategoryUsage";
+import { LogWriterType } from "@libs/logWriter";
 import { getConfigValue } from "@libs/ssmUtils";
 
-export const runCmsIntegrityTests = async (stage: string): Promise<void> => {
-  if (stage === "testing" || stage === "content") {
-    const topicArn = await getConfigValue("cms_alerts_sns_topic_arn");
+export const runCmsIntegrityTests = async (stage: string, logger: LogWriterType): Promise<void> => {
+  const topicArn = await getConfigValue("cms_alerts_sns_topic_arn");
 
-    const topicArnIsSetForThisEnvironment = topicArn !== "";
+  const topicArnIsSetForThisEnvironment = topicArn !== "";
 
-    if (topicArnIsSetForThisEnvironment) {
-      checkAnytimeActionCategoryUsage(topicArn, stage);
-    }
+  if (topicArnIsSetForThisEnvironment) {
+    await checkAnytimeActionCategoryUsage(topicArn, stage, logger);
   }
 };
