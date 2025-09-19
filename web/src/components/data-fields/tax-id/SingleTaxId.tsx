4f29ff8b7e097d0fe0eb36c3e71c35b4addc6c33
adidner <adidner@gmail.com>
2025-09-19 15:46:27 -0400

    fix: [AB#14055] error handling bar css fix

--- web/src/components/data-fields/tax-id/SingleTaxId.tsx
+++ web/src/components/data-fields/tax-id/SingleTaxId.tsx
@@ -2,12 +2,10 @@
 
 import { GenericTextField } from "@/components/GenericTextField";
 import { ShowHideStatus } from "@/components/ShowHideToggleButton";
-import { WithErrorBar } from "@/components/WithErrorBar";
 import { ProfileDataFieldProps } from "@/components/data-fields/ProfileDataField";
 import { DataFormErrorMapContext } from "@/contexts/dataFormErrorMapContext";
 import { ProfileDataContext } from "@/contexts/profileDataContext";
 import { MediaQueries } from "@/lib/PageSizes";
-import { useFormContextFieldHelpers } from "@/lib/data-hooks/useFormContextFieldHelpers";
 import { formatTaxId } from "@/lib/domain-logic/formatTaxId";
 import { InputAdornment, useMediaQuery } from "@mui/material";
 import { ReactElement, useContext } from "react";
@@ -30,7 +28,6 @@ export const SingleTaxId = ({
   const isTabletAndUp = useMediaQuery(MediaQueries.tabletAndUp);
 
   const { state, setProfileData } = useContext(ProfileDataContext);
-  const { isFormFieldInvalid } = useFormContextFieldHelpers(fieldName, DataFormErrorMapContext);
 
   const handleChange = (value: string): void => {
     if (handleChangeOverride) {
@@ -45,29 +42,27 @@ export const SingleTaxId = ({
 
   return (
     <div className={isTabletAndUp ? "" : "flex flex-column"}>
-      <WithErrorBar hasError={isFormFieldInvalid} type="ALWAYS">
-        <GenericTextField
-          allowMasking={true}
-          disabled={props.taxIdDisplayStatus === "password-view"}
-          fieldName={fieldName}
-          formContext={DataFormErrorMapContext}
-          handleChange={handleChange}
-          inputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                {isTabletAndUp && props.getShowHideToggleButton()}
-              </InputAdornment>
-            ),
-          }}
-          inputWidth={"reduced"}
-          numericProps={{ minLength: 12, maxLength: 12 }}
-          type={props.taxIdDisplayStatus === "text-view" ? "text" : "password"}
-          validationText={validationText}
-          value={state.profileData[fieldName] as string | undefined}
-          visualFilter={formatTaxId}
-          {...props}
-        />
-      </WithErrorBar>
+      <GenericTextField
+        allowMasking={true}
+        disabled={props.taxIdDisplayStatus === "password-view"}
+        fieldName={fieldName}
+        formContext={DataFormErrorMapContext}
+        handleChange={handleChange}
+        inputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              {isTabletAndUp && props.getShowHideToggleButton()}
+            </InputAdornment>
+          ),
+        }}
+        inputWidth={"reduced"}
+        numericProps={{ minLength: 12, maxLength: 12 }}
+        type={props.taxIdDisplayStatus === "text-view" ? "text" : "password"}
+        validationText={validationText}
+        value={state.profileData[fieldName] as string | undefined}
+        visualFilter={formatTaxId}
+        {...props}
+      />
 
       {!isTabletAndUp && (
         <div className="flex flex-justify-center margin-bottom-3">
