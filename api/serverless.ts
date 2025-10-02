ab28fded179318908b31fec7d281e83f13f7812e
Ade Adedeji <aadedeji@fearless.tech>
2025-10-02 09:35:44 -0400

    chore: [AB#16180] upgrade serverless to nodejs22.x base

--- api/serverless.ts
+++ api/serverless.ts
@@ -142,7 +142,7 @@ const serverlessConfiguration: AWS = {
   provider: {
     name: "aws",
     deploymentBucket: serverlessDeploymentS3Bucket,
-    runtime: "nodejs20.x",
+    runtime: "nodejs22.x",
     stage: stage,
     region: region,
     iam: {
