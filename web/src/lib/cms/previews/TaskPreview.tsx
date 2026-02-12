b13aa2cdbd13aa6de26ef5d1f617c3112723d345
JP Hechter <jhechter@fearless.tech>
2026-02-12 18:01:54 -0500

    feat: show roadmaps where a task is used in the CMS task preview

--- web/src/lib/cms/previews/TaskPreview.tsx
+++ web/src/lib/cms/previews/TaskPreview.tsx
@@ -1,8 +1,10 @@
+import { Icon } from "@/components/njwds/Icon";
 import { TaskPageSwitchComponent } from "@/components/TaskPageSwitchComponent";
 import { PreviewProps } from "@/lib/cms/helpers/previewHelpers";
 import { usePageData } from "@/lib/cms/helpers/usePageData";
 import { usePreviewRef } from "@/lib/cms/helpers/usePreviewRef";
 import { generateRoadmap } from "@/test/factories";
+import { getIndustries } from "@businessnjgovnavigator/shared/industry";
 import {
   generateBusiness,
   generateMunicipality,
@@ -12,9 +14,11 @@ import {
   createEmptyTaskDisplayContent,
   TaskWithLicenseTaskId,
 } from "@businessnjgovnavigator/shared/types";
-import { ReactElement } from "react";
+import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
+import { ReactElement, useState } from "react";
 
 const TaskPreview = (props: PreviewProps): ReactElement => {
+  const [isOpenRoadmaps, setIsOpenRoadmaps] = useState<boolean>(false);
   const ref = usePreviewRef(props);
   const task = usePageData<TaskWithLicenseTaskId>(props);
 
@@ -28,21 +32,73 @@ const TaskPreview = (props: PreviewProps): ReactElement => {
   const displayContent = createEmptyTaskDisplayContent();
   const roadmap = generateRoadmap({});
 
-  return (
-    <div className="cms" ref={ref} style={{ margin: 40, pointerEvents: "none" }}>
-      <div>
-        <div>This file is mapped to the following license (not enabled if blank):</div>
-        <div className="margin-bottom-10 text-bold">{task?.licenseName}</div>
-      </div>
+  const applicableIndustries = getIndustries({ overrideShowDisabledIndustries: true }).filter(
+    (industry) => {
+      const taskIds = industry.roadmapSteps.map((step) => step.task);
+      return taskIds.includes(task.id);
+    },
+  );
 
-      <TaskPageSwitchComponent
-        task={task}
-        business={fakeBusinessWithMunicipality}
-        displayContent={displayContent}
-        roadmap={roadmap}
-        CMS_ONLY_disable_default_functionality
-      />
-    </div>
+  const industryRoadmaps = (): ReactElement => {
+    return (
+      <Accordion
+        expanded={isOpenRoadmaps}
+        onChange={() => {
+          setIsOpenRoadmaps(!isOpenRoadmaps);
+        }}
+        className="margin-left-1 margin-right-10"
+      >
+        <AccordionSummary
+          expandIcon={
+            <Icon className={"usa-icon--size-5 margin-left-1"} iconName={"expand_more"} />
+          }
+        >
+          <b>Roadmaps</b>
+        </AccordionSummary>
+        <AccordionDetails>
+          <ul>
+            {applicableIndustries.length > 0 ? (
+              applicableIndustries.map((industry) => {
+                return (
+                  <li key={industry.id}>
+                    <a
+                      href={`/mgmt/cms#/collections/roadmaps/entries/${industry.id}`}
+                      target="_top"
+                    >
+                      {industry.name}
+                    </a>
+                  </li>
+                );
+              })
+            ) : (
+              <li>This task is not used in any industry roadmaps.</li>
+            )}
+          </ul>
+        </AccordionDetails>
+      </Accordion>
+    );
+  };
+
+  return (
+    <>
+      {industryRoadmaps()}
+      <div className="cms" ref={ref} style={{ margin: 40, pointerEvents: "none" }}>
+        {task?.licenseName && (
+          <div>
+            <div>This file is mapped to the following license:</div>
+            <div className="margin-bottom-10 text-bold">{task?.licenseName}</div>
+          </div>
+        )}
+
+        <TaskPageSwitchComponent
+          task={task}
+          business={fakeBusinessWithMunicipality}
+          displayContent={displayContent}
+          roadmap={roadmap}
+          CMS_ONLY_disable_default_functionality
+        />
+      </div>
+    </>
   );
 };
 
