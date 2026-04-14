210cd074bfd19db080b15aa9c919e9fec28fc17b
Ade Adedeji <aadedeji@fearless.tech>
2026-04-14 10:52:01 -0400

    chore: [AB#17520] migrate additional monitoring resources

--- api/cdk/lib/monitoringStack.ts
+++ api/cdk/lib/monitoringStack.ts
@@ -9,6 +9,8 @@ import {
   NAVIGATOR_DB_CLIENT,
   NAVIGATOR_WEBSERVICE,
   HEALTH_CHECK_ENDPOINTS,
+  USERS_TABLE,
+  BUSINESSES_TABLE,
 } from "./constants";
 
 export interface MonitoringStackProps extends StackProps {
@@ -37,6 +39,18 @@ export class MonitoringStack extends Stack {
       removalPolicy: RemovalPolicy.RETAIN,
     });
 
+    const expressLogGroup = logs.LogGroup.fromLogGroupName(
+      this,
+      "ExpressRegLogGroup",
+      `/aws/lambda/businessnjgov-api-v2-${props.stage}-express`,
+    );
+
+    const migrateUsersVersionLogGroup = logs.LogGroup.fromLogGroupName(
+      this,
+      "MigrateUsersVersionLogGroup",
+      `/aws/lambda/businessnjgov-api-v2-${props.stage}-migrateUsersVersion`,
+    );
+
     if (props.stage === "dev") {
       new logs.LogGroup(this, "ApiLogLocalGroup", {
         logGroupName: `/${NAVIGATOR_WEBSERVICE}/local/ApiLogs`,
@@ -48,6 +62,18 @@ export class MonitoringStack extends Stack {
         logGroupName: `/${NAVIGATOR_DB_CLIENT}/local/DataMigrationLogs`,
         retention: logs.RetentionDays.SIX_MONTHS,
         removalPolicy: RemovalPolicy.RETAIN,
+      });
+
+      const cmsAlertTopic = new sns.Topic(this, "CmsAlertTopic", {
+        topicName: `bfs-navigator-${props.stage}-cms-alert-topic`,
+      });
+
+      cmsAlertTopic.applyRemovalPolicy(RemovalPolicy.RETAIN);
+
+      new sns.Subscription(this, "CmsAlertTopicSubscription", {
+        topic: cmsAlertTopic,
+        protocol: sns.SubscriptionProtocol.HTTPS,
+        endpoint: "https://global.sns-api.chatbot.amazonaws.com",
       });
     }
 
@@ -85,9 +111,136 @@ export class MonitoringStack extends Stack {
       },
     );
 
+    const migrationFailureMetricFilter = new logs.MetricFilter(
+      this,
+      "MigrationFailureMetricFilter",
+      {
+        logGroup: migrateUsersVersionLogGroup,
+        filterName: `MigrationFailureCount-${props.stage}`,
+        metricNamespace: "BFS/Navigator/Lambda",
+        metricName: "MigrateUserVersionsFailures",
+        filterPattern: logs.FilterPattern.literal('"MigrateUserVersions Failed"'),
+        metricValue: "1",
+        defaultValue: 0,
+      },
+    );
+
+    const migrationFailureMetric = migrationFailureMetricFilter.metric({
+      statistic: "Sum",
+      period: Duration.seconds(300),
+    });
+
+    const migrationLambdaTopic = new sns.Topic(this, "migrationLambdaTopic", {
+      topicName: `bfs-navigator-${props.stage}-migrationLambda-Topic`,
+    });
+    migrationLambdaTopic.applyRemovalPolicy(RemovalPolicy.RETAIN);
+
+    const killSwitchAlertsTopic = new sns.Topic(this, "KillSwitchAlertsTopic", {
+      topicName: `bfs-navigator-${props.stage}-kill-switch-alerts`,
+    });
+    killSwitchAlertsTopic.applyRemovalPolicy(RemovalPolicy.RETAIN);
+
+    new sns.Subscription(this, "KillSwitchAlertsSubscription", {
+      topic: killSwitchAlertsTopic,
+      protocol: sns.SubscriptionProtocol.HTTPS,
+      endpoint: "https://global.sns-api.chatbot.amazonaws.com",
+    });
+
+    const migrateUserVersionErrorTopic = new sns.Topic(this, "MigrateUserVersionErrorTopic", {
+      topicName: `bfs-navigator-${props.stage}-migrate-user-version-errors`,
+    });
+    migrateUserVersionErrorTopic.applyRemovalPolicy(RemovalPolicy.RETAIN);
+
+    new sns.Subscription(this, "MigrateUserVersionErrorSubscription", {
+      topic: migrateUserVersionErrorTopic,
+      protocol: sns.SubscriptionProtocol.HTTPS,
+      endpoint: "https://global.sns-api.chatbot.amazonaws.com",
+    });
+
+    const migrationFailureAlarm = new cloudwatch.Alarm(this, "MigrationFailureAlarm", {
+      alarmName: `bfs-migrate-userVersions-failures-${props.stage}`,
+      metric: migrationFailureMetric,
+      threshold: 1,
+      evaluationPeriods: 1,
+      comparisonOperator: cloudwatch.ComparisonOperator.GREATER_THAN_THRESHOLD,
+      treatMissingData: cloudwatch.TreatMissingData.NOT_BREACHING,
+      alarmDescription: `Alarm when the MigrateUserVersions failure count exceeds the threshold for ${props.stage}`,
+    });
+
+    migrationFailureAlarm.addAlarmAction(
+      new cloudwatch_actions.SnsAction(migrateUserVersionErrorTopic),
+    );
+    migrationFailureAlarm.applyRemovalPolicy(RemovalPolicy.RETAIN);
+
+    const killSwitchAlarm = new cloudwatch.Alarm(this, "KillSwitchSNSAlarm", {
+      alarmName: `KillSwitchSNSActivity-${props.stage}`,
+      metric: new cloudwatch.Metric({
+        namespace: "AWS/SNS",
+        metricName: "NumberOfMessagesPublished",
+        statistic: "Sum",
+        period: Duration.seconds(60),
+        dimensionsMap: {
+          TopicName: migrationLambdaTopic.topicName,
+        },
+      }),
+      threshold: 1,
+      evaluationPeriods: 1,
+      comparisonOperator: cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+      treatMissingData: cloudwatch.TreatMissingData.NOT_BREACHING,
+      alarmDescription: "Alarms when kill switch SNS topic is triggered for visibility.",
+    });
+
+    killSwitchAlarm.addAlarmAction(new cloudwatch_actions.SnsAction(killSwitchAlertsTopic));
+
+    killSwitchAlarm.addOkAction(new cloudwatch_actions.SnsAction(killSwitchAlertsTopic));
+
+    killSwitchAlarm.applyRemovalPolicy(RemovalPolicy.RETAIN);
+
     const shouldCreateMonitoringResources = props.stage !== "content" && props.stage !== "testing";
 
     if (shouldCreateMonitoringResources) {
+      const selfRegErrorMetricFilter = new logs.MetricFilter(this, "SelfRegErrorMetricFilter", {
+        logGroup: expressLogGroup,
+        filterName: `myNJSelfRegErrorCount-${props.stage}`,
+        metricNamespace: "BFS/Navigator",
+        metricName: "myNJSelfRegErrorCount",
+        filterPattern: logs.FilterPattern.literal('"myNJSelfRegError"'),
+        metricValue: "1",
+        defaultValue: 0,
+      });
+      const selfRegMetric = selfRegErrorMetricFilter.metric({
+        statistic: "Sum",
+        period: Duration.seconds(300),
+      });
+
+      const mynjSelfRegTopic = new sns.Topic(this, "MynjSelfRegTopic", {
+        topicName: `bfs-navigator-${props.stage}-mynj-selfreg-errors`,
+      });
+
+      new sns.Subscription(this, "MynjSelfRegTopicSubscription", {
+        topic: mynjSelfRegTopic,
+        protocol: sns.SubscriptionProtocol.HTTPS,
+        endpoint: "https://global.sns-api.chatbot.amazonaws.com",
+      });
+
+      mynjSelfRegTopic.applyRemovalPolicy(RemovalPolicy.RETAIN);
+
+      const selfRegAlarm = new cloudwatch.Alarm(this, "MyNjLoginErrorsAlarm", {
+        alarmName: `myNJSelfRegLoginErrors-${props.stage}`,
+        metric: selfRegMetric,
+        threshold: 5,
+        evaluationPeriods: 1,
+        comparisonOperator: cloudwatch.ComparisonOperator.GREATER_THAN_THRESHOLD,
+        treatMissingData: cloudwatch.TreatMissingData.NOT_BREACHING,
+        alarmDescription:
+          "Triggered when myNJ login failures exceed threshold, indicating potential login issues.",
+      });
+
+      selfRegAlarm.addAlarmAction(new cloudwatch_actions.SnsAction(mynjSelfRegTopic));
+      selfRegAlarm.addOkAction(new cloudwatch_actions.SnsAction(mynjSelfRegTopic));
+
+      selfRegAlarm.applyRemovalPolicy(RemovalPolicy.RETAIN);
+
       const dataMigrationTopic = new sns.Topic(this, "DataMigrationErrorTopic", {
         topicName: `bfs-navigator-${props.stage}-data-migration-errors`,
       });
@@ -166,6 +319,86 @@ export class MonitoringStack extends Stack {
       apiErrorAlarm.addAlarmAction(new cloudwatch_actions.SnsAction(navigatorApiErrorTopic));
       apiErrorAlarm.applyRemovalPolicy(RemovalPolicy.RETAIN);
 
+      const dynamoDbUsersLatencyMetric = new cloudwatch.Metric({
+        namespace: "AWS/DynamoDB",
+        metricName: "SuccessfulRequestLatency",
+        statistic: "Average",
+        period: Duration.seconds(300),
+        dimensionsMap: {
+          Operation: "Query",
+          TableName: `${USERS_TABLE}-${props.stage}`,
+        },
+      });
+
+      const dynamoDbLatencyAlarm = new cloudwatch.Alarm(this, "DynamoDbLatencyAlarm", {
+        alarmName: `${props.stage}-dynamodb-users-latency`,
+        metric: dynamoDbUsersLatencyMetric,
+        threshold: 30,
+        evaluationPeriods: 2,
+        comparisonOperator: cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+        treatMissingData: cloudwatch.TreatMissingData.NOT_BREACHING,
+      });
+      dynamoDbLatencyAlarm.addAlarmAction(new cloudwatch_actions.SnsAction(navigatorApiErrorTopic));
+      dynamoDbLatencyAlarm.addOkAction(new cloudwatch_actions.SnsAction(navigatorApiErrorTopic));
+      dynamoDbLatencyAlarm.applyRemovalPolicy(RemovalPolicy.RETAIN);
+
+      const dynamoDbBusinessesLatencyMetric = new cloudwatch.Metric({
+        namespace: "AWS/DynamoDB",
+        metricName: "SuccessfulRequestLatency",
+        statistic: "Average",
+        period: Duration.seconds(300),
+        dimensionsMap: {
+          Operation: "Query",
+          TableName: `${BUSINESSES_TABLE}-${props.stage}`,
+        },
+      });
+
+      const dynamoDbBusinessesLatencyAlarm = new cloudwatch.Alarm(this, "DynamoDbLatencyAlarm", {
+        alarmName: `${props.stage}-dynamodb-businesses-latency`,
+        metric: dynamoDbBusinessesLatencyMetric,
+        threshold: 30,
+        evaluationPeriods: 2,
+        comparisonOperator: cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+        treatMissingData: cloudwatch.TreatMissingData.NOT_BREACHING,
+      });
+
+      dynamoDbBusinessesLatencyAlarm.addAlarmAction(
+        new cloudwatch_actions.SnsAction(navigatorApiErrorTopic),
+      );
+      dynamoDbBusinessesLatencyAlarm.addOkAction(
+        new cloudwatch_actions.SnsAction(navigatorApiErrorTopic),
+      );
+      dynamoDbBusinessesLatencyAlarm.applyRemovalPolicy(RemovalPolicy.RETAIN);
+
+      const lambdaErrorMetricFilter = new logs.MetricFilter(this, "LambdaErrorMetricFilter", {
+        logGroup: expressLogGroup,
+        filterName: `LambdaErrorCount-${props.stage}`,
+        metricNamespace: "BFS/Navigator/Lambda",
+        metricName: "LambdaErrorCount",
+        filterPattern: logs.FilterPattern.literal("error"),
+        metricValue: "2",
+        defaultValue: 0,
+      });
+
+      const lambdaErrorMetric = lambdaErrorMetricFilter.metric({
+        statistic: "Sum",
+        period: Duration.seconds(60),
+      });
+
+      const lambdaErrorAlarm = new cloudwatch.Alarm(this, "LambdaErrorAlarm", {
+        alarmName: `${props.stage}-bfs-navigator-lambda-errors`,
+        metric: lambdaErrorMetric,
+        threshold: 10,
+        evaluationPeriods: 1,
+        datapointsToAlarm: 1,
+        comparisonOperator: cloudwatch.ComparisonOperator.GREATER_THAN_THRESHOLD,
+        treatMissingData: cloudwatch.TreatMissingData.NOT_BREACHING,
+      });
+
+      lambdaErrorAlarm.addAlarmAction(new cloudwatch_actions.SnsAction(navigatorApiErrorTopic));
+      lambdaErrorAlarm.addOkAction(new cloudwatch_actions.SnsAction(navigatorApiErrorTopic));
+      lambdaErrorAlarm.applyRemovalPolicy(RemovalPolicy.RETAIN);
+
       const healthCheckErrorTopic = new sns.Topic(this, "HealthCheckErrorTopic", {
         topicName: `bfs-navigator-${props.stage}-health-check-errors`,
       });
@@ -189,7 +422,7 @@ export class MonitoringStack extends Stack {
           },
         });
 
-        const healthCheckalarm = new cloudwatch.Alarm(this, `HealthCheckAlarm-${key}`, {
+        const healthCheckAlarm = new cloudwatch.Alarm(this, `HealthCheckAlarm-${key}`, {
           alarmName: `${props.stage}-bfs-navigator-healthcheck-${key}-alarm`,
           metric,
           threshold: 1,
@@ -199,9 +432,9 @@ export class MonitoringStack extends Stack {
           treatMissingData: cloudwatch.TreatMissingData.NOT_BREACHING,
         });
 
-        healthCheckalarm.addAlarmAction(new cloudwatch_actions.SnsAction(healthCheckErrorTopic));
-        healthCheckalarm.addOkAction(new cloudwatch_actions.SnsAction(healthCheckErrorTopic));
-        healthCheckalarm.applyRemovalPolicy(RemovalPolicy.RETAIN);
+        healthCheckAlarm.addAlarmAction(new cloudwatch_actions.SnsAction(healthCheckErrorTopic));
+        healthCheckAlarm.addOkAction(new cloudwatch_actions.SnsAction(healthCheckErrorTopic));
+        healthCheckAlarm.applyRemovalPolicy(RemovalPolicy.RETAIN);
       }
     }
   }
