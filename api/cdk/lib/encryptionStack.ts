7a6c567a05228e76983128ceb6886361652f6910
Ade Adedeji <aadedeji@fearless.tech>
2026-05-12 09:37:37 -0400

    feat: [AB#17525] transfer bucket ownership to CDK

--- api/cdk/lib/encryptionStack.ts
+++ api/cdk/lib/encryptionStack.ts
@@ -4,7 +4,6 @@ import * as kms from "aws-cdk-lib/aws-kms";
 import * as iam from "aws-cdk-lib/aws-iam";
 
 export interface EncryptionStackProps extends StackProps {
-  taxKMSRole: iam.IRole;
   stage: string;
 }
 
@@ -15,6 +14,12 @@ export class EncryptionStack extends Stack {
   constructor(scope: Construct, id: string, props: EncryptionStackProps) {
     super(scope, id, props);
 
+    const taxKMSRole = iam.Role.fromRoleArn(
+      this,
+      "TaxKMSRole",
+      `arn:aws:iam::${this.account}:role/iamRoleForTaxIdEncryptionAndHashing`,
+    );
+
     this.taxIdHashingKey = new kms.Key(this, "TaxIdHashingKey", {
       description: `Tax Id Hashing Key for ${props.stage}`,
       enableKeyRotation: true,
@@ -23,8 +28,8 @@ export class EncryptionStack extends Stack {
 
     this.taxIdHashingKey.addAlias(`alias/${props.stage}/HashingTaxId`);
 
-    this.taxIdHashingKey.grantEncryptDecrypt(props.taxKMSRole);
-    this.taxIdHashingKey.grant(props.taxKMSRole, "kms:GenerateDataKey*");
+    this.taxIdHashingKey.grantEncryptDecrypt(taxKMSRole);
+    this.taxIdHashingKey.grant(taxKMSRole, "kms:GenerateDataKey*");
 
     this.taxIdEncryptionKey = new kms.Key(this, "TaxIdEncryptionKey", {
       description: `Tax Id Encryption Key for ${props.stage}`,
@@ -34,7 +39,7 @@ export class EncryptionStack extends Stack {
 
     this.taxIdEncryptionKey.addAlias(`alias/${props.stage}/EncryptionTaxId`);
 
-    this.taxIdEncryptionKey.grantEncryptDecrypt(props.taxKMSRole);
-    this.taxIdEncryptionKey.grant(props.taxKMSRole, "kms:GenerateDataKey*");
+    this.taxIdEncryptionKey.grantEncryptDecrypt(taxKMSRole);
+    this.taxIdEncryptionKey.grant(taxKMSRole, "kms:GenerateDataKey*");
   }
 }
