3fc79b16c9e72722d2602a0c0926ea23988a6db1
Anne LoVerso <anne.loverso@gmail.com>
2026-05-21 16:26:58 -0400

    chore: limit hideFromCategoryPage type

--- packages/static-site/domain/content/types.ts
+++ packages/static-site/domain/content/types.ts
@@ -35,7 +35,7 @@ export interface PageItem {
   /** If "true", make a page for this but do not create a link to it from the "Plan"/"Start" etc main page */
   /** Assumes false if undefined */
   /** Needs to be a string due to the "[key: string]: string | undefined" index type */
-  hideFromCategoryPage?: string | undefined;
+  hideFromCategoryPage?: "true" | "false" | undefined;
   /** Category this page belongs to (e.g., "plan", "start", "operate"). */
   category?: string;
   /** Introductory text displayed below the page title. */
