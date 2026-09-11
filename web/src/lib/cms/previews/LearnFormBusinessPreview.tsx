19de9a779a4110d0ef4305f4fe9d309360c15db6
ashlsun <janniesun00@gmail.com>
2026-09-11 16:18:14 -0400

    feat: [AB#17967] implement form your business learn page

---
+++ web/src/lib/cms/previews/LearnFormBusinessPreview.tsx
@@ -0,0 +1,21 @@
+import { FormBusiness } from "@/components/learn/FormBusiness";
+import { PreviewProps } from "@/lib/cms/helpers/previewHelpers";
+import { usePreviewConfig } from "@/lib/cms/helpers/usePreviewConfig";
+import { usePreviewRef } from "@/lib/cms/helpers/usePreviewRef";
+import { ConfigContext } from "@businessnjgovnavigator/shared/contexts";
+import { ReactElement } from "react";
+
+const LearnFormBusinessPreview = (props: PreviewProps): ReactElement => {
+  const { config, setConfig } = usePreviewConfig(props);
+  const ref = usePreviewRef(props);
+
+  return (
+    <ConfigContext.Provider value={{ config, setOverrides: setConfig }}>
+      <div className="cms" ref={ref} style={{ margin: 40, pointerEvents: "none" }}>
+        <FormBusiness />
+      </div>
+    </ConfigContext.Provider>
+  );
+};
+
+export default LearnFormBusinessPreview;
